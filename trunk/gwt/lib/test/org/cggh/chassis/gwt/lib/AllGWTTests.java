d0a2a84fcf58e58d2db176f5d718c7aceac0d2ff
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-07-13 16:21:42 +0000

    finished first pass on atompub, fixed problems in study format lib

---
+++ trunk/gwt/lib/test/org/cggh/chassis/gwt/lib/AllGWTTests.java
@@ -0,0 +1,29 @@
+/**
+ * $Id$
+ */
+package org.cggh.chassis.gwt.lib;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+/**
+ * TODO document me
+ * 
+ * @author aliman
+ *
+ */
+public class AllGWTTests extends GWTTestSuite {
+	
+	public static Test suite() {
+		TestSuite suite = new TestSuite("All GWT tests for Chassis GWT libraries");
+		suite.addTest(org.cggh.chassis.gwt.lib.xml.client.AllGWTTests.suite()); 
+		suite.addTest(org.cggh.chassis.gwt.lib.atom.client.format.AllGWTTests.suite()); 
+		suite.addTest(org.cggh.chassis.gwt.lib.atom.client.protocol.AllGWTTests.suite()); 
+		suite.addTest(org.cggh.chassis.gwt.lib.study.client.format.AllGWTTests.suite()); 
+		return suite;
+	}
+
+
+}
---
+++ trunk/gwt/lib/test/org/cggh/chassis/gwt/lib/xml/client/AllGWTTests.java
@@ -0,0 +1,26 @@
+/**
+ * $Id$
+ */
+package org.cggh.chassis.gwt.lib.xml.client;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+import com.google.gwt.junit.tools.GWTTestSuite;
+
+/**
+ * TODO document me
+ * 
+ * @author aliman
+ *
+ */
+public class AllGWTTests extends GWTTestSuite {
+	
+	public static Test suite() {
+		TestSuite suite = new TestSuite("All GWT tests for package: org.cggh.chassis.gwt.lib.xml.client");
+		suite.addTestSuite(TestXML.class);
+		return suite;
+	}
+
+
+}
