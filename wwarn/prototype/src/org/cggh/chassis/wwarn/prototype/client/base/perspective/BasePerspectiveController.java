566171b76624fcfc3e015797ccbbecaf5f72d5d8
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-03 13:12:27 +0000

    refactored perspectives code;

--- wwarn/prototype/src/org/cggh/chassis/wwarn/prototype/client/base/perspective/BasePerspectiveController.java
+++ wwarn/prototype/src/org/cggh/chassis/wwarn/prototype/client/base/perspective/BasePerspectiveController.java
@@ -1,32 +1,36 @@
 /**
  * 
  */
-package org.cggh.chassis.wwarn.prototype.client.curator.perspective;
+package org.cggh.chassis.wwarn.prototype.client.base.perspective;
 
 import org.cggh.chassis.gwt.lib.log.client.GWTLogger;
 import org.cggh.chassis.gwt.lib.log.client.Logger;
-import org.cggh.chassis.wwarn.prototype.client.submitter.perspective.SubmitterPerspective;
+
 
 
 /**
  * @author aliman
  *
  */
-class Controller {
+public class BasePerspectiveController {
 
-	private Model model;
-	private CuratorPerspective owner;
-	private Logger log;
-
-
-	Controller(Model model, CuratorPerspective owner) {
+	
+	
+	protected BasePerspectiveModel model;
+	protected BasePerspective owner;
+	protected Logger log;
+	
+	
+	
+	public BasePerspectiveController(BasePerspectiveModel model, BasePerspective owner) {
 		this.model = model;
 		this.owner = owner;
 		this.log = new GWTLogger();
-		log.setCurrentClass(Controller.class.getName());
 	}
 
-	void setIsCurrentPerspective(boolean b) {
+	
+	
+	public void setIsCurrentPerspective(boolean b) {
 		log.enter("setIsCurrentPerspective");
 		
 		log.trace("set current perspective on model");
@@ -35,14 +39,15 @@ class Controller {
 		log.leave();
 	}
 
+	
 
-	void setMainWidget(String widgetName) {
+	public void setMainWidget(String widgetName) {
 		setMainWidget(widgetName, true);
 	}
 	
 	
 	
-	void setMainWidget(String widgetName, boolean waypoint) {
+	public void setMainWidget(String widgetName, boolean waypoint) {
 		log.enter("setMainWidget");
 		log.trace("widgetName: "+widgetName);
 		
@@ -70,20 +75,20 @@ class Controller {
 
 	
 	
-	void setDefault() {
+	public void setDefault() {
 		log.enter("setDefault");
 		
 		log.trace("set main widget as home");
-		this.setMainWidget(SubmitterPerspective.WIDGET_HOME, false);
+		this.setMainWidget(BasePerspective.WIDGET_HOME, false);
 
-		// TODO review this
-		log.trace("sync state key, in case any parents or children change state");
-		this.owner.syncStateKey();
-		
+//		// TODO review this
+//		log.trace("sync state key, in case any parents or children change state");
+//		this.owner.syncStateKey();
+//		
 		log.leave();
 	}
 
 
-
-
+	
+	
 }
---
+++ wwarn/prototype/src/org/cggh/chassis/wwarn/prototype/client/base/perspective/BasePerspectiveModelReadOnly.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package org.cggh.chassis.wwarn.prototype.client.base.perspective;
+
+/**
+ * @author aliman
+ *
+ */
+public interface BasePerspectiveModelReadOnly {
+
+	public boolean getIsCurrentPerspective();
+	public String getMainWidgetName();
+
+}
