10c0c0150a942116f3e6046868af63e12f352a69
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-03 17:53:05 +0000

    part-baked

--- trunk/wwarn/prototype/src/org/cggh/chassis/wwarn/prototype/client/submitter/widget/newstudy/Controller.java
+++ trunk/wwarn/prototype/src/org/cggh/chassis/wwarn/prototype/client/submitter/widget/newstudy/Controller.java
@@ -3,6 +3,9 @@
  */
 package org.cggh.chassis.wwarn.prototype.client.submitter.widget.newstudy;
 
+import org.cggh.chassis.gwt.lib.study.client.format.StudyEntry;
+import org.cggh.chassis.gwt.lib.twisted.client.Deferred;
+
 /**
  * @author aliman
  *
@@ -18,5 +21,20 @@ class Controller {
 		this.model = model;
 		this.owner = owner;
 	}
+
+	Deferred<StudyEntry> createStudy(StudyEntry study) {
+		
+		// TODO make async call to create study
+		
+		// hack for now
+		this.owner.fireStudyCreationSuccess(study);
+		
+		Deferred<StudyEntry> def = new Deferred<StudyEntry>();
+		def.callback(study);
+		
+		return def;
+		
+	}
+	
 
 }
