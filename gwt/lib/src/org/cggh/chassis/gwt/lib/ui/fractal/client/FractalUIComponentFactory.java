b8b585d85fb5039c175449c59c5e4c7be23c7783
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-03 16:34:36 +0000

    initial version of component factory, factored out from wwarn prototype;

---
+++ gwt/lib/src/org/cggh/chassis/gwt/lib/ui/fractal/client/FractalUIComponentFactory.java
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+package org.cggh.chassis.gwt.lib.ui.fractal.client;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.cggh.chassis.gwt.lib.log.client.GWTLogger;
+import org.cggh.chassis.gwt.lib.log.client.Logger;
+import org.cggh.chassis.gwt.lib.twisted.client.Function;
+
+
+/**
+ * @author aliman
+ *
+ */
+public class FractalUIComponentFactory {
+
+	private static Map<String,Function<Object[],FractalUIComponent>> register = new HashMap<String,Function<Object[],FractalUIComponent>>();
+	private static Logger log = new GWTLogger();
+	
+	static {
+		log.setCurrentClass(FractalUIComponentFactory.class.getName());
+		log.trace("===== static code executed =====");
+	}
+	
+	public static FractalUIComponent create(String componentName, Object[] args) {
+		log.enter("create");
+		
+		FractalUIComponent ret = null;
+
+		// guard condition
+		if (componentName == null) {
+			return ret;
+		}
+
+		
+		if (register.containsKey(componentName)) {
+			Function<Object[],FractalUIComponent> creator = register.get(componentName);
+			ret = creator.apply(args);
+		}
+		
+		log.leave();
+		return ret;
+	}
+	
+	public static void register(String componentName, Function<Object[], FractalUIComponent> creator) {
+		log.enter("register");
+		log.trace("================================ componentName: "+componentName);
+		
+		register.put(componentName, creator);
+		
+		log.leave();
+	}
+	
+}
