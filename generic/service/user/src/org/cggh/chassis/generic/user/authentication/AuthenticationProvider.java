cb52f45f8392f6c1c7103cf4c208391f8c6ee917
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-18 09:23:14 +0000

    set up gwt rpc packages and config; implemented test on UserServiceImpl using mock dependencies;

---
+++ generic/service/user/src/org/cggh/chassis/generic/user/authentication/AuthenticationProvider.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.user.authentication;
+
+/**
+ * @author aliman
+ *
+ */
+public interface AuthenticationProvider {
+
+	public String getAuthenticatedUserId();
+	
+}
