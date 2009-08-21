bc087f8b15ddb2e7d6bcfb69bc98c6d6d2947bbd
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-21 15:28:49 +0000

    spike implementation

---
+++ generic/client/gwt/src/main/java/spike/security/user/client/UserDetailsEntryPoint.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package spike.security.user.client;
+
+import org.cggh.chassis.generic.user.gwtrpc.client.GWTUserDetailsService;
+import org.cggh.chassis.generic.user.gwtrpc.client.GWTUserDetailsServiceAsync;
+import org.cggh.chassis.generic.user.transfer.UserDetailsTO;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.rpc.ServiceDefTarget;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * @author aliman
+ *
+ */
+public class UserDetailsEntryPoint implements EntryPoint {
+
+	/* (non-Javadoc)
+	 * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+	 */
+	public void onModuleLoad() {
+
+		// instantiate service
+		GWTUserDetailsServiceAsync userService = GWT.create(GWTUserDetailsService.class);
+		
+		// set service URL
+		ServiceDefTarget target = (ServiceDefTarget) userService;
+		target.setServiceEntryPoint("/chassis-generic-user-service/gwtrpc/userdetails");
+		
+		// make service call
+		userService.getAuthenticatedUser(new AsyncCallback<UserDetailsTO>() {
+
+			public void onFailure(Throwable ex) {
+				RootPanel.get().clear();
+				RootPanel.get().add(new Label("Error: "+ex.getLocalizedMessage()));
+			}
+
+			public void onSuccess(UserDetailsTO user) {
+				RootPanel.get().clear();
+				RootPanel.get().add(new Label("Authenticated user: "+user.getId()));
+			}
+			
+		});
+
+	}
+
+}
