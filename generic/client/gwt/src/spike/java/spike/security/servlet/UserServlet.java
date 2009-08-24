ec246f460633303ea210184306b4d3fc6de84f1f
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-24 08:38:36 +0000

    minor change

--- generic/client/gwt/src/spike/java/spike/security/servlet/UserServlet.java
+++ generic/client/gwt/src/spike/java/spike/security/servlet/UserServlet.java
@@ -2,8 +2,6 @@ package spike.security.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
