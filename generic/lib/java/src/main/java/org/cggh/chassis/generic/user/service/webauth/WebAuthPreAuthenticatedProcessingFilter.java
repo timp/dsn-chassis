9ccae14cf8aca5bc32829f72c8e90ce963566796
krishna.rao@well.ox.ac.uk <krishna.rao@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-25 14:56:57 +0000

    Created spring PreAuthFilter to attempt to extract WebAuth authentication ID. Created spring security file to allow test instance to use WebAuth authentication, and use CGGH database authorization.

---
+++ generic/lib/java/src/main/java/org/cggh/chassis/generic/user/service/webauth/WebAuthPreAuthenticatedProcessingFilter.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.user.service.webauth;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.ui.preauth.AbstractPreAuthenticatedProcessingFilter;
+
+/**
+ * @author raok
+ *
+ */
+public class WebAuthPreAuthenticatedProcessingFilter extends AbstractPreAuthenticatedProcessingFilter {
+
+	/* (non-Javadoc)
+	 * @see org.springframework.security.ui.preauth.AbstractPreAuthenticatedProcessingFilter#getPreAuthenticatedCredentials(javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+	protected Object getPreAuthenticatedCredentials(HttpServletRequest arg0) {
+		//Required override, not used, but must not return null.
+		return "";
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.security.ui.preauth.AbstractPreAuthenticatedProcessingFilter#getPreAuthenticatedPrincipal(javax.servlet.http.HttpServletRequest)
+	 */
+	@Override
+	protected Object getPreAuthenticatedPrincipal(HttpServletRequest arg0) {
+		//TODO throw more appropriate errors.
+		
+		//Get the WEBAUTH_USER WebAuth environment variable.
+		//This is of the form: 'user<webAuthUserId>/gmap@GMAP.NET'
+		String webAuthUserString = (String) arg0.getAttribute("WEBAUTH_USER");
+		
+		//Extract the webAuthUserId.
+		String webAuthUserIdString;
+		if (webAuthUserString != null)
+		{
+			Pattern regEx = Pattern.compile("^user(\\d+)/gmap@GMAP\\.NET$");
+			Matcher matcher = regEx.matcher(webAuthUserString);
+			
+			//throw exception if webAuth attribute is invalid or not found
+			if (!matcher.find())
+			{
+				throw new Error ("WebAuth attribute invalid.");
+			}
+			
+			//Store webAuthUserId
+			webAuthUserIdString = matcher.group(1);
+		}
+		else
+		{
+			throw new Error ("WebAuth attribute not found.");
+		}
+		
+		return webAuthUserIdString;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.springframework.core.Ordered#getOrder()
+	 */
+	public int getOrder() {
+        return org.springframework.security.ui.FilterChainOrder.PRE_AUTH_FILTER;
+	}
+
+}
