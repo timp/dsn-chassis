936bd87836d15f47df9f0aaae12174c246f40667
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-27 09:16:27 +0000

    part-baked work on atom

---
+++ trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/client/format/AtomCategory.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.atom.client.format;
+
+/**
+ * @author aliman
+ *
+ */
+public interface AtomCategory {
+
+	public String getTerm();
+	public void setTerm(String term);
+	public String getScheme();
+	public void setScheme(String scheme);
+	public String getLabel();
+	public void setLabel(String label);
+	
+}
---
+++ trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/client/format/AtomPersonConstruct.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.atom.client.format;
+
+/**
+ * @author aliman
+ *
+ */
+public interface AtomPersonConstruct {
+
+	public String getURI();
+	public void setURI(String uri);
+	public String getEmail();
+	public void setEmail(String email);
+	public String getName();
+	public void setName(String name);
+	
+}
