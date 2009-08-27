8e2533a5e19ce835a3f8abc56a049c1c85e470de
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-08-27 12:22:15 +0000

    some refactoring; partial implementation of mock atom client

---
+++ generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/study/client/format/StudyEntry.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.atom.study.client.format;
+
+import java.util.List;
+
+import org.cggh.chassis.generic.atom.vanilla.client.format.AtomEntry;
+
+/**
+ * @author aliman
+ *
+ */
+public interface StudyEntry extends AtomEntry {
+
+	public List<String> getModules();
+	public void setModules(List<String> modules);
+	public void addModule(String module);
+	public void removeModule(String module);
+
+}
--- generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/vanilla/client/protocol/AtomClient.java
+++ generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/vanilla/client/protocol/AtomClient.java
@@ -1,10 +1,10 @@
 /**
  * 
  */
-package org.cggh.chassis.generic.atom.client.protocol;
+package org.cggh.chassis.generic.atom.vanilla.client.protocol;
 
-import org.cggh.chassis.generic.atom.client.format.AtomEntry;
-import org.cggh.chassis.generic.atom.client.format.AtomFeed;
+import org.cggh.chassis.generic.atom.vanilla.client.format.AtomEntry;
+import org.cggh.chassis.generic.atom.vanilla.client.format.AtomFeed;
 import org.cggh.chassis.generic.twisted.client.Deferred;
 
 /**
@@ -13,7 +13,7 @@ import org.cggh.chassis.generic.twisted.client.Deferred;
  */
 public interface AtomClient {
 
-	public Deferred<AtomFeed> getFeed(String collectionURL);
+	public Deferred<AtomFeed> getFeed(String feedURL);
 	public Deferred<AtomEntry> getEntry(String entryURL);
 	public Deferred<AtomEntry> postEntry(String collectionURL, AtomEntry entry);
 	public Deferred<AtomEntry> putEntry(String entryURL, AtomEntry entry);
