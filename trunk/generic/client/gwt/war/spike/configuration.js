4aaebe626e02551478dbae5b28d6a89d91a86551
krishna.rao@well.ox.ac.uk <krishna.rao@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-09-02 09:32:26 +0000

    Removed compiled code, because it recompilation causes too much trouble with svn. Any folder containing compiled code should be added to svn:ignore.

--- trunk/generic/client/gwt/war/spike/configuration.js
+++
@@ -1,4 +0,0 @@
-CHASSIS = new Object();
-CHASSIS.userDetailsServiceEndpointURL = "/chassis-generic-service-user/gwtrpc/userdetails";
-CHASSIS.studyFeedURL = "/chassis-generic-service-atom/edit/studies";
-CHASSIS.userChassisRolesPrefix = "ROLE_CHASSIS_";
