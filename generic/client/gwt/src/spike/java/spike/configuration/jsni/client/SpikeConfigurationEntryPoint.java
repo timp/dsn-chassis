55f85451b5f6405fd09db413a2f329f6f2e8bdee
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-09-22 10:07:51 +0000

    added basis for configurable modules

--- generic/client/gwt/src/spike/java/spike/configuration/jsni/client/SpikeConfigurationEntryPoint.java
+++ generic/client/gwt/src/spike/java/spike/configuration/jsni/client/SpikeConfigurationEntryPoint.java
@@ -4,8 +4,10 @@
 package spike.configuration.jsni.client;
 
 import org.cggh.chassis.generic.client.gwt.configuration.client.Configuration;
+import org.cggh.chassis.generic.client.gwt.configuration.client.Module;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.core.client.JsArray;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.RootPanel;
 
@@ -23,6 +25,20 @@ public class SpikeConfigurationEntryPoint implements EntryPoint {
 		RootPanel root = RootPanel.get("gwtcontent");
 
 		root.add(new HTML("<p>userDetailsServiceEndpointURL: "+Configuration.getUserDetailsServiceEndpointURL()+"</p>"));
+		root.add(new HTML("<p>studyFeedURL: "+Configuration.getStudyFeedURL()+"</p>"));
+		root.add(new HTML("<p>submissionFeedURL: "+Configuration.getSubmissionFeedURL()+"</p>"));
+		
+		JsArray<Module> modules = Configuration.getModules();
+		root.add(new HTML("<h2>Modules</h2>"));
+		String content = "<ul>";
+		for (int i=0; i<modules.length(); i++) {
+			Module module = modules.get(i);
+			content += "<li>id: "+module.getId();
+			content += "; labels: [en] "+module.getLabel("en");
+			content += "</li>";
+		}
+		content += "</ul>";
+		root.add(new HTML(content));
 
 	}
 
