ce844b890486fa6227ab46c42546ea923f6ca58d
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-02 10:52:30 +0000

    changed implementation of study query service to use server-side xquery script;

--- generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/study/client/protocol/impl/StudyQueryServiceImpl.java
+++ generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/study/client/protocol/impl/StudyQueryServiceImpl.java
@@ -9,6 +9,9 @@ import org.cggh.chassis.generic.atom.study.client.format.StudyFeed;
 import org.cggh.chassis.generic.atom.study.client.format.impl.StudyFactoryImpl;
 import org.cggh.chassis.generic.atom.study.client.protocol.StudyQueryService;
 import org.cggh.chassis.generic.atom.vanilla.client.format.Atom;
+import org.cggh.chassis.generic.atom.vanilla.client.format.AtomFeed;
+import org.cggh.chassis.generic.atom.vanilla.client.protocol.AtomService;
+import org.cggh.chassis.generic.atom.vanilla.client.protocol.impl.AtomServiceImpl;
 import org.cggh.chassis.generic.atom.vanilla.client.protocol.impl.AtomXQueryServiceImpl;
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
@@ -19,11 +22,12 @@ import org.cggh.chassis.generic.twisted.client.Function;
  * @author aliman
  *
  */
-public class StudyQueryServiceImpl extends AtomXQueryServiceImpl implements StudyQueryService {
+public class StudyQueryServiceImpl implements StudyQueryService {
 
 	
 	
 	private Log log = LogFactory.getLog(this.getClass());
+	private String serviceUrl;
 	
 	
 	
@@ -31,7 +35,38 @@ public class StudyQueryServiceImpl extends AtomXQueryServiceImpl implements Stud
 	 * @param serviceUrl
 	 */
 	public StudyQueryServiceImpl(String serviceUrl) {
-		super(serviceUrl);
+		this.serviceUrl = serviceUrl;
+	}
+
+
+
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.atom.study.client.protocol.StudyQueryService#getStudiesByAuthorEmail(java.lang.String)
+	 */
+	public Deferred<StudyFeed> getStudiesByAuthorEmail(String email) {
+		log.enter("getStudiesByAuthorEmail");
+
+		String url = this.serviceUrl + "?authoremail=" + email;
+		log.trace("url: "+url);
+
+		StudyFactory factory = new StudyFactoryImpl();
+		AtomService service = new AtomServiceImpl(factory);
+
+		Deferred<AtomFeed> deferredFeed = service.getFeed(url);
+
+		Function<AtomFeed,StudyFeed> adapter = new Function<AtomFeed,StudyFeed>() {
+			public StudyFeed apply(AtomFeed in) {
+				log.enter("[anonymous adapter function]");
+				log.trace("found "+in.getEntries().size()+" results; casting as study feed");
+				log.leave();
+				return (StudyFeed) in;
+			}
+		};
+		
+		Deferred<StudyFeed> deferredResults = deferredFeed.adapt(adapter);
+
+		log.leave();
+		return deferredResults;
 	}
 
 	
@@ -39,50 +74,50 @@ public class StudyQueryServiceImpl extends AtomXQueryServiceImpl implements Stud
 	/* (non-Javadoc)
 	 * @see org.cggh.chassis.generic.atom.study.protocol.StudyQueryService#getStudiesByAuthorEmail(java.lang.String)
 	 */
-	public Deferred<StudyFeed> getStudiesByAuthorEmail(String email) {
-		log.enter("getStudiesByAuthorEmail");
-		
-		String xquery = buildXQueryForGetStudiesByAuthorEmail(email);
-		log.trace(xquery);
-		
-		Deferred<String> deferredString = this.query(xquery);
-		
-		Function<String,StudyFeed> adapter = new Function<String,StudyFeed>() {
-
-			public StudyFeed apply(String studyFeedDoc) {
-				StudyFactory factory = new StudyFactoryImpl();
-				return factory.createStudyFeed(studyFeedDoc); // assume parse errors handled by errbacks in adapted deferred
-			}
-			
-		};
-		
-		log.leave();
-		return deferredString.adapt(adapter);
-
-	}
+//	public Deferred<StudyFeed> getStudiesByAuthorEmail(String email) {
+//		log.enter("getStudiesByAuthorEmail");
+//		
+//		String xquery = buildXQueryForGetStudiesByAuthorEmail(email);
+//		log.trace(xquery);
+//		
+//		Deferred<String> deferredString = this.query(xquery);
+//		
+//		Function<String,StudyFeed> adapter = new Function<String,StudyFeed>() {
+//
+//			public StudyFeed apply(String studyFeedDoc) {
+//				StudyFactory factory = new StudyFactoryImpl();
+//				return factory.createStudyFeed(studyFeedDoc); // assume parse errors handled by errbacks in adapted deferred
+//			}
+//			
+//		};
+//		
+//		log.leave();
+//		return deferredString.adapt(adapter);
+//
+//	}
 	
 	
 	
-	
-	public static String buildXQueryForGetStudiesByAuthorEmail(String email) {
-
-		String xquery = "\n" +
-			"<atom:feed \n" +
-				"xmlns:atom=\""+Atom.NSURI+"\" \n" +
-				"xmlns:chassis=\""+Chassis.NSURI+"\"> \n" +
-				"{ \n" +
-				"/atom:feed/atom:id, \n" +
-				"/atom:feed/atom:title, \n" +
-				"/atom:feed/atom:updated, \n" +
-				"for $e in (/atom:feed/atom:entry) \n" +
-				"where $e/atom:author/atom:email = '"+email+"' \n" +
-				"return $e \n" +
-				"} \n" +
-			"</atom:feed> \n";
-		
-		return xquery;	
-		
-	}
+// N.B. the following only works for eXist 1.2.x	
+//	public static String buildXQueryForGetStudiesByAuthorEmail(String email) {
+//
+//		String xquery = "\n" +
+//			"<atom:feed \n" +
+//				"xmlns:atom=\""+Atom.NSURI+"\" \n" +
+//				"xmlns:chassis=\""+Chassis.NSURI+"\"> \n" +
+//				"{ \n" +
+//				"/atom:feed/atom:id, \n" +
+//				"/atom:feed/atom:title, \n" +
+//				"/atom:feed/atom:updated, \n" +
+//				"for $e in (/atom:feed/atom:entry) \n" +
+//				"where $e/atom:author/atom:email = '"+email+"' \n" +
+//				"return $e \n" +
+//				"} \n" +
+//			"</atom:feed> \n";
+//		
+//		return xquery;	
+//		
+//	}
 	
 
 }
