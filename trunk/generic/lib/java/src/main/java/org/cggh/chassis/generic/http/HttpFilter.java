dbf16eb9a90282fe7cd63ca7d2fefaf9dafcfcdb
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-06 12:58:28 +0000

    created spike for abdera-based java atom client;

--- trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/HttpFilter.java
+++ trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/HttpFilter.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
+import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
@@ -19,6 +20,8 @@ import javax.servlet.http.HttpServletResponse;
  *
  */
 public abstract class HttpFilter implements Filter {
+
+	protected ServletContext context;
 
 	/* (non-Javadoc)
 	 * @see javax.servlet.Filter#destroy()
@@ -46,6 +49,8 @@ public abstract class HttpFilter implements Filter {
 	/* (non-Javadoc)
 	 * @see javax.servlet.Filter#init(javax.servlet.FilterConfig)
 	 */
-	public void init(FilterConfig filterConfig) throws ServletException {}
+	public void init(FilterConfig filterConfig) throws ServletException {
+		this.context = filterConfig.getServletContext();
+	}
 
 }
