e9b912cfee25001472c61df4ad28be4a98442aef
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-06 16:25:07 +0000

    added param for datafiles collection url

--- generic/client/gwt/war/spike/configuration.js
+++ generic/client/gwt/war/spike/configuration.js
@@ -10,6 +10,8 @@ CHASSIS.studyQueryServiceURL = "/chassis-generic-service-exist/query/studies.xql
 CHASSIS.submissionFeedURL = "/chassis-generic-service-exist/atom/edit/submissions";
 CHASSIS.submissionQueryServiceURL = "/chassis-generic-service-exist/query/submissions.xql";
 
+CHASSIS.dataFileFeedURL = "/chassis-generic-service-exist/atom/edit/datafiles";
+
 // configure role name prefix
 CHASSIS.userChassisRolesPrefix = "ROLE_CHASSIS_";
 
