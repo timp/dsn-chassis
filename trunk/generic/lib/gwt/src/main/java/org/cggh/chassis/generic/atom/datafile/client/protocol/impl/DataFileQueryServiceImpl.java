23da9f544605a15e92d089b585a0b3cdca51e7b4
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-07 11:09:22 +0000

    first implementation of data file query service

---
+++ trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/datafile/client/protocol/impl/DataFileQueryServiceImpl.java
@@ -0,0 +1,67 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.atom.datafile.client.protocol.impl;
+
+import org.cggh.chassis.generic.atom.datafile.client.protocol.DataFileQuery;
+import org.cggh.chassis.generic.atom.datafile.client.protocol.DataFileQueryService;
+import org.cggh.chassis.generic.atom.vanilla.client.format.AtomFactory;
+import org.cggh.chassis.generic.atom.vanilla.client.format.AtomFeed;
+import org.cggh.chassis.generic.atom.vanilla.client.format.impl.AtomFactoryImpl;
+import org.cggh.chassis.generic.atom.vanilla.client.protocol.AtomService;
+import org.cggh.chassis.generic.atom.vanilla.client.protocol.impl.AtomServiceImpl;
+import org.cggh.chassis.generic.log.client.Log;
+import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.twisted.client.Deferred;
+
+/**
+ * @author aliman
+ *
+ */
+public class DataFileQueryServiceImpl implements DataFileQueryService {
+
+	
+	
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.atom.datafile.client.protocol.DataFileQueryService#query(org.cggh.chassis.generic.atom.datafile.client.protocol.DataFileQuery)
+	 */
+	public Deferred<AtomFeed> query(DataFileQuery query) {
+		log.enter("query");
+
+		String url = this.serviceUrl + "?";
+
+		if (query.getAuthorEmail() != null) {
+			url += "authoremail=" + query.getAuthorEmail() + "&";
+		}
+		
+		if (query.getSubmissionUrl() != null) {
+			url += "submission=" + query.getSubmissionUrl() + "&";
+		}
+		
+		log.trace("url: "+url);
+
+		AtomFactory factory = new AtomFactoryImpl();
+		AtomService service = new AtomServiceImpl(factory);
+
+		Deferred<AtomFeed> deferredFeed = service.getFeed(url);
+
+		log.leave();
+		return deferredFeed;
+	}
+
+	
+	
+	private Log log = LogFactory.getLog(this.getClass());
+	private String serviceUrl;
+	
+	
+	
+	/**
+	 * @param serviceUrl
+	 */
+	public DataFileQueryServiceImpl(String serviceUrl) {
+		this.serviceUrl = serviceUrl;
+	}
+
+
+}
