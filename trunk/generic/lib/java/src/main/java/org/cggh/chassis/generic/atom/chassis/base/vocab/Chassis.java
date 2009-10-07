9515f93938208d887abdc0fc2022a91e864c012e
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-07 09:19:04 +0000

    refactored to common location

---
+++ trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/atom/chassis/base/vocab/Chassis.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.atom.chassis.base.vocab;
+
+/**
+ * @author aliman
+ *
+ */
+public class Chassis {
+	
+	public static final String REL_STUDY = "chassis.study";
+	public static final String NSURI = "http://www.cggh.org/2009/chassis/atom/xmlns";
+	public static final String ELEMENT_STUDY = "study";
+	public static final String ELEMENT_MODULE = "module";
+	public static final String PREFIX = "chassis";
+	public static final String ELEMENT_SUBMISSION = "submission";
+	public static final String REL_SUBMISSION = "chassis.submission";
+
+}
