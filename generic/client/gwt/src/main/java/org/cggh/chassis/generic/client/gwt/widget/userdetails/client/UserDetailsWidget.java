c7e59c94f8775b1215a24dc799d0f20b6a4fcd7a
krishna.rao@well.ox.ac.uk <krishna.rao@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-07 10:05:20 +0000

    Reverted UserDetails widget constructor back to requiring a GWTAsync service to be passed in.

--- generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/userdetails/client/UserDetailsWidget.java
+++ generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/userdetails/client/UserDetailsWidget.java
@@ -6,12 +6,8 @@ package org.cggh.chassis.generic.client.gwt.widget.userdetails.client;
 import java.util.HashSet;
 import java.util.Set;
 
-import org.cggh.chassis.generic.client.gwt.configuration.client.ConfigurationBean;
-import org.cggh.chassis.generic.user.gwtrpc.client.GWTUserDetailsService;
 import org.cggh.chassis.generic.user.gwtrpc.client.GWTUserDetailsServiceAsync;
 
-import com.google.gwt.core.client.GWT;
-import com.google.gwt.user.client.rpc.ServiceDefTarget;
 import com.google.gwt.user.client.ui.Panel;
 
 /**
@@ -25,13 +21,13 @@ public class UserDetailsWidget implements UserDetailsWidgetAPI {
 	final private UserDetailsWidgetDefaultRenderer renderer;
 	final private Set<UserDetailsUserIdPubSubAPI> listeners = new HashSet<UserDetailsUserIdPubSubAPI>();
 
-	public UserDetailsWidget(Panel canvas) {
+	public UserDetailsWidget(Panel canvas, GWTUserDetailsServiceAsync userService) {
 				
 		// instantiate a model
 		model = new UserDetailsWidgetModel();
 		
 		// instantiate a controller
-		controller = new UserDetailsWidgetController(model, this, getUserService());
+		controller = new UserDetailsWidgetController(model, this, userService);
 		
 		// instantiate a renderer
 		renderer = new UserDetailsWidgetDefaultRenderer(canvas, controller);
@@ -41,13 +37,13 @@ public class UserDetailsWidget implements UserDetailsWidgetAPI {
 
 	}
 	
-	public UserDetailsWidget(UserDetailsWidgetDefaultRenderer customRenderer) {
+	public UserDetailsWidget(UserDetailsWidgetDefaultRenderer customRenderer, GWTUserDetailsServiceAsync userService) {
 
 		// instantiate a model
 		model = new UserDetailsWidgetModel();
 		
 		// instantiate a controller
-		controller = new UserDetailsWidgetController(model, this, getUserService());
+		controller = new UserDetailsWidgetController(model, this, userService);
 		
 		// set renderer
 		renderer = customRenderer;
@@ -58,20 +54,6 @@ public class UserDetailsWidget implements UserDetailsWidgetAPI {
 		// register renderer as listener to model
 		model.addListener(renderer);
 
-	}
-	
-	
-	
-	private GWTUserDetailsServiceAsync getUserService() {
-	
-		// instantiate service
-		GWTUserDetailsServiceAsync userService = GWT.create(GWTUserDetailsService.class);
-		
-		// set service URL
-		ServiceDefTarget target = (ServiceDefTarget) userService;
-		target.setServiceEntryPoint(ConfigurationBean.getUserDetailsServiceEndpointURL());
-		
-		return userService;
 	}
 
 	/* (non-Javadoc)
