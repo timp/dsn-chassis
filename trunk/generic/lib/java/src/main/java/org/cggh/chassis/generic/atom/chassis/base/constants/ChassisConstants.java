b8bf539a6c062b3ff9d10d51db87ef70283a0fc0
krishna.rao@well.ox.ac.uk <krishna.rao@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-12 16:05:14 +0000

    Commit of chassisConstants.

---
+++ trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/atom/chassis/base/constants/ChassisConstants.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.atom.chassis.base.constants;
+
+/**
+ * @author raok
+ *
+ */
+public class ChassisConstants {
+
+	//define roleIds
+	public static final Integer coordinatorRoleId = new Integer(0);
+	public static final Integer curatorRoleId = new Integer(1);
+	public static final Integer gatekeeperRoleId = new Integer(2);
+	public static final Integer submitterRoleId = new Integer(3);
+	public static final Integer userRoleId = new Integer(4);
+
+}
