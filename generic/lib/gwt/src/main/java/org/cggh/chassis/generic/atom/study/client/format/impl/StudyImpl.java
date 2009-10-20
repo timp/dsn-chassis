c38a65f103b76582faba63330e5ed8c52e9b4981
krishna.rao@well.ox.ac.uk <krishna.rao@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-20 08:41:20 +0000

    Some extra Study meta data added to Study Entry.

--- generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/study/client/format/impl/StudyImpl.java
+++ generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/study/client/format/impl/StudyImpl.java
@@ -5,6 +5,7 @@ package org.cggh.chassis.generic.atom.study.client.format.impl;
 
 import java.util.List;
 
+import org.cggh.chassis.generic.atom.chassis.base.client.format.ChassisFormatException;
 import org.cggh.chassis.generic.atom.chassis.base.vocab.Chassis;
 import org.cggh.chassis.generic.atom.study.client.format.Study;
 import org.cggh.chassis.generic.atom.study.client.format.StudyFactory;
@@ -62,6 +63,34 @@ public class StudyImpl extends ElementWrapperImpl implements Study {
 	public void setModules(List<String> modules) {
 //		XML.setElementsSimpleContentsByLocalName(element, Chassis.ELEMENT_MODULE, modules);
 		XMLNS.setElementsSimpleContentsByTagNameNS(element, Chassis.ELEMENT_MODULE, Chassis.PREFIX, Chassis.NSURI, modules);
+	}
+
+	public int getEndYear() throws ChassisFormatException {
+		String content = XMLNS.getFirstElementSimpleContentByTagNameNS(element, Chassis.ELEMENT_ENDYEAR, Chassis.NSURI);
+		int year = -1;
+		try {
+			year = Integer.parseInt(content);
+		}
+		catch (NumberFormatException e) {
+			throw new ChassisFormatException("could not parse value "+content+" as integer");
+		}
+		// TODO check range of year???
+		return year;
+	}
+
+	public int getStartYear() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public void setEndYear(int endYear) {
+		String content = Integer.toString(endYear);
+		XMLNS.setSingleElementSimpleContentByTagNameNS(element, Chassis.ELEMENT_ENDYEAR, Chassis.PREFIX, Chassis.NSURI, content);
+	}
+
+	public void setStartYear(int startYear) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
