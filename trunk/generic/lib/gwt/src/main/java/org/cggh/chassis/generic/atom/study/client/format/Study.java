374921250e7726502cd5b6a99912e9a1dd154d69
krishna.rao@well.ox.ac.uk <krishna.rao@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-20 08:41:20 +0000

    Some extra Study meta data added to Study Entry.

--- trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/study/client/format/Study.java
+++ trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atom/study/client/format/Study.java
@@ -5,6 +5,7 @@ package org.cggh.chassis.generic.atom.study.client.format;
 
 import java.util.List;
 
+import org.cggh.chassis.generic.atom.chassis.base.client.format.ChassisFormatException;
 import org.cggh.chassis.generic.atom.vanilla.client.format.ElementWrapper;
 
 /**
@@ -17,5 +18,11 @@ public interface Study extends ElementWrapper {
 	public void setModules(List<String> modules);
 	public void addModule(String module);
 	public void removeModule(String module);
+	
+	public void setStartYear(int startYear);
+	public int getStartYear();
+	public void setEndYear(int endYear);
+	public int getEndYear() throws ChassisFormatException;
+	
 
 }
