95a503a8e7361d8360e5d0f0315ea215c2b8caa1
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-10-30 13:43:45 +0000

    factored out some logic and delegated to controller

---
+++ generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/submission/view/client/ViewSubmissionWidgetController.java
@@ -0,0 +1,118 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.client.gwt.widget.submission.view.client;
+
+import org.cggh.chassis.generic.atom.submission.client.format.SubmissionEntry;
+import org.cggh.chassis.generic.atom.submission.client.format.SubmissionFactory;
+import org.cggh.chassis.generic.atom.submission.client.format.impl.SubmissionFactoryImpl;
+import org.cggh.chassis.generic.atom.vanilla.client.format.AtomEntry;
+import org.cggh.chassis.generic.atom.vanilla.client.protocol.AtomService;
+import org.cggh.chassis.generic.atom.vanilla.client.protocol.impl.AtomServiceImpl;
+import org.cggh.chassis.generic.client.gwt.configuration.client.ConfigurationBean;
+import org.cggh.chassis.generic.log.client.Log;
+import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.twisted.client.Deferred;
+import org.cggh.chassis.generic.twisted.client.Function;
+
+/**
+ * @author aliman
+ *
+ */
+public class ViewSubmissionWidgetController {
+
+	
+	
+	
+	private Log log = LogFactory.getLog(ViewSubmissionWidgetController.class);
+	private ViewSubmissionWidgetModel model;
+	private SubmissionFactory submissionFactory;
+	private AtomService service;
+
+	
+	
+	public ViewSubmissionWidgetController(ViewSubmissionWidgetModel model) {
+		log.enter("<constructor>");
+		
+		this.model = model;
+
+		log.debug("instantiate helpers");
+		this.submissionFactory = new SubmissionFactoryImpl();
+		this.service = new AtomServiceImpl(this.submissionFactory);
+		
+		log.leave();
+	}
+	
+	
+
+
+	/**
+	 * Set the submission entry to display properties for.
+	 * 
+	 * @param entry
+	 */
+	public void setSubmissionEntry(SubmissionEntry entry) {
+		log.enter("setSubmissionEntry");
+
+		this.model.setSubmissionEntry(entry);
+		this.model.setStatus(ViewSubmissionWidgetModel.STATUS_READY);
+
+		// should not need to do anything else, renderer will automatically
+		// update UI on submission entry change
+		
+		log.leave();
+	}
+
+
+
+
+
+	public void retrieveSubmissionEntry(String submissionEntryUrl) {
+		log.enter("retrieveSubmissionEntry");
+		
+		log.debug("retrieving entry: " + submissionEntryUrl);
+
+		model.setStatus(ViewSubmissionWidgetModel.STATUS_RETRIEVE_PENDING);
+		
+		//request submissionEntry
+		Deferred<AtomEntry> deferred = service.getEntry(ConfigurationBean.getSubmissionFeedURL() + submissionEntryUrl);
+		
+		//add callbacks
+		deferred.addCallback(new RetrieveSubmissionEntryCallback());
+		deferred.addErrback(new RetrieveSubmissionEntryErrback());
+		
+		log.leave();
+	}
+
+	
+	
+	
+	private class RetrieveSubmissionEntryCallback implements Function<SubmissionEntry,SubmissionEntry> {
+
+		public SubmissionEntry apply(SubmissionEntry submissionEntry) {
+			
+			model.setSubmissionEntry(submissionEntry);
+			model.setStatus(ViewSubmissionWidgetModel.STATUS_READY);
+
+			return submissionEntry;
+		}
+		
+	}
+	
+	
+	
+	
+	private class RetrieveSubmissionEntryErrback implements Function<Throwable,Throwable> {
+
+		public Throwable apply(Throwable error) {
+			model.setStatus(ViewSubmissionWidgetModel.STATUS_ERROR);
+			return error;
+		}
+		
+	}
+	
+	
+	
+	
+	
+}
