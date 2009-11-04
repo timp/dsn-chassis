6acaa69cb655e2815341f491eadfbbec132efbec
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-11-04 09:54:25 +0000

    mid refactor of atom form framework

---
+++ generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/forms/client/BaseForm.java
@@ -0,0 +1,210 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.client.gwt.forms.client;
+
+import org.cggh.chassis.generic.atom.rewrite.client.AtomAuthor;
+import org.cggh.chassis.generic.atom.rewrite.client.AtomEntry;
+import org.cggh.chassis.generic.atom.rewrite.client.AtomFactory;
+import org.cggh.chassis.generic.atom.rewrite.client.AtomFeed;
+import org.cggh.chassis.generic.client.gwt.common.client.ChassisResources;
+import org.cggh.chassis.generic.client.gwt.common.client.ChassisUser;
+import org.cggh.chassis.generic.log.client.Log;
+import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.widget.client.ChassisWidget;
+
+/**
+ * @author aliman
+ *
+ */
+public abstract class BaseForm<E extends AtomEntry, F extends AtomFeed<E>> 
+	extends ChassisWidget {
+
+	
+	
+	
+	private Log log = LogFactory.getLog(BaseForm.class);
+	private E model;
+	private AtomFactory<E, F> factory;
+	private BaseFormRenderer<E> renderer;
+
+
+	
+	
+	protected BaseForm(AtomFactory<E, F> factory) {
+		this.factory = factory;
+	}
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.widget.client.ChassisWidget#getName()
+	 */
+	@Override
+	protected String getName() {
+		return "baseForm";
+	}
+
+
+	
+	
+	private void ensureLog() {
+		if (log == null) log = LogFactory.getLog(BaseForm.class);
+	}
+
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.widget.client.ChassisWidget#init()
+	 */
+	@Override
+	public void init() {
+		ensureLog();
+		log.enter("init");
+
+		this.model = this.createModel();
+		this.renderer = this.createRenderer();
+		this.renderer.setCanvas(this.contentBox);
+
+		log.leave();
+
+	}
+	
+	
+	
+	
+	protected E createModel() {
+		E model = this.factory.createEntry();
+		AtomAuthor author = this.factory.createAuthor();
+		author.setEmail(ChassisUser.getCurrentUserEmail());
+		model.addAuthor(author);
+		return model;
+	}
+	
+	
+	
+	
+	protected abstract BaseFormRenderer<E> createRenderer();
+
+
+	
+	
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.widget.client.ChassisWidget#renderUI()
+	 */
+	@Override
+	protected void renderUI() {
+		log.enter("renderUI");
+
+		// delegate
+		this.renderer.renderUI();
+
+		log.leave();
+	}
+
+
+	
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.widget.client.ChassisWidget#bindUI()
+	 */
+	@Override
+	protected void bindUI() {
+		log.enter("bindUI");
+
+		// delegate
+		this.renderer.bindUI(this.model);
+
+		log.leave();
+	}
+	
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.widget.client.ChassisWidget#syncUI()
+	 */
+	@Override
+	protected void syncUI() {
+		log.enter("syncUI");
+
+		// delegate
+		this.renderer.syncUI();
+
+		log.leave();
+	}
+
+	
+	
+	
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.widget.client.ChassisWidget#unbindUI()
+	 */
+	@Override
+	protected void unbindUI() {
+		log.enter("unbindUI");
+
+		// delegate
+		this.renderer.unbindUI();
+
+		log.leave();
+	}
+
+	
+	
+	
+
+	/* (non-Javadoc)
+	 * @see org.cggh.chassis.generic.widget.client.ChassisWidget#destroy()
+	 */
+	@Override
+	public void destroy() {
+		log.enter("destroy");
+
+		this.unbindUI();
+		
+		// TODO anything else?
+
+		log.leave();
+	}
+	
+	
+	
+	
+	public void setModel(E model) {
+		this.unbindUI();
+		this.model = model;
+		this.bindUI();
+		this.syncUI();
+	}
+	
+	
+	
+	
+	public static class Resources {
+		
+		public static final String HEADINGTITLEANDSUMMARY = "HEADINGTITLEANDSUMMARY";
+		public static final String QUESTIONLABELTITLE = "QUESTIONLABELTITLE";
+		public static final String QUESTIONLABELSUMMARY = "QUESTIONLABELSUMMARY";
+		
+		private String className;
+		
+		public Resources(String className) {
+			this.className = className;
+		}
+		
+		public String get(String key) {
+			return ChassisResources.get(this.className, ChassisUser.getLang(), key);
+		}
+		
+	}
+
+
+
+
+
+
+	
+}
---
+++ generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/forms/client/BaseFormRenderer.java
@@ -0,0 +1,267 @@
+/**
+ * 
+ */
+package org.cggh.chassis.generic.client.gwt.forms.client;
+
+import org.cggh.chassis.generic.atom.rewrite.client.AtomEntry;
+import org.cggh.chassis.generic.client.gwt.common.client.CSS;
+import org.cggh.chassis.generic.client.gwt.forms.client.BaseForm.Resources;
+import org.cggh.chassis.generic.log.client.Log;
+import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.widget.client.ChassisWidgetRenderer;
+
+import com.google.gwt.event.logical.shared.ValueChangeEvent;
+import com.google.gwt.event.logical.shared.ValueChangeHandler;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.InlineLabel;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.TextArea;
+import com.google.gwt.user.client.ui.TextBox;
+import com.google.gwt.user.client.ui.TextBoxBase;
+
+/**
+ * @author aliman
+ *
+ */
+public abstract class BaseFormRenderer<E extends AtomEntry> 
+	extends ChassisWidgetRenderer {
+
+	
+	
+	private Log log = LogFactory.getLog(BaseFormRenderer.class);
+	private Resources resources;
+	protected TextBoxBase titleInput, summaryInput;
+	protected FlowPanel titleQuestionPanel, summaryQuestionPanel;
+	protected E model;
+
+	
+	
+	
+	public BaseFormRenderer() {
+		this.resources = this.createResources();
+	}
+	
+	
+	
+	
+	/**
+	 * 
+	 */
+	public void renderUI() {
+		log.enter("renderUI");
+		
+		this.canvas.add(new HTML("<h3>"+resources.get(Resources.HEADINGTITLEANDSUMMARY)+"</h3>"));
+		this.initTitleQuestion();
+		this.canvas.add(this.titleQuestionPanel);
+		this.initSummaryQuestion();
+		this.canvas.add(this.summaryQuestionPanel);			
+		
+		log.leave();
+		
+	}
+
+
+	
+	
+
+	/**
+	 * 
+	 */
+	protected void initTitleQuestion() {
+		log.enter("initTitleQuestion");
+
+		this.titleQuestionPanel = new FlowPanel();
+		this.titleInput = new TextBox();
+
+		InlineLabel titleLabel = new InlineLabel(text(Resources.QUESTIONLABELTITLE)); 
+		this.titleQuestionPanel.add(titleLabel);
+		this.titleQuestionPanel.add(titleInput);
+		this.titleQuestionPanel.addStyleName(CSS.COMMON_QUESTION);
+
+		log.leave();
+	}
+
+
+
+
+
+	/**
+	 * 
+	 */
+	protected void initSummaryQuestion() {
+		log.enter("initSummaryQuestion");
+
+		this.summaryQuestionPanel = new FlowPanel();
+		this.summaryInput = new TextArea();
+		
+		Label summaryLabel = new Label(text(Resources.QUESTIONLABELSUMMARY));
+		summaryQuestionPanel.add(summaryLabel);
+		summaryQuestionPanel.add(summaryInput);
+		summaryQuestionPanel.addStyleName(CSS.COMMON_QUESTION);
+		
+		log.leave();		
+	}
+
+	
+
+
+	/**
+	 * 
+	 */
+	public void bindUI(E model) {
+		log.enter("bindUI");
+		
+		log.debug("unbind to clear anything");
+		this.unbindUI();
+		
+		log.debug("keep reference to model");
+		this.model = model;
+		
+		log.debug("register this as handler for model property change events");
+		this.registerHandlersForModelChanges();
+		
+		log.debug("register handlers for child widget events");
+		this.registerHandlersForChildWidgetEvents();
+		
+		log.leave();
+		
+	}
+
+
+
+
+	/**
+	 * 
+	 */
+	protected void registerHandlersForModelChanges() {
+		log.enter("registerHandlersForModelChanges");
+		
+		// not needed
+		
+		log.leave();
+		
+	}
+
+
+
+
+	/**
+	 * 
+	 */
+	protected void registerHandlersForChildWidgetEvents() {
+		log.enter("registerHandlersForChildWidgetEvents");
+		
+		HandlerRegistration a = this.titleInput.addValueChangeHandler(new TitleInputChangeHandler());
+		HandlerRegistration b = this.summaryInput.addValueChangeHandler(new SummaryInputChangeHandler());
+		
+		this.childWidgetEventHandlerRegistrations.add(a);
+		this.childWidgetEventHandlerRegistrations.add(b);
+		
+		log.leave();
+		
+	}
+
+	
+	
+	
+	
+	protected class TitleInputChangeHandler implements ValueChangeHandler<String> {
+		
+		public void onValueChange(ValueChangeEvent<String> arg0) {
+			model.setTitle(arg0.getValue());
+		}
+		
+	}
+
+	
+	
+	
+	protected class SummaryInputChangeHandler implements ValueChangeHandler<String> {
+
+		public void onValueChange(ValueChangeEvent<String> arg0) {
+			model.setSummary(arg0.getValue());
+		}
+		
+	}
+	
+	
+	
+	
+
+
+
+
+	/**
+	 * 
+	 */
+	public void syncUI() {
+		log.enter("syncUI");
+		
+		if (this.model != null) {
+
+			this.updateTitle();
+			this.updateSummary();
+			
+		}
+		else {
+
+			// TODO could this method legitimately be called when model is null,
+			// or should we throw an error here if model is null?
+			log.warn("model is null, not updating anything");
+
+		}
+
+		log.leave();
+	}
+
+
+
+
+	/**
+	 * 
+	 */
+	private void updateTitle() {
+		log.enter("updateTitle");
+		
+		String title = this.model.getTitle();
+		if (title == null) title = "";
+		this.titleInput.setText(title);
+		
+		log.leave();
+		
+	}
+
+
+
+
+	/**
+	 * 
+	 */
+	private void updateSummary() {
+		log.enter("updateSummary");
+		
+		String summary = this.model.getSummary();
+		if (summary == null) summary = "";
+		this.summaryInput.setText(summary);
+		
+		log.leave();
+	}
+
+
+
+
+	protected abstract Resources createResources();
+	
+	
+	
+	
+	protected String text(String key) {
+		return this.resources.get(key);
+	}
+	
+	
+	
+
+}
