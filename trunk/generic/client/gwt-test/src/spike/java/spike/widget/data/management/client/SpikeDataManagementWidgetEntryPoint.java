4ec7502108b1fa83fd7491861ba06dc51153161e
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-11-05 18:55:20 +0000

    just started hack of data management widget

---
+++ trunk/generic/client/gwt-test/src/spike/java/spike/widget/data/management/client/SpikeDataManagementWidgetEntryPoint.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package spike.widget.data.management.client;
+
+import org.cggh.chassis.generic.client.gwt.common.client.ChassisUser;
+import org.cggh.chassis.generic.client.gwt.widget.data.client.DataManagementWidget;
+import org.cggh.chassis.generic.log.client.Log;
+import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.user.transfer.UserDetailsTO;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.MenuBar;
+import com.google.gwt.user.client.ui.Panel;
+import com.google.gwt.user.client.ui.RootPanel;
+
+/**
+ * @author aliman
+ *
+ */
+public class SpikeDataManagementWidgetEntryPoint implements EntryPoint {
+	private Log log = LogFactory
+			.getLog(SpikeDataManagementWidgetEntryPoint.class);
+
+	/* (non-Javadoc)
+	 * @see com.google.gwt.core.client.EntryPoint#onModuleLoad()
+	 */
+	public void onModuleLoad() {
+		log.enter("onModuleLoad");
+
+		UserDetailsTO user = new UserDetailsTO();
+		user.setId("alice@example.com");
+		ChassisUser.setCurrentUser(user);
+
+		Panel root = RootPanel.get("holdall");
+		
+		MenuBar menu = new MenuBar();
+		root.add(menu);
+		
+		DataManagementWidget w = new DataManagementWidget();
+		root.add(w);
+
+		menu.addItem("data", w.getMenu());
+
+		log.leave();
+	}
+
+}
