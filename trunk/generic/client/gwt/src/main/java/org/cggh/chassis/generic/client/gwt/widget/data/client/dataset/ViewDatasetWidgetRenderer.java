83b5fc21fc49a4df724d6316a9f43152d8139a38
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-11-12 19:24:46 +0000

    got view dataset working with basic atom properties

--- trunk/generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/data/client/dataset/ViewDatasetWidgetRenderer.java
+++ trunk/generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/data/client/dataset/ViewDatasetWidgetRenderer.java
@@ -3,13 +3,13 @@
  */
 package org.cggh.chassis.generic.client.gwt.widget.data.client.dataset;
 
+import org.cggh.chassis.generic.atomext.client.dataset.DatasetEntry;
 import org.cggh.chassis.generic.client.gwt.common.client.CommonStyles;
-import org.cggh.chassis.generic.client.gwt.widget.data.client.datafile.DataFilePropertiesWidget;
-import org.cggh.chassis.generic.client.gwt.widget.data.client.datafile.DataFileRevisionsWidget;
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetRenderer;
 
+import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.Panel;
@@ -76,6 +76,37 @@ public class ViewDatasetWidgetRenderer
 	
 	
 	
+	
+	/**
+	 * 
+	 */
+	@Override
+	protected void registerHandlersForModelChanges() {
+		log.enter("registerHandlersForModelChanges");
+		
+		super.registerHandlersForModelChanges();
+
+		HandlerRegistration b = this.model.addDatasetEntryChangeHandler(new DatasetEntryChangeHandler() {
+			
+			public void onChange(DatasetEntryChangeEvent e) {
+				log.enter("onDataFileEntryChanged");
+				
+				updateInfo(e.getAfter());
+				
+				log.leave();
+			}
+		});
+
+		log.debug("store registrations so can remove later if necessary");
+		this.modelChangeHandlerRegistrations.add(b);
+		
+		log.leave();
+	}
+
+
+
+
+	
 	/* (non-Javadoc)
 	 * @see org.cggh.chassis.generic.widget.client.ChassisWidgetRenderer#registerHandlersForChildWidgetEvents()
 	 */
@@ -89,6 +120,55 @@ public class ViewDatasetWidgetRenderer
 
 	}
 
+	
+	
+	
+
+	/**
+	 * 
+	 */
+	public void syncUI() {
+		log.enter("syncUI");
+		
+		super.syncUI();
+		
+		if (this.model != null) {
+
+			log.debug("sync properties");
+			DatasetEntry entry = this.model.getEntry();
+			this.updateInfo(entry);
+
+		}
+		else {
+
+			// TODO could this method legitimately be called when model is null,
+			// or should we throw an error here if model is null?
+			log.warn("model is null, not updating anything");
+
+		}
+		
+		log.leave();
+	}
+
+
+
+
+
+	/**
+	 * @param entry
+	 */
+	private void updateInfo(DatasetEntry entry) {
+		log.enter("updateInfo");
+		
+		if (entry != null) {
+			this.datasetPropertiesWidget.setEntry(entry);
+		}
+		else {
+			this.datasetPropertiesWidget.setEntry(null); // TODO review this, rather call reset() ?
+		}
+		
+		log.leave();
+	}
 	
 	
 	
