809855cf8c136a35874d518d20a47a5a61cc9807
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-11-13 16:06:49 +0000

    fixed all tests test suite to include xquestion

--- trunk/generic/lib/gwt-test/src/test/java/org/cggh/chassis/generic/AllGWTTests.java
+++ trunk/generic/lib/gwt-test/src/test/java/org/cggh/chassis/generic/AllGWTTests.java
@@ -16,14 +16,9 @@ public class AllGWTTests {
 		TestSuite suite = new TestSuite("Chassis Generic Lib GWT - All GWT Tests");
 		//$JUnit-BEGIN$
 
-		// module org.cggh.chassis.generic.atom.vanilla.Atom
-		//suite.addTest(legacy.org.cggh.chassis.generic.atom.vanilla.AllGWTTests.suite());
-
-		// module org.cggh.chassis.generic.twisted.Twisted
 		suite.addTest(org.cggh.chassis.generic.async.AllGWTTests.suite());
-		
-		// module org.cggh.chassis.generic.xml.XML
 		suite.addTest(org.cggh.chassis.generic.xml.AllGWTTests.suite());
+		suite.addTest(org.cggh.chassis.generic.xquestion.AllGWTTests.suite());
 		
 		//$JUnit-END$
 		return suite;
