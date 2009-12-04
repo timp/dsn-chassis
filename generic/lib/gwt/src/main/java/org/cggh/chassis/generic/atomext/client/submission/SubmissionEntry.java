39e31da5863297a69bd4c6e2657a64abb9de3ab8
tim.pizey@gmail.com <tim.pizey@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-12-04 15:33:45 +0000

    Unused import

--- generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atomext/client/submission/SubmissionEntry.java
+++ generic/lib/gwt/src/main/java/org/cggh/chassis/generic/atomext/client/submission/SubmissionEntry.java
@@ -5,7 +5,6 @@ package org.cggh.chassis.generic.atomext.client.submission;
 
 import java.util.List;
 
-import org.cggh.chassis.generic.atom.client.AtomAuthor;
 import org.cggh.chassis.generic.atom.client.AtomEntry;
 import org.cggh.chassis.generic.atomext.client.dataset.DatasetLink;
 import org.cggh.chassis.generic.atomext.client.review.ReviewLink;
