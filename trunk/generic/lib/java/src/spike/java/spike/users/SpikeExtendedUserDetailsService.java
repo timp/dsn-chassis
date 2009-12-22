ee6b01a2e29b2b4c446791528488c2d5660980a6
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-12-22 12:57:01 +0000

    experimenting with spring security extensions

---
+++ trunk/generic/lib/java/src/spike/java/spike/users/SpikeExtendedUserDetailsService.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package spike.users;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+/**
+ * @author aliman
+ *
+ */
+public class SpikeExtendedUserDetailsService {
+
+	
+	
+	Log log = LogFactory.getLog(SpikeExtendedUserDetailsService.class);
+
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		
+	}
+
+}
