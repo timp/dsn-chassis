cf8464dfe0e94ac5f0bf02d5add2d6edb1767450
tim.pizey@gmail.com <tim.pizey@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-12-24 09:50:43 +0000

    Unused import

--- trunk/generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/submission/client/UpdateSubmissionSuccessHandler.java
+++ trunk/generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/submission/client/UpdateSubmissionSuccessHandler.java
@@ -1,8 +1,6 @@
 package org.cggh.chassis.generic.client.gwt.widget.submission.client;
 
-import org.cggh.chassis.generic.atomext.client.review.ReviewEntry;
 import org.cggh.chassis.generic.atomext.client.submission.SubmissionEntry;
-import org.cggh.chassis.generic.atomui.client.CreateSuccessHandler;
 import org.cggh.chassis.generic.atomui.client.UpdateSuccessHandler;
 
 
