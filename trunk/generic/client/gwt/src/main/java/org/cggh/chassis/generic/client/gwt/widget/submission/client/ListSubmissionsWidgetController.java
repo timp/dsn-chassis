83c535414ac3bd3ad152416bdd7f41067b02c401
tim.pizey@gmail.com <tim.pizey@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2009-12-24 10:40:25 +0000

    Format

--- trunk/generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/submission/client/ListSubmissionsWidgetController.java
+++ trunk/generic/client/gwt/src/main/java/org/cggh/chassis/generic/client/gwt/widget/submission/client/ListSubmissionsWidgetController.java
@@ -16,71 +16,63 @@ import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
 
 /**
  * @author aliman
- *
+ * 
  */
 public class ListSubmissionsWidgetController {
 
-	
-	
-	
 	private ListSubmissionsWidgetModel model;
 	private ListSubmissionsWidget owner;
-	
-	
-	
-	
+
 	private Log log = LogFactory.getLog(ListSubmissionsWidgetController.class);
-	
-	
-	
-	
-	public ListSubmissionsWidgetController(
-			ListSubmissionsWidget owner,
-			ListSubmissionsWidgetModel model
-	) {
-			
+
+	public ListSubmissionsWidgetController(ListSubmissionsWidget owner,
+			ListSubmissionsWidgetModel model) {
+
 		this.owner = owner;
 		this.model = model;
 	}
-	
-	
-	
+
 	public void refreshSubmissions() {
 		log.enter("refreshSubmissions");
 		if (model != null) {
-		
-		log.debug("set async pending status on model");
-		
-		this.model.setStatus(AsyncWidgetModel.STATUS_ASYNC_REQUEST_PENDING);
-		
-		log.debug("set up async request for submissions pending acceptance review");
-		
-		SubmissionQuery query = new SubmissionQuery();
-		
-		log.debug("execute service query");
-		
-		SubmissionQueryService service = new SubmissionQueryService(Configuration.getSubmissionQueryServiceUrl());
-		Deferred<SubmissionFeed> deferredResults = service.query(query);
-		
-		log.debug("add callbacks");
-		deferredResults.addCallback(new RefreshSubmissionsCallback());
-		deferredResults.addErrback(new AsyncErrback(this.owner, this.model));
-		}		
+
+			log.debug("set async pending status on model");
+
+			this.model.setStatus(AsyncWidgetModel.STATUS_ASYNC_REQUEST_PENDING);
+
+			log
+					.debug("set up async request for submissions pending acceptance review");
+
+			SubmissionQuery query = new SubmissionQuery();
+
+			log.debug("execute service query");
+
+			SubmissionQueryService service = new SubmissionQueryService(
+					Configuration.getSubmissionQueryServiceUrl());
+			Deferred<SubmissionFeed> deferredResults = service.query(query);
+
+			log.debug("add callbacks");
+			deferredResults.addCallback(new RefreshSubmissionsCallback());
+			deferredResults
+					.addErrback(new AsyncErrback(this.owner, this.model));
+		}
 		log.leave();
 	}
-	
-	
-	
 
 	/**
 	 * @author aliman
-	 *
+	 * 
 	 */
-	public class RefreshSubmissionsCallback implements Function<SubmissionFeed, SubmissionFeed> {
+	public class RefreshSubmissionsCallback implements
+			Function<SubmissionFeed, SubmissionFeed> {
 		private Log log = LogFactory.getLog(RefreshSubmissionsCallback.class);
 
-		/* (non-Javadoc)
-		 * @see org.cggh.chassis.generic.async.client.Function#apply(java.lang.Object)
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see
+		 * org.cggh.chassis.generic.async.client.Function#apply(java.lang.Object
+		 * )
 		 */
 		public SubmissionFeed apply(SubmissionFeed in) {
 			log.enter("apply");
@@ -93,8 +85,5 @@ public class ListSubmissionsWidgetController {
 		}
 
 	}
-
-
-
 
 }
