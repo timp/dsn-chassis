def251795049611adfefe7412619560db91eec6f
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-01-06 16:56:59 +0000

    fix svn issues

---
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/HomeNavigationHandler.java
@@ -0,0 +1,9 @@
+package org.cggh.chassis.wwarn.ui.submitter.client;
+
+import com.google.gwt.event.shared.EventHandler;
+
+public interface HomeNavigationHandler extends EventHandler {
+
+	void onNavigation(HomeNavigationEvent e);
+
+}
