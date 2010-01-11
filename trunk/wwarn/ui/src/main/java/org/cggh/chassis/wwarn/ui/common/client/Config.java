cec1558665bee208693e68c71600313682c936d7
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-01-11 15:17:01 +0000

    refactored and added some config features

---
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/common/client/Config.java
@@ -0,0 +1,27 @@
+package org.cggh.chassis.wwarn.ui.common.client;
+
+public class Config {
+
+	
+	
+	
+	public static final String USER_EMAIL = "user.email";
+	public static final String COLLECTION_STUDIES_URL = "collection.studies.url";
+	public static final String COLLECTION_MEDIA_URL = "collection.media.url";
+	public static final String COLLECTION_SUBMISSIONS_URL = "collection.submissions.url";
+	public static final String COLLECTION_REVIEWS_URL = "collection.reviews.url";
+	public static final String QUERY_STUDIES_URL = "query.studies.url";
+	public static final String QUERY_SUBMISSIONS_URL = "query.submissions.url";
+	public static final String QUERY_MEDIA_URL = "query.media.url";
+	
+	
+	
+	
+	public static native String get(String key) /*-{
+		return $wnd.config[key];
+	}-*/;
+	
+	
+	
+	
+}
