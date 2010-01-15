04ae2ea8403c15282f1c3c3c22aff08e18c3d888
tim.pizey@gmail.com <tim.pizey@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-01-15 18:42:32 +0000

    Home time

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/SelectStudyWidgetController.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/SelectStudyWidgetController.java
@@ -11,18 +11,22 @@ import org.cggh.chassis.generic.atomext.client.study.StudyQuery;
 import org.cggh.chassis.generic.atomext.client.study.StudyQueryService;
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.miniatom.client.Atom;
 import org.cggh.chassis.generic.widget.client.AsyncErrback;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
 import org.cggh.chassis.generic.widget.client.ChassisWidget;
+import org.cggh.chassis.generic.widget.client.ErrorEvent;
 import org.cggh.chassis.wwarn.ui.common.client.Config;
 
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.xml.client.Document;
 
 /**
  * @author timp
  * @since 13/01/10
  */
 public class SelectStudyWidgetController {
+
 
 	private Log log = LogFactory.getLog(SelectStudyWidgetController.class);
 
@@ -37,7 +41,7 @@ public class SelectStudyWidgetController {
 		this.owner = owner;
 		this.model = model;
 	}
-	
+	/*
 	public Deferred<ChassisWidget> refreshAndCallBack() { 
 		Deferred<Map<String,String>> deferredStudyLinksToTitles = loadItems(true);
 		
@@ -79,9 +83,15 @@ public class SelectStudyWidgetController {
 		return null;
 		
 	}
+	*/
+	
+	
+	
 	/* (non-Javadoc)
 	 * @see org.cggh.chassis.generic.widget.client.MultiSelectModel#loadItems(boolean)
 	 */
+	
+	/*
 	public Deferred<Map<String, String>> loadItems(boolean forceRefresh) {
 		log.enter("loadItems");
 		Deferred<Map<String,String>> deferredItems;
@@ -105,8 +115,10 @@ public class SelectStudyWidgetController {
 		log.leave();
 		return deferredItems;
 	}
-	
+	*/
 
+
+/*	
 	public static Deferred<Map<String,String>> getMapOfStudyLinksToTitlesForCurrentUser() {
 		
 		StudyQueryService service = new StudyQueryService(Config.get(Config.QUERY_STUDIES_URL));
@@ -131,5 +143,48 @@ public class SelectStudyWidgetController {
 		return deferredMap;
 		
 	}
+	
+	*/
 
+	public void retrieveStudies() {
+		// Set the model's status to pending.
+		model.setStatus(SelectStudyWidgetModel.STATUS_RETRIEVE_STUDIES_PENDING);
+		
+		String studyQueryServiceUrl = Config.get(Config.QUERY_STUDIES_URL);
+		
+		Deferred<Document> deferredStudyFeedDoc = Atom.getFeed(studyQueryServiceUrl);
+		
+		// Add a call-back and error-back for the asynchronous feed.
+		deferredStudyFeedDoc.addCallback(new RetrieveStudiesCallback());
+		deferredStudyFeedDoc.addErrback(new RetrieveStudiesErrback());
+		
+		
+	}
+	
+	
+	
+	
+	private class RetrieveStudiesCallback implements Function<Document, Document> {
+
+		public Document apply(Document in) {
+			model.setStudyFeed(in);
+			model.setStatus(SelectStudyWidgetModel.STATUS_STUDIES_RETRIEVED);
+			return in;
+		}
+
+	}
+
+
+	private class RetrieveStudiesErrback  implements
+			Function<Throwable, Throwable> {
+
+		public Throwable apply(Throwable in) {
+			log.error("unexpected error", in);
+			model.setStatus(SelectStudyWidgetModel.STATUS_ERROR);
+			owner.fireEvent(new ErrorEvent(in));
+			owner.getRenderer().error(in.getMessage());
+			return in;
+		}
+       
+	}
 }
