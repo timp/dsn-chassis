ac901bff5b98a43368ed32a214623016e522c100
l33h4r7 <l33h4r7@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-01-18 15:32:48 +0000

    Added mock submission query results representing three entries across 2 studies with one shared file; Improved UI for SubmitterHomeWidget; Took comments out of SubmitterHomeWidgetModel and Renderer.

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/SubmitterHomeWidgetModel.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/SubmitterHomeWidgetModel.java
@@ -3,14 +3,9 @@
  */
 package org.cggh.chassis.wwarn.ui.submitter.client;
 
-import org.cggh.chassis.generic.log.client.Log;
-import org.cggh.chassis.generic.log.client.LogFactory;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
-import org.cggh.chassis.generic.widget.client.ModelChangeEvent;
-import org.cggh.chassis.generic.widget.client.ModelChangeHandler;
 import org.cggh.chassis.generic.widget.client.ObservableProperty;
 
-import com.google.gwt.event.shared.HandlerRegistration;
 import com.google.gwt.xml.client.Document;
 
 
@@ -20,11 +15,7 @@ import com.google.gwt.xml.client.Document;
  */
 public class SubmitterHomeWidgetModel extends AsyncWidgetModel {
 
-
-	private Log log = LogFactory.getLog(SubmitterHomeWidgetModel.class);
 	
-	
-//	private Document submissionFeed;
 	public final ObservableProperty<Document> submissionFeed = new ObservableProperty<Document>();
 	
 	
@@ -38,55 +29,6 @@ public class SubmitterHomeWidgetModel extends AsyncWidgetModel {
 		super.init();
 	}
 	
-//	public void setSubmissions(Document submissionsFeedDoc) {
-//		
-//		log.enter("setSubmissions");
-//
-//		SubmissionsChangeEvent e = new SubmissionsChangeEvent(this.submissionFeed, submissionsFeedDoc);
-//		
-//		this.submissionFeed = submissionsFeedDoc;
-//		
-//		this.fireChangeEvent(e);
-//		
-//		log.leave();
-//	}
-
-//	public Document getSubmissions() {
-//		return this.submissionFeed;
-//	}
-
-//	public static class SubmissionsChangeEvent extends ModelChangeEvent<Document, SubmissionsChangeHandler> {
-//
-//		public static final Type<SubmissionsChangeHandler> TYPE = new Type<SubmissionsChangeHandler>();
-//		
-//		public SubmissionsChangeEvent(Document before, Document after) {
-//			super(before, after);		
-//		}
-//
-//		@Override
-//		protected void dispatch(SubmissionsChangeHandler h) {
-//			h.onChange(this);
-//		}
-//		
-//		@Override
-//		public com.google.gwt.event.shared.GwtEvent.Type<SubmissionsChangeHandler> getAssociatedType() {
-//			return TYPE;
-//		}
-//
-//	}
-	
-//	public interface SubmissionsChangeHandler extends ModelChangeHandler {
-//
-//		public void onChange(SubmissionsChangeEvent submissionsChangeEvent);
-//		
-//
-//	}	
-	
-//	public HandlerRegistration addSubmissionsChangeHandler(SubmissionsChangeHandler h) {
-//		return this.addChangeHandler(h, SubmissionsChangeEvent.TYPE);
-//	}
-
-
 	
 	public static class RetrieveSubmissionsPendingStatus extends AsyncRequestPendingStatus {}
 	public static class SubmissionsNotFoundStatus extends ReadyStatus {}
