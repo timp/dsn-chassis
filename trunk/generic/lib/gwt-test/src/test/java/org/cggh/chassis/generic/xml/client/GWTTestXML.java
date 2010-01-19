5b3d5c09c1ec5e71ae49fec44492cca17613b173
alimanfoo@gmail.com <alimanfoo@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-01-19 16:40:31 +0000

    added non-namespace aware tests for simpel path selector utility

--- trunk/generic/lib/gwt-test/src/test/java/org/cggh/chassis/generic/xml/client/GWTTestXML.java
+++ trunk/generic/lib/gwt-test/src/test/java/org/cggh/chassis/generic/xml/client/GWTTestXML.java
@@ -4,6 +4,13 @@
 package org.cggh.chassis.generic.xml.client;
 
 
+import java.util.List;
+import java.util.Set;
+
+import org.cggh.chassis.generic.log.client.Log;
+import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.log.client.SystemOutLog;
+
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.xml.client.Document;
 import com.google.gwt.xml.client.Element;
@@ -18,6 +25,13 @@ import com.google.gwt.xml.client.XMLParser;
  */
 public class GWTTestXML extends GWTTestCase {
 
+	static {
+		LogFactory.create = SystemOutLog.create;
+	}
+	
+	static Log log = LogFactory.getLog(GWTTestXML.class);
+	
+	
 	/**
 	 * 
 	 */
@@ -92,6 +106,93 @@ public class GWTTestXML extends GWTTestCase {
 	}
 	
 	
+	
+	
+	public void testGetElementsBySimplePath_1() {
+		log.enter("testGetElementsBySimplePath_1");
+		
+		String xml = 
+			"<foo>" +
+				"<bar>" +
+					"<quux/>" +
+					"<spong/>" +
+				"</bar>" +
+				"<baz>" +
+					"<quux/>" +
+					"<quux/>" +
+				"</baz>" +
+				"<baz/>" +
+			"</foo>";
+
+		Document d = XMLParser.parse(xml);
+		Element e = d.getDocumentElement();
+
+		Set<Element> results; 
+		
+		results = XML.getElementsBySimplePath(d, "foo");
+		assertEquals(1, results.size());
+		
+		results = XML.getElementsBySimplePath(d, "bar");
+		assertEquals(0, results.size());
+
+		results = XML.getElementsBySimplePath(d, "/foo");
+		assertEquals(1, results.size());
+		
+		results = XML.getElementsBySimplePath(d, "/bar");
+		assertEquals(0, results.size());
+
+		results = XML.getElementsBySimplePath(e, "/foo");
+		assertEquals(1, results.size());
+		
+		results = XML.getElementsBySimplePath(e, "/bar");
+		assertEquals(0, results.size());
+
+		results = XML.getElementsBySimplePath(e, "bar");
+		assertEquals(1, results.size());
+
+		results = XML.getElementsBySimplePath(e, "baz");
+		assertEquals(2, results.size());
+
+		results = XML.getElementsBySimplePath(e, "*");
+		assertEquals(3, results.size());
+
+		results = XML.getElementsBySimplePath(e, "quux");
+		assertEquals(0, results.size());
+
+		results = XML.getElementsBySimplePath(e, "bar/quux");
+		assertEquals(1, results.size());
+
+		results = XML.getElementsBySimplePath(e, "baz/quux");
+		assertEquals(2, results.size());
+
+		results = XML.getElementsBySimplePath(e, "*/quux");
+		assertEquals(3, results.size());
+
+		results = XML.getElementsBySimplePath(e, "bar/*");
+		assertEquals(2, results.size());
+
+		results = XML.getElementsBySimplePath(e, "*/*");
+		assertEquals(4, results.size());
+
+		results = XML.getElementsBySimplePath(e, "bar/../baz");
+		assertEquals(2, results.size());
+
+		results = XML.getElementsBySimplePath(e, "*/*/..");
+		assertEquals(2, results.size());
+
+		results = XML.getElementsBySimplePath(e, "*/*/../quux");
+		assertEquals(3, results.size());
+
+		Element f = XML.getChildrenByTagName(e, "bar").get(0);
+		
+		results = XML.getElementsBySimplePath(f, "/foo");
+		assertEquals(1, results.size());
+		
+		results = XML.getElementsBySimplePath(f, "/bar");
+		assertEquals(0, results.size());
+
+		log.leave();
+	}
 	
 	
 }
