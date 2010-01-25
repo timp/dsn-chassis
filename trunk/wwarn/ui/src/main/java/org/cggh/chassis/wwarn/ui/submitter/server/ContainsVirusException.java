9cd6668cfdfbec8a1ce19231525ddb997c55f7a8
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-01-25 23:51:37 +0000

    First cut of virus scanner, commented out as appears to consume stream

---
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/server/ContainsVirusException.java
@@ -0,0 +1,12 @@
+package org.cggh.chassis.wwarn.ui.submitter.server;
+
+public class ContainsVirusException extends RuntimeException {
+
+	private static final long serialVersionUID = 431170477888774174L;
+
+	public ContainsVirusException(String message) {
+		super(message);
+	}
+
+
+}
