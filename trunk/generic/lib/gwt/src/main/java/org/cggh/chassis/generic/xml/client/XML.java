99e22b121da370480e4517eb037e798c9faf78fd
Tim.Pizey <Tim.Pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-01-29 00:15:34 +0000

    Unused import

--- trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/xml/client/XML.java
+++ trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/xml/client/XML.java
@@ -5,7 +5,6 @@ package org.cggh.chassis.generic.xml.client;
 
 import java.util.ArrayList;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
