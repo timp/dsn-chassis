0f135b6d199e47ce5a249d29206ec1d8bba0c1cd
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-02-22 12:09:43 +0000

    Warnings: getting used to Idea

--- trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/PutAtomOnlyFilter.java
+++ trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/PutAtomOnlyFilter.java
@@ -3,14 +3,8 @@
  */
 package org.cggh.chassis.generic.http;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
-import org.springframework.security.context.SecurityContextHolder;
-import org.springframework.security.userdetails.UserDetails;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -23,8 +17,6 @@ import java.io.IOException;
  *
  */
 public class PutAtomOnlyFilter extends HttpFilter {
-
-	private Log log = LogFactory.getLog(this.getClass());
 
 	@Override
 	public void doHttpFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
