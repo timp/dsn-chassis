8d5d252c1aee276bf006709130469d2818ade419
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-02-22 15:11:42 +0000

    Copy of SmokeTest, committed to get into Eclipse

---
+++ htmlunit/src/test/java/org/cggh/chassis/generic/client/htmlunit/AtomAuthorisationTestCase.java
@@ -0,0 +1,91 @@
+package org.cggh.chassis.generic.client.htmlunit;
+
+import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import net.sourceforge.jwebunit.exception.TestingEngineResponseException;
+import net.sourceforge.jwebunit.junit.WebTestCase;
+
+/**
+ * @author Tim.Pizey
+ * @since 2010/02/22
+ *
+ */
+public class AtomAuthorisationTestCase extends WebTestCase {
+
+	public void testUiRunning() throws Exception {
+		try {
+			beginAt("/chassis-wwarn-ui/administrator");
+		} catch (TestingEngineResponseException e) {
+			assertEquals(401, e.getHttpStatusCode());
+		}
+		final DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
+		credentialsProvider.addCredentials("alice@example.org", "bar");
+
+		WebClient webClient = new WebClient();
+		webClient.setCredentialsProvider(credentialsProvider);
+		final HtmlPage administratorPage = webClient.getPage(url("/chassis-wwarn-ui/administrator"));
+		assertEquals("WWARN - Administrator // alice@example.org", administratorPage.getTitleText());
+		System.err.println(administratorPage.asXml());
+
+		final HtmlPage startPage = webClient.getPage(url("/chassis-wwarn-ui/submitter/index.jsp"));
+		assertEquals("WWARN - Submitter // alice@example.org", startPage.getTitleText());
+		System.err.println(startPage.asXml());
+		getTestContext().setAuthorization("alice@example.org","bar");
+		// TODO authorization not working
+		//gotoPage("/chassis-wwarn-ui/submitter/index.jsp");
+
+	}
+
+
+	public void testGenericRunning() throws Exception {
+		try {
+			beginAt("/chassis-generic-client-gwt/");
+		} catch (TestingEngineResponseException e) {
+			assertEquals(401, e.getHttpStatusCode());
+		}
+		final DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
+		credentialsProvider.addCredentials("alice@example.org", "bar");
+
+		WebClient webClient = new WebClient();
+		webClient.setCredentialsProvider(credentialsProvider);
+		final HtmlPage startPage = webClient
+				.getPage(url("/chassis-generic-client-gwt/"));
+		assertEquals("Chassis/*", startPage.getTitleText());
+	}
+
+	public void testWarnRunning() throws Exception {
+		try {
+			beginAt("/chassis-wwarn-client-gwt/");
+		} catch (TestingEngineResponseException e) {
+			assertEquals(401, e.getHttpStatusCode());
+		}
+		final DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
+		credentialsProvider.addCredentials("alice@example.org", "bar");
+
+		WebClient webClient = new WebClient();
+		webClient.setCredentialsProvider(credentialsProvider);
+		final HtmlPage startPage = webClient
+				.getPage(url("/chassis-wwarn-client-gwt/"));
+		assertEquals("Chassis/WWARN", startPage.getTitleText());
+	}
+
+    public void testCannotPutToNonAtom() throws Exception {
+        
+    }
+    public void testCanPutToAtom() throws Exception {
+
+    }
+
+	public void beginAt(String url) {
+		super.beginAt(url(url));
+	}
+	public void gotoPage(String url) {
+		super.gotoPage(url(url));
+	}
+
+    protected String url(String url) {
+		return "http://localhost:8080" + url;
+	}
+
+}
