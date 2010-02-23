070c45c0d8e43844d28c75382e07269bcefdbbb9
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-02-23 12:34:39 +0000

    Hardcode PUT

--- trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/PutAtomOnlyFilter.java
+++ trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/PutAtomOnlyFilter.java
@@ -1,12 +1,10 @@
-/**
- * 
- */
 package org.cggh.chassis.generic.http;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
 
 /**
@@ -21,7 +19,7 @@ public class PutAtomOnlyFilter extends HttpFilter {
 	@Override
 	public void doHttpFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
 
-        if (request.getMethod().equals("PUT"))   // HttpServlet.METHOD_PUT is private !!
+        if (request.getMethod().equals("PUT"))
             if (!request.getContentType().startsWith("application/atom+xml"))
                 response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Only Atom entries may be updated");
 
