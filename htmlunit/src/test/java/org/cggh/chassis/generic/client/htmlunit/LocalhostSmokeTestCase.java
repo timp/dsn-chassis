f7c64b3ecb5c2f5ad2b182d7a0f61f4f8ac5839e
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-02-24 17:26:26 +0000

    Add authorisation test, comment out old tests

--- htmlunit/src/test/java/org/cggh/chassis/generic/client/htmlunit/LocalhostSmokeTestCase.java
+++ htmlunit/src/test/java/org/cggh/chassis/generic/client/htmlunit/LocalhostSmokeTestCase.java
@@ -1,11 +1,19 @@
 package org.cggh.chassis.generic.client.htmlunit;
 
+/*
 import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
+*/
+import com.gargoylesoftware.htmlunit.DefaultCredentialsProvider;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.xml.XmlPage;
 
-import net.sourceforge.jwebunit.junit.WebTestCase;
 import net.sourceforge.jwebunit.exception.TestingEngineResponseException;
+import net.sourceforge.jwebunit.htmlunit.HtmlUnitTestingEngineImpl;
+import net.sourceforge.jwebunit.junit.WebTestCase;
+import net.sourceforge.jwebunit.util.TestingEngineRegistry;
 
 /**
  * @author Tim.Pizey
@@ -13,7 +21,63 @@ import net.sourceforge.jwebunit.exception.TestingEngineResponseException;
  * 
  */
 public class LocalhostSmokeTestCase extends WebTestCase {
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		getTester().setTestingEngineKey(TestingEngineRegistry.TESTING_ENGINE_HTMLUNIT);
+	}
 
+	protected void setAuthorization(String username, String password) { 
+		final DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
+		credentialsProvider.addCredentials(username, password);
+		getTestContext().setAuthorization(username, password);
+		getWebClient().setCredentialsProvider(credentialsProvider);
+	}
+	
+	WebClient getWebClient() { 
+		return ((HtmlUnitTestingEngineImpl)getTestingEngine()).
+		  getWebClient();
+	}
+	public void beginAt(String url) {
+		super.beginAt(url(url));
+	}
+	public void gotoPage(String url) {
+		super.gotoPage(url(url));
+	}
+	
+	public HtmlPage getPage(String url) throws Exception { 
+		return (HtmlPage)getWebClient().getPage(url(url));
+	}
+	public XmlPage getXmlPage(String url) throws Exception { 
+		return (XmlPage)getWebClient().getPage(url(url));
+	}
+
+	
+    protected String url(String url) {
+		return "http://localhost:8082" + url;
+	}
+
+    public void testAuthorisation() throws Exception {
+		String collectionUrl =  "/chassis-wwarn-ui/atom/edit/studies";
+		try {
+			beginAt(collectionUrl);
+		} catch (TestingEngineResponseException e) {
+			if (e.getHttpStatusCode() == 401)
+				setAuthorization("alice@example.org", "bar");
+			else
+				fail("Unexpected status " + e.getHttpStatusCode());
+		}
+		beginAt(collectionUrl);
+		XmlPage feed = getXmlPage(collectionUrl);
+		assertNull(feed.getNamespaceURI());
+		assertNull(feed.getLocalName());
+		assertNull(feed.getNodeValue());
+		assertEquals("#document", feed.getNodeName());
+		assertEquals("atom:feed", feed.getFirstChild().getNodeName());
+	}
+
+    /*
 	public void testUiRunning() throws Exception {
 		try {
 			beginAt("/chassis-wwarn-ui/administrator");
@@ -32,9 +96,8 @@ public class LocalhostSmokeTestCase extends WebTestCase {
 		final HtmlPage startPage = webClient.getPage(url("/chassis-wwarn-ui/submitter/index.jsp"));
 		assertEquals("WWARN - Submitter // alice@example.org", startPage.getTitleText());
 		System.err.println(startPage.asXml());
-		getTestContext().setAuthorization("alice@example.org","bar");
-		// TODO authorization not working 
-		//gotoPage("/chassis-wwarn-ui/submitter/index.jsp");
+		setAuthorization("alice@example.org","bar");
+		gotoPage("/chassis-wwarn-ui/submitter/index.jsp");
 		
 	}
 	
@@ -45,14 +108,10 @@ public class LocalhostSmokeTestCase extends WebTestCase {
 		} catch (TestingEngineResponseException e) {
 			assertEquals(401, e.getHttpStatusCode());
 		}
-		final DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
-		credentialsProvider.addCredentials("alice@example.org", "bar");
 
-		WebClient webClient = new WebClient();
-		webClient.setCredentialsProvider(credentialsProvider);
-		final HtmlPage startPage = webClient
-				.getPage(url("/chassis-generic-client-gwt/"));
-		assertEquals("Chassis/*", startPage.getTitleText());
+		setAuthorization("alice@example.org","bar");
+		assertEquals("Chassis/*", 
+				getPage("/chassis-generic-client-gwt/").getTitleText());
 	}
 
 	public void testWarnRunning() throws Exception {
@@ -60,26 +119,11 @@ public class LocalhostSmokeTestCase extends WebTestCase {
 			beginAt("/chassis-wwarn-client-gwt/");
 		} catch (TestingEngineResponseException e) {
 			assertEquals(401, e.getHttpStatusCode());
+			setAuthorization("alice@example.org","bar");
+			assertEquals("Chassis/WWARN", getPage("/chassis-wwarn-client-gwt/").getTitleText());
 		}
-		final DefaultCredentialsProvider credentialsProvider = new DefaultCredentialsProvider();
-		credentialsProvider.addCredentials("alice@example.org", "bar");
-
-		WebClient webClient = new WebClient();
-		webClient.setCredentialsProvider(credentialsProvider);
-		final HtmlPage startPage = webClient
-				.getPage(url("/chassis-wwarn-client-gwt/"));
-		assertEquals("Chassis/WWARN", startPage.getTitleText());
 	}
+*/
+    
 	
-	public void beginAt(String url) {
-		super.beginAt(url(url));
-	}
-	public void gotoPage(String url) {
-		super.gotoPage(url(url));
-	}
-
-	private String url(String url) {
-		return "http://localhost:8080" + url;
-	}
-
 }
