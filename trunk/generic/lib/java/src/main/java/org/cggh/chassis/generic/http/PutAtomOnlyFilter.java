3d7a06cedf753544da5f1fb72b53a4cfa211e28c
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-02-26 11:06:49 +0000

    R

--- trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/PutAtomOnlyFilter.java
+++ trunk/generic/lib/java/src/main/java/org/cggh/chassis/generic/http/PutAtomOnlyFilter.java
@@ -18,7 +18,6 @@ public class PutAtomOnlyFilter extends HttpFilter {
 
 	@Override
 	public void doHttpFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
-System.err.println("Called PutAtomOnlyFilter with " + request.getMethod());
         if (request.getMethod().equals("PUT") && !request.getContentType().startsWith("application/atom+xml"))
           response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Only Atom entries may be updated");
         else 
