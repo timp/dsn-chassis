aae20ec37fccf03e8b8c334a2cd58b13d00d40c9
Tim.Pizey <Tim.Pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-02-28 23:22:25 +0000

    Test put

--- generic/lib/java/src/test/java/org/cggh/chassis/generic/http/test/MockFilterChain.java
+++ generic/lib/java/src/test/java/org/cggh/chassis/generic/http/test/MockFilterChain.java
@@ -9,12 +9,36 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 
 public class MockFilterChain implements FilterChain {
-
+	private final static String ATOM_ENTRY = 	
+		"<atom:entry xmlns:atom=\"http://www.w3.org/2005/Atom\">" + 
+		"<atom:title>An Atomic Entry</atom:title>" + 
+        "<atom:author>" + 
+        "<atom:email>notalice@example.org</atom:email>" +
+        "</atom:author>" + 
+		"</atom:entry>" + 
+		"\n";
+	String returnFlag ="Nothing, 404";
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response)
 			throws IOException, ServletException {
-		HttpServletResponse httpResponse = (HttpServletResponse)response; 
-		httpResponse.setStatus(200);
+		
+		HttpServletResponse httpResponse = (HttpServletResponse)response;
+		
+		if (returnFlag.equals("Nothing, OK")) { 
+			httpResponse.setStatus(200);
+		} else if (returnFlag.equals("Not atom, OK")) {
+			httpResponse.getOutputStream().print("Not atom content");
+			httpResponse.setStatus(200);
+		} else if (returnFlag.equals("Atom Entry, OK")) {
+			httpResponse.getOutputStream().print(ATOM_ENTRY);
+			httpResponse.setStatus(200);
+		} else { 
+			httpResponse.setStatus(404);			
+		}
+	
+		
 	}
-
+    public void setReturnFlag(String returnFlag) { 
+    	this.returnFlag = returnFlag;
+    }
 }
