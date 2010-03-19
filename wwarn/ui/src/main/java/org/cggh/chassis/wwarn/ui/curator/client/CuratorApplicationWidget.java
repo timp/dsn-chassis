58dc14b4e3fc89cbc55e31d982e29cf2a7a0054b
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-19 12:54:16 +0000

    Organise imports

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorApplicationWidget.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorApplicationWidget.java
@@ -5,30 +5,11 @@ package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
-import org.cggh.chassis.generic.widget.client.ErrorEvent;
-import org.cggh.chassis.generic.widget.client.ErrorHandler;
 import org.cggh.chassis.generic.widget.client.MultiWidget;
 import org.cggh.chassis.generic.widget.client.WidgetEvent;
 import org.cggh.chassis.generic.widget.client.WidgetEventHandler;
 
-import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
-import org.cggh.chassis.wwarn.ui.submitter.client.AddInformationWidget;
-import org.cggh.chassis.wwarn.ui.submitter.client.BackToStartNavigationEvent;
-import org.cggh.chassis.wwarn.ui.submitter.client.BackToStartNavigationHandler;
-import org.cggh.chassis.wwarn.ui.submitter.client.HomeNavigationEvent;
-import org.cggh.chassis.wwarn.ui.submitter.client.HomeNavigationHandler;
-import org.cggh.chassis.wwarn.ui.submitter.client.ProceedActionEvent;
-import org.cggh.chassis.wwarn.ui.submitter.client.ProceedActionHandler;
-import org.cggh.chassis.wwarn.ui.submitter.client.SelectStudyWidget;
-import org.cggh.chassis.wwarn.ui.submitter.client.StepBackNavigationEvent;
-import org.cggh.chassis.wwarn.ui.submitter.client.StepBackNavigationHandler;
-import org.cggh.chassis.wwarn.ui.submitter.client.SubmitWidget;
-import org.cggh.chassis.wwarn.ui.submitter.client.SubmitterHomeWidget;
-import org.cggh.chassis.wwarn.ui.submitter.client.UploadFilesWidget;
-import org.cggh.chassis.wwarn.ui.submitter.client.UploadFilesWidgetModel;
-
 import com.google.gwt.event.shared.HandlerRegistration;
-import com.google.gwt.user.client.Window;
 
 /**
  *
