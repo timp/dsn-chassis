12456630c078123d4ed5b780de4e306c71c44731
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-20 11:34:43 +0000

    About to regenerate

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ListStudiesWidgetRenderer.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ListStudiesWidgetRenderer.java
@@ -1,40 +1,21 @@
 package org.cggh.chassis.wwarn.ui.curator.client;
 
-import java.util.List;
-
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
-import org.cggh.chassis.generic.miniatom.client.AtomHelper;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
 import org.cggh.chassis.generic.widget.client.ChassisWidgetRenderer;
-import org.cggh.chassis.generic.widget.client.PropertyChangeEvent;
-import org.cggh.chassis.generic.widget.client.PropertyChangeHandler;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.StatusChangeEvent;
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.StatusChangeHandler;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.event.dom.client.ChangeEvent;
-import com.google.gwt.event.dom.client.ChangeHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.uibinder.client.UiHandler;
 import com.google.gwt.uibinder.client.UiTemplate;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
-import com.google.gwt.user.client.ui.ListBox;
-import com.google.gwt.user.client.ui.TextArea;
-import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.xml.client.Document;
-import com.google.gwt.xml.client.Element;
 
 /**
- * BE SURE TO EDIT THE TEMPLATE NOT THE RENDERED RESULT
- *
- * DELETE_TO_MANUALLY_EDIT
  *
  * @author timp
  */
@@ -58,7 +39,8 @@ public class ListStudiesWidgetRenderer extends
 	@UiField FlowPanel errorMessage;
 
 	private ListStudiesWidget owner;
-  public ListStudiesWidgetRenderer(ListStudiesWidget owner) {
+	
+    public ListStudiesWidgetRenderer(ListStudiesWidget owner) {
 		this.owner = owner;
 	}
 	private ListStudiesWidgetController controller;
@@ -92,20 +74,15 @@ public class ListStudiesWidgetRenderer extends
 		
 		errorPanel.setVisible(false);	
 		if (status instanceof AsyncWidgetModel.InitialStatus) {
-
-			// Everything off, hidden.
+			pendingPanel.setVisible(true);	
 		}
-		
-		//TODO Widget specific statii
-		
+		else if (status instanceof AsyncWidgetModel.ReadyStatus) {
+			pendingPanel.setVisible(false);	
+		}			
 		else if (status instanceof AsyncWidgetModel.ErrorStatus) {
-
-			
 			error("Error status given on asynchronous call.");
 		}			
-		
 		else {
-
 			error("Unhandled status:" + status);
 		}
 
