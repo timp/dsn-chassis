f108ff8a2b8938403500fdeb271ca282a4419bab
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-20 18:51:35 +0000

    Tidy

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ListStudiesWidgetController.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ListStudiesWidgetController.java
@@ -6,14 +6,11 @@ import org.cggh.chassis.generic.async.client.Function;
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
 import org.cggh.chassis.generic.miniatom.client.Atom;
-import org.cggh.chassis.generic.miniatom.client.AtomHelper;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
-import org.cggh.chassis.generic.widget.client.ChassisWidget;
 import org.cggh.chassis.generic.widget.client.ErrorEvent;
 import org.cggh.chassis.wwarn.ui.common.client.Config;
 
 import com.google.gwt.xml.client.Document;
-import com.google.gwt.xml.client.Element;
 
 /**
  * @author timp
@@ -56,63 +53,6 @@ public class ListStudiesWidgetController {
 		return deferredDoc;
 	}
 	
-	/*
-	public Deferred<ChassisWidget> refreshAndCallback() {
-		log.enter("refreshAndCallback");
-		
-		final Deferred<ChassisWidget> deferredOwner = new Deferred<ChassisWidget>();
-		
-		Deferred<Element> chain = retrieveStudyFeed();
-			
-		chain.addCallback(new RetrieveStudyFeedCallback());
-			
-
-		chain.addErrback(new DefaultErrback());
-			
-		// finally callback with owner, in any case
-		chain.addBoth(new Function<Object, Object>() {
-
-			public Object apply(Object in) {
-				deferredOwner.callback(owner);
-				return in;
-			}
-			
-		});
-
-		
-		log.leave();
-		return deferredOwner;
-		
-	}
-*/
-/*	
-	private Deferred<Element> retrieveStudyFeed() {
-		log.enter("retrieveSubmission");
-		
-		model.status.set(ListStudiesWidgetModel.STATUS_RETRIEVE_STUDY_FEED_PENDING);
-		
-		String url = Config.get(Config.QUERY_STUDIES_URL);
-		log.debug("url: "+url);
-
-		log.debug("make get feed request");
-		Deferred<Document> deferredResultsFeedDoc = Atom.getFeed(url);
-		
-		Deferred<Element> deferredFeedElement = deferredResultsFeedDoc.adapt(new Function<Document, Element>() {
-
-			public Element apply(Document resultsFeedDoc) {
-				Element entryElement = AtomHelper.getFirstEntry(resultsFeedDoc.getDocumentElement());
-				return entryElement;
-			}
-			
-		});
-
-		log.leave();
-		return deferredFeedElement;
-	}
-
-	
-	
-*/		
 
 	private class DefaultErrback implements Function<Throwable, Throwable> {
 
@@ -140,13 +80,5 @@ public class ListStudiesWidgetController {
 
 
 
-	
-	
-        					
-  private ViewStudyNavigationEvent viewStudyNavigationEvent;
-	
-         	     	   	 
-	
-	
 
 }
