979b660924e181220cc1b16c99db1a17bb149d4f
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-21 23:46:39 +0000

    Organise imports

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidgetController.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidgetController.java
@@ -1,19 +1,8 @@
 
 package org.cggh.chassis.wwarn.ui.curator.client;
 
-import org.cggh.chassis.generic.async.client.Deferred;
-import org.cggh.chassis.generic.async.client.Function;
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
-import org.cggh.chassis.generic.miniatom.client.Atom;
-import org.cggh.chassis.generic.miniatom.client.AtomHelper;
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
-import org.cggh.chassis.generic.widget.client.ChassisWidget;
-import org.cggh.chassis.generic.widget.client.ErrorEvent;
-import org.cggh.chassis.wwarn.ui.common.client.Config;
-
-import com.google.gwt.xml.client.Document;
-import com.google.gwt.xml.client.Element;
 
 /**
  * @author timp
