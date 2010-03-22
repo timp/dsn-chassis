3b2c1329697119a5e052a034d4a5699e4e50894b
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-22 17:45:43 +0000

    Starting to work!!

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/StudySummaryWidget.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/StudySummaryWidget.java
@@ -3,20 +3,36 @@
  */
 package org.cggh.chassis.wwarn.ui.curator.client;
 
+import static org.cggh.chassis.generic.widget.client.HtmlElements.strongWidget;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.miniatom.client.AtomHelper;
+import org.cggh.chassis.generic.miniatom.client.ext.ChassisHelper;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
 import org.cggh.chassis.generic.widget.client.ChassisWidget;
 import org.cggh.chassis.generic.widget.client.ObservableProperty;
+import org.cggh.chassis.generic.widget.client.PropertyChangeEvent;
+import org.cggh.chassis.generic.widget.client.PropertyChangeHandler;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
+import org.cggh.chassis.wwarn.ui.common.client.RenderUtils;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.Anchor;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
+import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.xml.client.Document;
 import com.google.gwt.xml.client.Element;
 
 
@@ -30,8 +46,6 @@ public class StudySummaryWidget
 
 	private static final Log log = LogFactory.getLog(StudySummaryWidget.class);
 	
-	private Element studyEntry;
-
 	@UiTemplate("StudySummaryWidget.ui.xml")
 	interface StudySummaryWidgetRendererUiBinder extends
 			UiBinder<HTMLPanel, StudySummaryWidget> {
@@ -50,8 +64,7 @@ public class StudySummaryWidget
 
 	
 	
-	public final ObservableProperty<String> studyUrl = new ObservableProperty<String>();
-	public final ObservableProperty<Element> studyEntryElement = new ObservableProperty<Element>();
+	public final ObservableProperty<Element> studyEntry = new ObservableProperty<Element>();
 	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
 	public final ObservableProperty<String> message = new ObservableProperty<String>();
 
@@ -76,34 +89,121 @@ public class StudySummaryWidget
 	}	
 
 
-
-
-
-	// Using init() rather than constructor because reset() uses init().
-	public void init() {
-		
-		super.init();
-		
-	}
-	
 	
 	public void setStudyEntry(Element studyEntry) { 
-		this.studyEntry = studyEntry;
+		this.studyEntry.set(studyEntry);
 	}
 	
 	
 	@Override
 	protected void renderUI() {
-		// TODO Auto-generated method stub
-		super.renderUI();
+
+		this.clear();
+		this.add(uiBinder.createAndBindUi(this));
+		errorPanel.setVisible(false);	
+		
 	}
 
 
 	@Override
 	protected void syncUI() {
-		// TODO Auto-generated method stub
-		super.syncUI();
+		syncUIWithStatus(status.get());
+        syncUIWithStudyEntry(studyEntry.get());
+        registerHandlersForModelChanges();
+    }
+
+	protected void registerHandlersForModelChanges() {
+		log.enter("registerHandlersForModelChanges");
+		
+		status.addChangeHandler(new PropertyChangeHandler<Status>() {
+		
+			public void onChange(PropertyChangeEvent<Status> e) {
+				log.enter("onChange(status)");
+			
+				log.debug("Status " + e.getAfter());
+				syncUIWithStatus(e.getAfter());
+			
+				log.leave();
+			}
+		});
+		studyEntry.addChangeHandler(new PropertyChangeHandler<Element>() {
+			
+			public void onChange(PropertyChangeEvent<Element> e) {
+				log.enter("onChange(Element)");
+				syncUIWithStudyEntry(e.getAfter());
+				log.leave();
+			}
+			
+		});
+		log.leave();
 	}
+
+	private void syncUIWithStudyEntry(Element study){
+		log.enter("syncUIWithStudyEntry");
+		
+		if (study == null) 
+			log.debug("StudyFeed null");
+		else {
+			
+			List<Widget[]> rows = new ArrayList<Widget[]>();
+			
+			Widget[] headerRow = {
+				strongWidget("Study Title"), // TODO i18n
+				strongWidget("Modules"),     // TODO i18n
+				strongWidget("Submitters"),  // TODO i18n
+				strongWidget("Actions"),     // TODO i18n
+			};
+			
+			rows.add(headerRow);
+			
+			Widget[] row = {
+						new HTML(ChassisHelper.getTitle(study)),
+						new HTML(RenderUtils.join(ChassisHelper.getModules(study), ", ")),
+						new HTML(RenderUtils.join(ChassisHelper.getAuthorEmails(study), ", ")),
+						new HTML(RenderUtils.join(ChassisHelper.getAuthorEmails(study), ", ")),
+			};
+			rows.add(row);
+			
+			FlexTable table = RenderUtils.renderResultsTable(rows);
+			this.tablePanel.clear();
+			this.tablePanel.add(table);
+			pendingPanel.setVisible(false);	
+
+		}
+		
+		log.leave();
+	}
+
+	protected void syncUIWithStatus(Status status) {
+		log.enter("syncUIWithStatus");		
+		log.debug("status:" + status);
+		
+		if (status == null) {
+			// nothing to do yet
+		}
+		else if (status instanceof AsyncWidgetModel.InitialStatus) {
+			pendingPanel.setVisible(true);	
+		}
+		else if (status == ListStudiesWidgetModel.STATUS_RETRIEVE_STUDY_FEED_PENDING) {
+			// still pending
+		}			
+		else if (status == ListStudiesWidgetModel.STATUS_READY_FOR_INTERACTION) {
+			pendingPanel.setVisible(false);	
+		}			
+		else if (status instanceof AsyncWidgetModel.ReadyStatus) {
+			pendingPanel.setVisible(false);	
+		}			
+		else if (status instanceof AsyncWidgetModel.ErrorStatus) {
+			error("Error status given on asynchronous call.");
+		}			
+		else {
+			error("Unhandled status:" + status);
+		}
+		
+		log.leave();
+	}
+
+
 
 	@Override
 	public void refresh() {
