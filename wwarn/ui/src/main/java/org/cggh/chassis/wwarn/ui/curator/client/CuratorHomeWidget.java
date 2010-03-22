fc03530ad39ff2bdb56524a8bdfad23144661cde
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-22 23:35:19 +0000

    Make a chassis widget, no longer a delegating widget

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
@@ -2,8 +2,18 @@ package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.widget.client.ChassisWidget;
 import org.cggh.chassis.generic.widget.client.DelegatingWidget;
+import org.cggh.chassis.generic.widget.client.WidgetEvent;
 import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
+import org.cggh.chassis.generic.widget.client.WidgetEventHandler;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.event.shared.HandlerRegistration;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.HTMLPanel;
 
 /**
  *
@@ -11,41 +21,48 @@ import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
  *
  */
 public class CuratorHomeWidget 
-	 	extends DelegatingWidget<CuratorHomeWidgetModel, CuratorHomeWidgetRenderer> {
+	 	extends ChassisWidget {
 
 	private static final Log log = LogFactory.getLog(CuratorHomeWidget.class);
 	
-	private CuratorHomeWidgetController controller;
+	@UiTemplate("CuratorHomeWidget.ui.xml")
+	interface CuratorHomeWidgetRendererUiBinder extends
+			UiBinder<HTMLPanel, CuratorHomeWidget> {
+	}
+
+	private static CuratorHomeWidgetRendererUiBinder uiBinder =
+			GWT.create(CuratorHomeWidgetRendererUiBinder.class);
+
+	@UiField HTMLPanel mainPanel;
+	@UiField HTMLPanel contentPanel;
+
+	@UiField ListStudiesWidget listStudiesWidgetUiField;
 	
-	@Override
-	protected CuratorHomeWidgetModel createModel() {
-		return new CuratorHomeWidgetModel();
-	}
-
-	public CuratorHomeWidgetModel getModel() {
-		return model;
-	}
 
 	@Override
-	protected CuratorHomeWidgetRenderer createRenderer() {
-		log.enter("createRenderer");
+	protected void renderUI() {
+
+		log.enter("renderUI");
+		
+		this.add(uiBinder.createAndBindUi(this));
+		
+		this.childWidgetEventHandlerRegistrations.add(
+				listStudiesWidgetUiField.viewStudyNavigationEventChannel.addHandler(new WidgetEventHandler() {
+			public void onEvent(WidgetEvent e) {
+				viewStudyNavigationEventChannel.fireEvent(e);
+			}
+		}));
+
 		log.leave();
-		return new CuratorHomeWidgetRenderer(this);
 	}
 
-	// Using init() rather than constructor because reset() uses init().
-	public void init() {
-		super.init();
-		this.controller = new CuratorHomeWidgetController(this, this.model);
-	}
-	
+
 	@Override
 	public void refresh() {
 		log.enter("refresh");
-		log.debug("renderer: " + this.renderer); 
-		//log.debug("listStudiesWidget" + this.renderer.listStudiesWidgetUiField); 
 
-		renderer.listStudiesWidgetUiField.refresh();
+		listStudiesWidgetUiField.refresh();
+		
 		log.leave();
 	}
 	
@@ -54,6 +71,11 @@ public class CuratorHomeWidget
 	public final WidgetEventChannel viewStudyNavigationEventChannel = new WidgetEventChannel(this);
 
 	
+
+	public void registerHandlersForChildWidgetEvents() {
+		
+		
+	}
 
 	
 
