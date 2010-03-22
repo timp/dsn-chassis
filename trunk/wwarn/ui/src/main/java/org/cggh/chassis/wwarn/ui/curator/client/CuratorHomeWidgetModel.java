03573997176040eacdb6b2e9fc6a4895de8fa913
l33h4r7 <l33h4r7@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-22 17:33:12 +0000

    Amended CuratorHomeWidget, taking out superfluous status-change handling.

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidgetModel.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidgetModel.java
@@ -3,7 +3,6 @@
 package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.widget.client.ObservableProperty;
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.ReadyStatus;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
 
 import com.google.gwt.xml.client.Element;
@@ -13,10 +12,6 @@ import com.google.gwt.xml.client.Element;
  */
 public class CuratorHomeWidgetModel {
 
-	public static class ReadyForInteractionStatus extends ReadyStatus {}
-	public static final ReadyForInteractionStatus STATUS_READY_FOR_INTERACTION = new ReadyForInteractionStatus();	
-	
-	
 	public final ObservableProperty<String> studyUrl = new ObservableProperty<String>();
 	public final ObservableProperty<Element> studyEntryElement = new ObservableProperty<Element>();
 	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
