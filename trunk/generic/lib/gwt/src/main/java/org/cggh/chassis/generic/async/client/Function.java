45dabce729f5b702b837fdf681d87fbaa2d2dd4f
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-22 15:59:47 +0000

    Javadoc

--- trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/async/client/Function.java
+++ trunk/generic/lib/gwt/src/main/java/org/cggh/chassis/generic/async/client/Function.java
@@ -1,9 +1,9 @@
-/**
- * 
- */
 package org.cggh.chassis.generic.async.client;
 
 /**
+ * A function has an apply method, which is applied to an input (the domain)
+ * to return an output (the codomain). 
+ * 
  * @author aliman
  *
  */
