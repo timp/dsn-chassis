2b976e9deaef30c6652d13f828bcc07a7f1f5c2f
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-24 00:49:24 +0000

    Regenerated

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/EditQuestionnaireWidget.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/EditQuestionnaireWidget.java
@@ -1,10 +1,23 @@
-/**
- * 
- */
 package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.log.client.Log;
-import org.cggh.chassis.generic.log.client.LogFactory;import org.cggh.chassis.generic.widget.client.ChassisWidget;
+import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
+import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
+import com.google.gwt.xml.client.Element;
+
+import org.cggh.chassis.generic.widget.client.ChassisWidget;
+import org.cggh.chassis.generic.widget.client.ObservableProperty;
+import org.cggh.chassis.generic.widget.client.WidgetEvent;
+import org.cggh.chassis.generic.widget.client.WidgetEventHandler;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
 
 
 import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
@@ -24,29 +37,102 @@ public class EditQuestionnaireWidget
 	private static final Log log = LogFactory.getLog(EditQuestionnaireWidget.class);
 	
 
-
-
-	// Using init() rather than constructor because reset() uses init().
-	public void init() {
-		
-		super.init();
-		
+	@UiTemplate("EditQuestionnaireWidget.ui.xml")
+	interface EditQuestionnaireWidgetRendererUiBinder extends
+			UiBinder<HTMLPanel, EditQuestionnaireWidget> {
 	}
+	private static EditQuestionnaireWidgetRendererUiBinder uiBinder = 
+		GWT.create(EditQuestionnaireWidgetRendererUiBinder.class);
+
+	@UiField HTMLPanel mainPanel;
+	@UiField HTMLPanel contentPanel;
+	@UiField HTMLPanel pendingPanel;
+	@UiField HTMLPanel errorPanel;
+	@UiField FlowPanel errorMessage;
+
+	@UiField FlowPanel editQuestionnairePanel;
+	
+
+	@Override
+	protected void renderUI() {
+
+		log.enter("renderUI");
+		
+		this.clear();
+		this.add(uiBinder.createAndBindUi(this));
+		errorPanel.setVisible(false);	
+		
+
+		log.leave();
+	}
+
+
+	@Override
+	protected void bindUI() {
+		super.bindUI();
+		
+}
+
+
+
+	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
+	public final ObservableProperty<String> message = new ObservableProperty<String>();
+
+	public final WidgetEventChannel editQuestionnaireSaveStudyEventChannel = new WidgetEventChannel(this);
+
+
+
 	
 	@Override
 	public void refresh() {
 		log.enter("refresh");
-		// TODO get data 
+		
+		// TODO refresh this
 		log.leave();	
 	}
 	
 	
-	public final WidgetEventChannel saveStudyEventChannel = new WidgetEventChannel(this);
-
-
 	
+    protected void syncUIWithStatus(Status status) {
+		log.enter("syncUIWithStatus");		
+		log.debug("status:" + status);
+		
+		if (status == null) {
+			// nothing to do yet
+		}
+		else if (status instanceof AsyncWidgetModel.InitialStatus) {
+			pendingPanel.setVisible(true);	
+		}
+		else if (status == ListStudiesWidgetModel.STATUS_RETRIEVE_STUDY_FEED_PENDING) {
+			// still pending
+		}			
+		else if (status == ListStudiesWidgetModel.STATUS_READY_FOR_INTERACTION) {
+			pendingPanel.setVisible(false);	
+		}			
+		else if (status instanceof AsyncWidgetModel.ReadyStatus) {
+			pendingPanel.setVisible(false);	
+		}			
+		else if (status instanceof AsyncWidgetModel.ErrorStatus) {
+			error("Error status given on asynchronous call.");
+		}			
+		else {
+			error("Unhandled status:" + status);
+		}
+		
+		log.leave();
+	}
 	
 
+	public void error(String err) {
+		log.enter("error");
+		log.debug("Error:" + err);
+		pendingPanel.setVisible(false);	
+		contentPanel.setVisible(false);
+		errorMessage.clear();
+		errorMessage.add(new HTML(err));
+		errorPanel.setVisible(true);
+		log.leave();
+	}
 	
 
 }
