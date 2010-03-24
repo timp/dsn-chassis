6e6b220aecb58adde161466bc28a34f0622bba15
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-24 02:00:30 +0000

    Unify with generated

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
@@ -2,18 +2,31 @@ package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
+import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
+import com.google.gwt.xml.client.Element;
+
 import org.cggh.chassis.generic.widget.client.ChassisWidget;
+import org.cggh.chassis.generic.widget.client.ObservableProperty;
 import org.cggh.chassis.generic.widget.client.WidgetEvent;
-import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
 import org.cggh.chassis.generic.widget.client.WidgetEventHandler;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
 import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
+
+import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
+
+
 /**
+ * BE SURE TO EDIT THE TEMPLATE NOT THE RENDERED RESULT
+ *
+ * DELETE_TO_MANUALLY_EDIT
  *
  * @author timp
  *
@@ -23,26 +36,37 @@ public class CuratorHomeWidget
 
 	private static final Log log = LogFactory.getLog(CuratorHomeWidget.class);
 	
+
+	public final WidgetEventChannel listStudiesViewStudyNavigationEventChannel = new WidgetEventChannel(this);
+	
+
 	@UiTemplate("CuratorHomeWidget.ui.xml")
 	interface CuratorHomeWidgetRendererUiBinder extends
 			UiBinder<HTMLPanel, CuratorHomeWidget> {
 	}
-
-	private static CuratorHomeWidgetRendererUiBinder uiBinder =
-			GWT.create(CuratorHomeWidgetRendererUiBinder.class);
+	private static CuratorHomeWidgetRendererUiBinder uiBinder = 
+		GWT.create(CuratorHomeWidgetRendererUiBinder.class);
 
 	@UiField HTMLPanel mainPanel;
 	@UiField HTMLPanel contentPanel;
+	@UiField HTMLPanel pendingPanel;
+	@UiField HTMLPanel errorPanel;
+	@UiField FlowPanel errorMessage;
 
-	@UiField ListStudiesWidget listStudiesWidgetUiField;
+	@UiField FlowPanel curatorHomePanel;
 	
+    @UiField ListStudiesWidget listStudiesWidgetUiField;
+
+
 
 	@Override
 	protected void renderUI() {
 
 		log.enter("renderUI");
 		
+		this.clear();
 		this.add(uiBinder.createAndBindUi(this));
+		errorPanel.setVisible(false);	
 		
 
 		log.leave();
@@ -53,31 +77,87 @@ public class CuratorHomeWidget
 	protected void bindUI() {
 		super.bindUI();
 		this.childWidgetEventHandlerRegistrations.add(
-				listStudiesWidgetUiField.viewStudyNavigationEventChannel.addHandler(new WidgetEventHandler() {
+				listStudiesWidgetUiField.listStudiesViewStudyNavigationEventChannel.addHandler(new WidgetEventHandler() {
 			public void onEvent(WidgetEvent e) {
-				log.enter("onEvent");
-				viewStudyNavigationEventChannel.fireEvent(e);
+				log.enter("onEvent(listStudies)");
+				listStudiesViewStudyNavigationEventChannel.fireEvent(e);
 				log.leave();
 			}
 		}));
-	}
+		
+    }
+
 
 
 	@Override
+	protected void syncUI() {
+		super.syncUI();
+		syncUIWithStatus(status.get());
+	}
+
+
+
+	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
+	public final ObservableProperty<String> message = new ObservableProperty<String>();
+
+	public final WidgetEventChannel curatorHomeViewStudyNavigationEventChannel = new WidgetEventChannel(this);
+
+
+
+	
+	@Override
 	public void refresh() {
 		log.enter("refresh");
+		
+		// TODO refresh this
 
 		listStudiesWidgetUiField.refresh();
+
+		log.leave();	
+	}
+	
+	
+	
+    protected void syncUIWithStatus(Status status) {
+		log.enter("syncUIWithStatus");		
+		log.debug("status:" + status);
+		
+		if (status == null) {
+			// nothing to do yet
+		}
+		else if (status instanceof AsyncWidgetModel.InitialStatus) {
+			pendingPanel.setVisible(true);	
+		}
+		else if (status == ListStudiesWidgetModel.STATUS_RETRIEVE_STUDY_FEED_PENDING) {
+			// still pending
+		}			
+		else if (status == ListStudiesWidgetModel.STATUS_READY_FOR_INTERACTION) {
+			pendingPanel.setVisible(false);	
+		}			
+		else if (status instanceof AsyncWidgetModel.ReadyStatus) {
+			pendingPanel.setVisible(false);	
+		}			
+		else if (status instanceof AsyncWidgetModel.ErrorStatus) {
+			error("Error status given on asynchronous call.");
+		}			
+		else {
+			error("Unhandled status:" + status);
+		}
 		
 		log.leave();
 	}
 	
-	
-	
-	public final WidgetEventChannel viewStudyNavigationEventChannel = new WidgetEventChannel(this);
 
-	
-
+	public void error(String err) {
+		log.enter("error");
+		log.debug("Error:" + err);
+		pendingPanel.setVisible(false);	
+		contentPanel.setVisible(false);
+		errorMessage.clear();
+		errorMessage.add(new HTML(err));
+		errorPanel.setVisible(true);
+		log.leave();
+	}
 	
 
 }
