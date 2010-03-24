f7cea102d74eac65471f2cee24aa27ee817d4348
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-24 21:57:14 +0000

    Regenerated, with unused properties removed

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/AddCurationWidgetModel.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/AddCurationWidgetModel.java
@@ -31,9 +31,7 @@ public class AddCurationWidgetModel {
 	
 	public static class ReadyForInteractionStatus extends ReadyStatus {}
 	public static final ReadyForInteractionStatus STATUS_READY_FOR_INTERACTION = new ReadyForInteractionStatus();
-	
-	
-	
+
 	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
 	public final ObservableProperty<String> message = new ObservableProperty<String>();
 	
