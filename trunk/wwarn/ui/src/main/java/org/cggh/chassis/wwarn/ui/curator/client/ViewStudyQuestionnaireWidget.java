c27c0f424f3d083e8bfc7769dc7ff79b8c1ad481
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-25 10:02:00 +0000

    Unify with ViewStudy

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ViewStudyQuestionnaireWidget.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ViewStudyQuestionnaireWidget.java
@@ -1,11 +1,21 @@
 package org.cggh.chassis.wwarn.ui.curator.client;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.cggh.chassis.generic.async.client.Deferred;
+import org.cggh.chassis.generic.async.client.Function;
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
+import org.cggh.chassis.generic.miniatom.client.AtomHelper;
+import org.cggh.chassis.generic.widget.client.ChassisWidget;
 import org.cggh.chassis.generic.widget.client.DelegatingWidget;
+import org.cggh.chassis.generic.widget.client.MapMemory;
 import org.cggh.chassis.generic.widget.client.ObservableProperty;
+import org.cggh.chassis.generic.widget.client.WidgetMemory;
 
 
+import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
 import com.google.gwt.xml.client.Element;
 
@@ -25,6 +35,12 @@ public class ViewStudyQuestionnaireWidget
 	private static final Log log = LogFactory.getLog(ViewStudyQuestionnaireWidget.class);
 	
 	private ViewStudyQuestionnaireWidgetController controller;
+
+	public final ObservableProperty<Element> studyEntry = new ObservableProperty<Element>();
+
+
+	public final ObservableProperty<String> studyUrl = new ObservableProperty<String>();
+
 	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
 	public final ObservableProperty<String> message = new ObservableProperty<String>();
 	public final WidgetEventChannel studyActionsViewStudyNavigationEventChannel = new WidgetEventChannel(this);
@@ -64,6 +80,7 @@ public class ViewStudyQuestionnaireWidget
 
 		this.controller = new ViewStudyQuestionnaireWidgetController(this, this.model);
 		this.renderer.setController(controller);
+		this.memory = new Memory();
 
 	}
 	
@@ -71,9 +88,101 @@ public class ViewStudyQuestionnaireWidget
 	public void refresh() {
 		log.enter("refresh");
 		
-		// TODO refresh this
+		renderer.studySummaryWidgetUiField.refresh();
+		
 		log.leave();	
 	}
+	
+
+	@Override
+	public Deferred<ChassisWidget> refreshAndCallback() {
+		
+		log.enter("refreshAndCallback");
+		
+		Deferred<ChassisWidget> deferredSelf = this.controller.refreshAndCallback();
+		
+		log.leave();
+		return deferredSelf;
+	}
+	
+	
+	private class Memory extends MapMemory {
+
+		private static final String KEY_STUDYID = "studyid";
+
+
+		@Override
+		public Map<String, String> createMnemonicMap() {
+			log.enter("createMnemonicMap");
+			
+			Map<String, String> map = new HashMap<String, String>();
+			
+			Element study = model.studyEntry.get();
+			
+			String studyID = null;
+			
+			if (study != null) { 
+				
+				studyID = AtomHelper.getId(study);
+			
+				if (studyID != null) {
+					
+					map.put(KEY_STUDYID, studyID);
+				}
+				
+			}
+			
+			log.leave();
+			return map;
+		}
+
+		@Override
+		public Deferred<WidgetMemory> remember(Map<String, String> mnemonic) {
+			log.enter("remember");
+			
+			final Deferred<WidgetMemory> deferredMemory;
+			
+			model.status.set(AsyncWidgetModel.STATUS_INITIAL);
+			
+			String studyID = mnemonic.get(KEY_STUDYID);
+			
+			log.debug("found studyId: " + studyID);
+			
+			if (studyID != null) {
+				
+				log.debug("set selected study id to:" + studyID);
+				
+				model.studyID.set(studyID);
+				
+				deferredMemory = refreshAndCallback().adapt(new Function<ChassisWidget, WidgetMemory>() {
+
+					public WidgetMemory apply(ChassisWidget in) {
+						return Memory.this;
+					}
+					
+				});
+
+			}
+			
+			else {
+				
+				log.debug("call back immediately");
+				deferredMemory = new Deferred<WidgetMemory>();
+				deferredMemory.callback(this);
+				
+			}
+			
+			log.leave();
+			return deferredMemory;
+		}
+
+
+	}
+
+
+
+
+	
 	
 	
 	
