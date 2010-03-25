7e149954210d80692b3911a26e83c89eed000695
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-25 00:32:06 +0000

    Add events for each link in viewStudy.StudyActions

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/EditStudyQuestionnaireNavigationEvent.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/EditStudyQuestionnaireNavigationEvent.java
@@ -4,14 +4,32 @@ import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
 import org.cggh.chassis.generic.widget.client.WidgetEvent;
 
+import com.google.gwt.xml.client.Element;
+
 /**
- * BE SURE TO EDIT THE TEMPLATE NOT THE RENDERED RESULT
- *
- * DELETE_TO_MANUALLY_EDIT
- *
  */
 public class EditStudyQuestionnaireNavigationEvent extends WidgetEvent {
 
-	private static final Log log = LogFactory.getLog(EditStudyQuestionnaireNavigationEvent.class);	
+	private static final Log log = LogFactory.getLog(EditStudyQuestionnaireNavigationEvent.class);
+
+	private Element study;
+	
+	public void setStudy(Element study) {
+		log.enter("setStudy");		
+		
+		this.study = study;
+		
+		log.leave();
+	}
+	
+	public Element getStudy() {
+		log.enter("getStudy");		
+		
+		log.debug("Getting study " + this.study);
+
+		log.leave();
+		
+		return this.study;
+	}
 
 }
