db8b7b8aa480c956c99150e3a1de36b1a9de9dee
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-30 11:48:57 +0000

    Regenerated

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
@@ -2,13 +2,13 @@ package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
 import org.cggh.chassis.generic.widget.client.ChassisWidget;
 import org.cggh.chassis.generic.widget.client.ObservableProperty;
+import org.cggh.chassis.generic.widget.client.PropertyChangeEvent;
+import org.cggh.chassis.generic.widget.client.PropertyChangeHandler;
 import org.cggh.chassis.generic.widget.client.WidgetEvent;
 import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
 import org.cggh.chassis.generic.widget.client.WidgetEventHandler;
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -17,6 +17,7 @@ import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
+
 
 /**
  * BE SURE TO EDIT THE TEMPLATE NOT THE RENDERED RESULT
@@ -30,7 +31,7 @@ public class CuratorHomeWidget
 	 	extends ChassisWidget {
 
 	private static final Log log = LogFactory.getLog(CuratorHomeWidget.class);
-	
+
 
 	@UiTemplate("CuratorHomeWidget.ui.xml")
 	interface CuratorHomeWidgetRendererUiBinder extends
@@ -41,7 +42,7 @@ public class CuratorHomeWidget
 
 	@UiField HTMLPanel mainPanel;
 	@UiField HTMLPanel contentPanel;
-	@UiField HTMLPanel pendingPanel;
+
 	@UiField HTMLPanel errorPanel;
 	@UiField FlowPanel errorMessage;
 
@@ -51,9 +52,25 @@ public class CuratorHomeWidget
 
 
 
+	public final ObservableProperty<String> message = new ObservableProperty<String>();
+
+	public final WidgetEventChannel listStudiesViewStudyNavigationEventChannel = new WidgetEventChannel(this);
+
+
+
+	
+	@Override
+	public void refresh() {
+		log.enter("refresh");
+		
+		listStudiesWidgetUiField.refresh();
+
+		log.leave();	
+	}
+	
+
 	@Override
 	protected void renderUI() {
-
 		log.enter("renderUI");
 		
 		this.clear();
@@ -76,61 +93,33 @@ public class CuratorHomeWidget
 				log.leave();
 			}
 		}));
- 		
+ 
+
+	// Model changes
+
+	
+		message.addChangeHandler(new PropertyChangeHandler<String>() {
+			public void onChange(PropertyChangeEvent<String> e) {
+				log.enter("onChange<String>");		
+				syncUIWithMessage(e.getAfter());
+				log.leave();
+			}
+		});
+
+
+	
 	}
 
 
-	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
-	public final ObservableProperty<String> message = new ObservableProperty<String>();
-	public final WidgetEventChannel listStudiesViewStudyNavigationEventChannel = new WidgetEventChannel(this);
-
 
 	
-	@Override
-	public void refresh() {
-		log.enter("refresh");
-		
-		listStudiesWidgetUiField.refresh();
+	
 
-		log.leave();	
-	}
-	
-	
-	
-    protected void syncUIWithStatus(Status status) {
-		log.enter("syncUIWithStatus");		
-		log.debug("status:" + status);
-		
-		if (status == null) {
-			// nothing to do yet
-		}
-		else if (status instanceof AsyncWidgetModel.InitialStatus) {
-			pendingPanel.setVisible(true);	
-		}
-		else if (status == ListStudiesWidgetModel.STATUS_RETRIEVE_STUDY_FEED_PENDING) {
-			// still pending
-		}			
-		else if (status == ListStudiesWidgetModel.STATUS_READY_FOR_INTERACTION) {
-			pendingPanel.setVisible(false);	
-		}			
-		else if (status instanceof AsyncWidgetModel.ReadyStatus) {
-			pendingPanel.setVisible(false);	
-		}			
-		else if (status instanceof AsyncWidgetModel.ErrorStatus) {
-			message.set("Error status given on asynchronous call.");
-		}			
-		else {
-			message.set("Unhandled status:" + status);
-		}
-		
-		log.leave();
-	}
 	
 	protected void syncUIWithMessage(String message) {
 		log.enter("syncUIWithMessage");
 
 		if (message != null) {
-			pendingPanel.setVisible(false);	
 			contentPanel.setVisible(false);
 			errorMessage.clear();
 			errorMessage.add(new HTML(message));
@@ -140,5 +129,4 @@ public class CuratorHomeWidget
 		log.leave();
 	}
 	
-
 }
