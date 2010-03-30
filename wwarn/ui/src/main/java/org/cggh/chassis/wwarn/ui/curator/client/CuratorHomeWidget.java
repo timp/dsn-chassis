ff2ae165684259386044b2bc78040777da1bb16a
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-03-30 11:30:50 +0000

    Remove warnings and todos

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/CuratorHomeWidget.java
@@ -2,13 +2,13 @@ package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
-import org.cggh.chassis.generic.widget.client.ObservableProperty;
-
 import org.cggh.chassis.generic.widget.client.AsyncWidgetModel;
 import org.cggh.chassis.generic.widget.client.ChassisWidget;
+import org.cggh.chassis.generic.widget.client.ObservableProperty;
 import org.cggh.chassis.generic.widget.client.WidgetEvent;
+import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
 import org.cggh.chassis.generic.widget.client.WidgetEventHandler;
+import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.uibinder.client.UiBinder;
@@ -17,10 +17,6 @@ import com.google.gwt.uibinder.client.UiTemplate;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
-
-import com.google.gwt.xml.client.Element;
-
-import org.cggh.chassis.generic.widget.client.WidgetEventChannel;
 
 /**
  * BE SURE TO EDIT THE TEMPLATE NOT THE RENDERED RESULT
@@ -73,7 +69,7 @@ public class CuratorHomeWidget
 	protected void bindUI() {
 		super.bindUI();
 		this.childWidgetEventHandlerRegistrations.add(
-				listStudiesWidgetUiField.listStudiesViewStudyNavigationEventChannel.addHandler(new WidgetEventHandler() {
+				listStudiesWidgetUiField.listStudiesViewStudyNavigationEventChannel.addHandler(new WidgetEventHandler<WidgetEvent>() {
 			public void onEvent(WidgetEvent e) {
 				log.enter("onEvent(ListStudiesViewStudyNavigation)");
 				listStudiesViewStudyNavigationEventChannel.fireEvent(e);
@@ -94,8 +90,6 @@ public class CuratorHomeWidget
 	public void refresh() {
 		log.enter("refresh");
 		
-		// TODO refresh this
-
 		listStudiesWidgetUiField.refresh();
 
 		log.leave();	
