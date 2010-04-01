f5200a3130118e58f961b5695290abb71a07a7fd
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-04-01 17:50:47 +0000

    Should be a ChassisWidget, at least for now

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ViewQuestionnaireWidget.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ViewQuestionnaireWidget.java
@@ -2,11 +2,18 @@ package org.cggh.chassis.wwarn.ui.curator.client;
 
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
-import org.cggh.chassis.generic.widget.client.DelegatingWidget;
+import org.cggh.chassis.generic.widget.client.ChassisWidget;
 import org.cggh.chassis.generic.widget.client.ObservableProperty;
+import org.cggh.chassis.generic.widget.client.PropertyChangeEvent;
+import org.cggh.chassis.generic.widget.client.PropertyChangeHandler;
 
-
-import org.cggh.chassis.generic.widget.client.AsyncWidgetModel.Status;
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.uibinder.client.UiTemplate;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.xml.client.Element;
 
 /**
@@ -18,46 +25,108 @@ import com.google.gwt.xml.client.Element;
  *
  */
 public class ViewQuestionnaireWidget 
-	 	extends DelegatingWidget<ViewQuestionnaireWidgetModel, ViewQuestionnaireWidgetRenderer> {
+	 	extends ChassisWidget {
 
 	private static final Log log = LogFactory.getLog(ViewQuestionnaireWidget.class);
+
+
+	@UiTemplate("ViewQuestionnaireWidget.ui.xml")
+	interface ViewQuestionnaireWidgetRendererUiBinder extends
+			UiBinder<HTMLPanel, ViewQuestionnaireWidget> {
+	}
+	private static ViewQuestionnaireWidgetRendererUiBinder uiBinder = 
+		GWT.create(ViewQuestionnaireWidgetRendererUiBinder.class);
+
+	@UiField HTMLPanel mainPanel;
+	@UiField HTMLPanel contentPanel;
+
+	@UiField HTMLPanel errorPanel;
+	@UiField FlowPanel errorMessage;
+
+	@UiField FlowPanel viewQuestionnairePanel;
 	
-	private ViewQuestionnaireWidgetController controller;
-	public final ObservableProperty<Status> status = new ObservableProperty<Status>();
+	public final ObservableProperty<Element> studyEntry = new ObservableProperty<Element>();
+
+
 	public final ObservableProperty<String> message = new ObservableProperty<String>();
 
 
 
-	@Override
-	protected ViewQuestionnaireWidgetModel createModel() {
-		return new ViewQuestionnaireWidgetModel();
-	}
 
-	public ViewQuestionnaireWidgetModel getModel() {
-		return model;
-	}
-
-	@Override
-	protected ViewQuestionnaireWidgetRenderer createRenderer() {
-		return new ViewQuestionnaireWidgetRenderer(this);
-	}
-	// Using init() rather than constructor because reset() uses init().
-	public void init() {
-		super.init();
-	
-
-		this.controller = new ViewQuestionnaireWidgetController(this, this.model);
-
-	}
 	
 	@Override
 	public void refresh() {
 		log.enter("refresh");
-		
-		// TODO refresh this
-		log.leave();	
+				log.leave();	
 	}
 	
+
+	@Override
+	protected void renderUI() {
+		log.enter("renderUI");
+		
+		this.clear();
+		this.add(uiBinder.createAndBindUi(this));
+		errorPanel.setVisible(false);	
+		
+
+		log.leave();
+	}
+
+
+	@Override
+	protected void bindUI() {
+		super.bindUI();
+
+
+		// Model changes
+
+
+		message.addChangeHandler(new PropertyChangeHandler<String>() {
+			public void onChange(PropertyChangeEvent<String> e) {
+				log.enter("onChange<String>");		
+				syncUIWithMessage(e.getAfter());
+				log.leave();
+			}
+		});
+
+	
+		studyEntry.addChangeHandler(new PropertyChangeHandler<Element>() {
+			public void onChange(PropertyChangeEvent<Element> e) {
+				log.enter("onchange(studyEntry)");
+				syncUIWithStudyEntry(e.getAfter());
+				log.leave();
+			}
+		});
+
+
+	
+	}
+
+
+
 	
 	
+
+	
+	protected void syncUIWithMessage(String message) {
+		log.enter("syncUIWithMessage");
+
+		if (message != null) {
+			contentPanel.setVisible(false);
+			errorMessage.clear();
+			errorMessage.add(new HTML(message));
+			errorPanel.setVisible(true);
+		}
+
+		log.leave();
+	}
+		
+	protected void syncUIWithStudyEntry(Element studyEntry) {
+		log.enter("syncUIWithStudyEntry");
+		// TODO needs to be a method in an extension
+		log.leave();
+	}
+
+
 }
