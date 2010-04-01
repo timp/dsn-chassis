f33335c37307a0f8bbe3f949d06a9049988b8564
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-04-01 09:05:41 +0000

    Renerated, without unused imports

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/UploadDataFilesWizardNavigationEvent.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/UploadDataFilesWizardNavigationEvent.java
@@ -1,42 +1,29 @@
 package org.cggh.chassis.wwarn.ui.curator.client;
-
 import org.cggh.chassis.generic.log.client.Log;
 import org.cggh.chassis.generic.log.client.LogFactory;
+
 import org.cggh.chassis.generic.widget.client.WidgetEvent;
 
 import com.google.gwt.xml.client.Element;
 
+
+/**
+ * Generated event UploadDataFilesWizardNavigation from StudyActions to 
+ * UploadDataFilesWizard.
+ *
+ * BE SURE TO EDIT THE TEMPLATE NOT THE RENDERED RESULT
+ *
+ * DELETE_TO_MANUALLY_EDIT
+ *
+ */
 public class UploadDataFilesWizardNavigationEvent extends WidgetEvent {
 
 	private static final Log log = LogFactory.getLog(UploadDataFilesWizardNavigationEvent.class);	
-	
-	// TODO: depreciate the study element in favour of the studyUrl
-	private Element study;
+
 	private String studyUrl;
-
-	public void setStudy(Element study) {
-		log.enter("setStudy");		
-		
-		this.study = study;
-		
-		log.leave();
-	}
-	
-	public Element getStudy() {
-		log.enter("getStudy");		
-		
-		log.debug("Returning study " + this.study);
-
-		log.leave();
-		
-		return this.study;
-	}
-
-
 
 
 	public void setStudyUrl(String studyUrl) {
-
 		log.enter("setStudyUrl");		
 		
 		this.studyUrl = studyUrl;
@@ -45,15 +32,35 @@ public class UploadDataFilesWizardNavigationEvent extends WidgetEvent {
 	}
 	
 	public String getStudyUrl() {
-
 		log.enter("getStudyUrl");		
 		
-		log.debug("Getting study url " + this.studyUrl);
+		log.debug("Returning studyUrl " + this.studyUrl);
 
 		log.leave();
 		
 		return this.studyUrl;
+	}
 
-	}	
+	private Element studyEntry;
+
+
+	public void setStudyEntry(Element studyEntry) {
+		log.enter("setStudyEntry");		
+		
+		this.studyEntry = studyEntry;
+		
+		log.leave();
+	}
 	
+	public Element getStudyEntry() {
+		log.enter("getStudyEntry");		
+		
+		log.debug("Returning studyEntry " + this.studyEntry);
+
+		log.leave();
+		
+		return this.studyEntry;
+	}
+
+
 }
