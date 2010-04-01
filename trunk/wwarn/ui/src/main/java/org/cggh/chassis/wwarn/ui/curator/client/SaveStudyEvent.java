91cfd9cc016a6355bf954eb12cdeb78c2dfd27a4
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-04-01 09:59:56 +0000

    Nothing doing

--- trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/SaveStudyEvent.java
+++ trunk/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/SaveStudyEvent.java
@@ -1,7 +1,5 @@
 package org.cggh.chassis.wwarn.ui.curator.client;
 
-import org.cggh.chassis.generic.log.client.Log;
-import org.cggh.chassis.generic.log.client.LogFactory;
 import org.cggh.chassis.generic.widget.client.WidgetEvent;
 
 /**
@@ -11,7 +9,5 @@ import org.cggh.chassis.generic.widget.client.WidgetEvent;
  *
  */
 public class SaveStudyEvent extends WidgetEvent {
-
-	private static final Log log = LogFactory.getLog(SaveStudyEvent.class);	
 
 }
