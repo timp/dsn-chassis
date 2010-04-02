b27f659f6270ae6705b575438c4b47d3c105c9fa
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-04-02 23:26:56 +0000

    'Fix' Not Found status error message, not happy

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ViewStudyRevisionWidgetRendererBase.java
+++ wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/curator/client/ViewStudyRevisionWidgetRendererBase.java
@@ -268,7 +268,7 @@ public abstract class ViewStudyRevisionWidgetRendererBase extends
 		else if (status instanceof AsyncWidgetModel.AsyncRequestPendingStatus) {
 		}
 		else if (status instanceof AsyncWidgetModel.NotFoundStatus) {
-			model.message.set("Not found. url " + model.studyUrl.get());
+			model.message.set("Not found."); // TODO Fix message to include model.studyUrl.get()
 		}			
 		else if (status instanceof AsyncWidgetModel.ReadyStatus) {
 			pendingPanel.setVisible(false);
