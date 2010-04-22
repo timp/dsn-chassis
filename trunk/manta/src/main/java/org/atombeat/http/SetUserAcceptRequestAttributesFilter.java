d1fddfb506b17d6e8b4042c2347d44405e5308ad
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-04-22 16:21:11 +0000

    Carry on at home

---
+++ trunk/manta/src/main/java/org/atombeat/http/SetUserAcceptRequestAttributesFilter.java
@@ -0,0 +1,62 @@
+package org.atombeat.http;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.atombeat.http.HttpFilter;
+
+/**
+ * @author timp
+ *
+ */
+public class SetUserAcceptRequestAttributesFilter extends HttpFilter {
+
+	
+	
+	
+	private Log log = LogFactory.getLog(this.getClass());
+	
+	
+	
+	public static final String USERLANGUAGE_REQUEST_ATTRIBUTE_KEY     = "user-language";
+	public static final String USER_COUNTRY_REQUEST_ATTRIBUTE_KEY     = "user-country";
+	
+	
+	
+	/*
+	 * See:
+	 *  Accept: http://www.w3.org/Protocols/rfc2616/rfc2616-sec14.html
+	 *  Language: http://www.rfc-editor.org/rfc/rfc3282.txt
+	 * eg:
+	 * Accept-Language: da, en-gb;q=0.8, en;q=0.7
+	 *  would mean: 
+	"I prefer Danish, but will accept British English and
+   other types of English." A language-range matches a language-tag if
+   it exactly equals the tag, or if it exactly equals a prefix of the
+   tag such that the first tag character following the prefix is "-".
+   The special range "*", if present in the Accept-Language field,
+   matches every tag not matched by any other range present in the
+   Accept-Language field.
+	 */
+	@Override
+	public void doHttpFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain) 
+			throws IOException, ServletException {
+		log.debug("request inbound");
+
+		System.err.println(request.getLocale() + "," + request.getLocale().getCountry());
+		
+		chain.doFilter(request, response);
+
+		log.debug("response outbound");
+	}
+
+
+
+
+}
