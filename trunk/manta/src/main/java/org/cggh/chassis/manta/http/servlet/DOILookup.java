d4a5483f429f305ced5ded3e9a84bc0dde1e4db8
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-06-18 08:47:33 +0000

    Review

--- trunk/manta/src/main/java/org/cggh/chassis/manta/http/servlet/DOILookup.java
+++ trunk/manta/src/main/java/org/cggh/chassis/manta/http/servlet/DOILookup.java
@@ -2,7 +2,6 @@ package org.cggh.chassis.manta.http.servlet;
 
 import java.io.IOException;
 
-import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -13,6 +12,8 @@ import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.methods.GetMethod;
 
 /**
+ * A class to wrap dx.doi.org and return testable statuses.
+ * 
  * @author timp
  * @since 17 Jun 2010
  */
@@ -22,63 +23,58 @@ public class DOILookup extends HttpServlet {
 
 	public static final HttpClient client = new HttpClient();
 	
-	public DOILookup() {
-		System.err.println("DOILookup constructor");
-	}
-
-	@Override
-	public void init(ServletConfig config) throws ServletException {
-
-		System.err.println("DOILookup init");
-
-	}
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws ServletException, IOException {
 		String doi = getCleanDOI(req);
 		if (doi != null) {
-			HttpMethod get = new GetMethod("http://dx.doi.org/" + doi);
+			String url = "http://dx.doi.org/" + doi;
+			HttpMethod get = new GetMethod();
 			client.executeMethod(get);
 			if (new String(get.getResponseBody()).contains("Not Found")) { 
 				resp.setStatus(404);
-				resp.setContentType("text/html");
+				
 				printHeader(resp, "Resource not found");
-				resp.getWriter().println("</html>");
+				
+				resp.getWriter().println("<p>" + doi + 
+						" could not be dereferenced at <a href=\"" + url + "\">" + url + "</a>.</p>");				
+
+				printFooter(resp);
 			} else { 
 				resp.setStatus(200);
-				resp.setContentType("text/html");
+				
 				printHeader(resp, "Resource found");
-				resp.getWriter().println("<p>Found " + doi + "</p>");				
-				resp.getWriter().println("</html>");				
+				
+				resp.getWriter().println("<p>Found " + doi + " at <a href=\"" + url + "\">" + url + "</a>.</p>");				
+
+				printFooter(resp);
 			}
 		} else {
 			resp.setStatus(200);
-			resp.setContentType("text/html");
 			
 			printHeader(resp, "Resolve a DOI");
 
-			resp.getWriter().println("<p>");
+			resp.getWriter().println("  <p>");
 			resp.getWriter().println(
 					"Enter a <a href=\"http://dx.doi.org/10.1000/1\">DOI</a>");
 			resp.getWriter().println("</p>");
 
-			resp.getWriter().println("<form method=\"GET\" action=\"\">");
+			resp.getWriter().println("  <form method=\"GET\" action=\"\">");
 
 			resp.getWriter().println(
 					"doi: <input type=\"text\" name=\"doi\" size=\"70\" />");
-			resp.getWriter().println("<br/>");
-			resp.getWriter().println("<input type=\"SUBMIT\">");
+			resp.getWriter().println("   <br/>");
+			resp.getWriter().println("   <input type=\"SUBMIT\">");
 
-			resp.getWriter().println("</form>");
-
-			resp.getWriter().println("</body>");
-			resp.getWriter().println("</html>");
-
+			resp.getWriter().println("  </form>");
+			
+			printFooter(resp);
 		}
 	}
 
 	private void printHeader(HttpServletResponse resp, String title) throws IOException {
+		resp.setContentType("text/html");
 		resp.getWriter()
 				.println(
 						"<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">");
@@ -92,24 +88,29 @@ public class DOILookup extends HttpServlet {
 		resp.getWriter().println("<h1>");
 		resp.getWriter().println(title);
 		resp.getWriter().println("</h1>");
-
 	}
 
-	private String getCleanDOI(HttpServletRequest req) {
+	private void printFooter(HttpServletResponse resp) throws IOException { 
+		resp.getWriter().println(" </body>");
+		resp.getWriter().println("</html>");
+		
+	}
+	
+	static String getCleanDOI(HttpServletRequest req) {
 		String doi = getDOI(req);
 		if (doi != null)
 			doi = doi.replaceFirst("^(doi|DOI):(//)?", "");
 		return doi;
 	}
 
-	private String getDOI(HttpServletRequest req) {
+	static String getDOI(HttpServletRequest req) {
 		if (req.getPathInfo() != null)
 			if (req.getPathInfo() != "")
 				if (req.getPathInfo() != "/")
 					return req.getPathInfo();
 		if (req.getQueryString() != null)
 			if (req.getQueryString() != "")
-				if (!req.getQueryString().contains("?"))
+				if (!req.getQueryString().contains("="))
 					return req.getQueryString();
 		if (req.getParameter("DOI") != null)
 			if (req.getParameter("DOI") != "")
