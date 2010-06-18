01292e5db9442a25d0d2b5f7908f183d94c08b6c
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-06-18 23:24:13 +0000

    Fussy xhtml

--- trunk/manta/src/main/java/org/cggh/chassis/manta/http/servlet/DOILookup.java
+++ trunk/manta/src/main/java/org/cggh/chassis/manta/http/servlet/DOILookup.java
@@ -61,10 +61,12 @@ public class DOILookup extends HttpServlet {
 			resp.getWriter().println("  </p>");
 
 			resp.getWriter().println("  <form method=\"get\" action=\"\">");
-			resp.getWriter().println("   <p>");
+      resp.getWriter().println("   <p>");
 			resp.getWriter().println("doi: <input type=\"text\" name=\"doi\" />");
-			resp.getWriter().println("   </p>");
+      resp.getWriter().println("   </p>");
+      resp.getWriter().println("   <p>");
 			resp.getWriter().println("   <input type=\"submit\" />");
+      resp.getWriter().println("   </p>");
 			resp.getWriter().println("  </form>");
 			
 			printFooter(resp);
