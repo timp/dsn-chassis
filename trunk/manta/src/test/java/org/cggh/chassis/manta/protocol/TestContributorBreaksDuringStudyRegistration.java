05451574b713acaa20bf55e9fbf0c1608b6a2633
alimanfoo <alimanfoo@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-07-02 16:48:21 +0000

    added more manta service tests

---
+++ trunk/manta/src/test/java/org/cggh/chassis/manta/protocol/TestContributorBreaksDuringStudyRegistration.java
@@ -0,0 +1,79 @@
+package org.cggh.chassis.manta.protocol;
+
+import java.util.List;
+
+import org.apache.abdera.i18n.iri.IRI;
+import org.apache.abdera.model.Entry;
+import org.apache.abdera.model.Feed;
+import org.apache.abdera.model.Link;
+import org.apache.abdera.protocol.client.ClientResponse;
+
+import junit.framework.TestCase;
+import static org.cggh.chassis.manta.protocol.ConstantsForTests.*;
+import static org.cggh.chassis.manta.protocol.UtilsForTests.*;
+
+public class TestContributorBreaksDuringStudyRegistration extends TestCase {
+
+	
+	
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	
+	
+	
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+	
+	
+	public void testContributorBreaksDuringStudyRegistration() {
+
+		Entry draftEntry;
+		Feed draftsFeed;
+		List<Entry> drafts;
+		ClientResponse response;
+		IRI location;
+		
+		String title = Double.toString(Math.random());
+		
+		// create a draft
+		draftEntry = newDraft(title);
+		response = abderaClient.post(URL_DRAFTS_COLLECTION, draftEntry, authn(USER_CORA));
+		assertEquals(201, response.getStatus());
+		location = response.getLocation(); assertNotNull(location);
+		draftEntry = getEntry(response); assertNotNull(draftEntry);
+		assertNotNull(draftEntry.getEditLink());
+		response.release();
+
+		// break, come back and list the drafts
+		response = abderaClient.get(URL_DRAFTS_COLLECTION, authn(USER_CORA));
+		assertEquals(200, response.getStatus());
+		draftsFeed = getFeed(response);
+		drafts = draftsFeed.getEntries();
+		assertTrue(drafts.size()>=1);
+		draftEntry = null;
+		for (Entry d : drafts) {
+			if (title.equals(d.getTitle())) {
+				draftEntry = d;
+			}
+		}
+		assertNotNull(draftEntry);
+		response.release();
+		
+		// update the draft again
+		draftEntry.setTitle("proper title");
+		response = abderaClient.put(draftEntry.getEditLink().getHref().toString(), draftEntry, authn(USER_CORA));
+		assertEquals(200, response.getStatus());
+		draftEntry = getEntry(response);
+		assertEquals("proper title", draftEntry.getTitle());
+		response.release();
+
+	}
+	
+	
+	
+}
--- trunk/manta/src/test/java/org/cggh/chassis/manta/protocol/TestContributorReturnsToSubmitMoreFiles.java
+++ trunk/manta/src/test/java/org/cggh/chassis/manta/protocol/TestContributorReturnsToSubmitMoreFiles.java
@@ -1,13 +1,24 @@
 package org.cggh.chassis.manta.protocol;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
+import org.apache.abdera.model.Category;
 import org.apache.abdera.model.Content;
+import org.apache.abdera.model.Document;
 import org.apache.abdera.model.Element;
 import org.apache.abdera.model.Entry;
 import org.apache.abdera.model.Feed;
 import org.apache.abdera.model.Link;
+import org.apache.abdera.parser.ParseException;
 import org.apache.abdera.protocol.client.ClientResponse;
+import org.apache.abdera.protocol.client.RequestOptions;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.multipart.FilePart;
+import org.apache.commons.httpclient.methods.multipart.Part;
+import org.apache.commons.httpclient.methods.multipart.StringPart;
 
 import junit.framework.TestCase;
 import static org.cggh.chassis.manta.protocol.ConstantsForTests.*;
@@ -102,6 +113,78 @@ public class TestContributorReturnsToSubmitMoreFiles extends TestCase {
 	}
 	
 	
+	
+	
+	public void testSubmitMoreFiles() throws Exception {
+		
+		ClientResponse response;
+		Feed submittedMediaFeed;
+		Entry studyEntry, mediaLinkEntry;
+		Link editLink, editMediaLink, submittedMediaLink;
+		String submittedMediaCollectionUrl;
+		int submittedMedia;
+		Document<Entry> responseDoc;
+		
+		// create a study, to ensure there is at least one
+		studyEntry = createStudy("My First Study", USER_CORA);
+		submittedMediaLink = studyEntry.getLink(REL_SUBMITTED_MEDIA); assertNotNull(submittedMediaLink);
+		submittedMediaCollectionUrl = submittedMediaLink.getHref().toString();
+
+		// retrieve submitted media feed
+		response = abderaClient.get(submittedMediaCollectionUrl, authn(USER_CORA)); assertEquals(200, response.getStatus());
+		submittedMediaFeed = getFeed(response); assertNotNull(submittedMediaFeed);
+		submittedMedia = submittedMediaFeed.getEntries().size();
+		response.release();
+		
+		// upload a file via Atom POST
+		InputStream content = this.getClass().getClassLoader().getResourceAsStream("spreadsheet1.xls");
+		RequestOptions options = authn(USER_CORA);
+		options.setContentType("application/vnd.ms-excel");
+		response = abderaClient.post(submittedMediaCollectionUrl, content, options);
+		assertEquals(201, response.getStatus());
+		mediaLinkEntry = getEntry(response); assertNotNull(mediaLinkEntry);
+		editLink = mediaLinkEntry.getEditLink(); assertNotNull(editLink);
+		editMediaLink = mediaLinkEntry.getEditMediaLink(); assertNotNull(editMediaLink);
+		response.release();
+		
+		// check user can update file metadata
+		assertFalse(mediaLinkEntry.getTitle().equals("testing.xls"));
+		mediaLinkEntry.setTitle("testing.xls");
+		response = abderaClient.put(editLink.getHref().toString(), mediaLinkEntry, authn(USER_CORA));
+		assertEquals(200, response.getStatus());
+		mediaLinkEntry = getEntry(response); assertNotNull(mediaLinkEntry);
+		assertEquals("testing.xls", mediaLinkEntry.getTitle());
+		response.release();
+		
+		// upload a file via multipart/form-data
+		PostMethod post = new PostMethod(submittedMediaCollectionUrl);
+		File file = new File(this.getClass().getClassLoader().getResource("spreadsheet1.xls").getFile());
+		FilePart fp = createFilePart(file, "spreadsheet1.xls", "application/vnd.ms-excel", "media");
+		StringPart sp1 = new StringPart("summary", "this is a great spreadsheet");
+		StringPart sp2 = new StringPart("category", "scheme=\"foo\"; term=\"bar\"; label=\"baz\"");
+		Part[] parts = { fp , sp1 , sp2 };
+		setMultipartRequestEntity(post, parts);
+		int result = executeMethod(post, USER_CORA);
+		assertEquals(201, result);
+		responseDoc = abdera.getParser().parse(post.getResponseBodyAsStream()); assertNotNull(responseDoc);
+		mediaLinkEntry = responseDoc.getRoot(); assertNotNull(mediaLinkEntry);
+		editLink = mediaLinkEntry.getEditLink(); assertNotNull(editLink);
+		editMediaLink = mediaLinkEntry.getEditMediaLink(); assertNotNull(editMediaLink);
+		assertEquals("spreadsheet1.xls", mediaLinkEntry.getTitle());
+		assertEquals("this is a great spreadsheet", mediaLinkEntry.getSummary());
+		assertEquals(1, mediaLinkEntry.getCategories().size());
+		Category c = mediaLinkEntry.getCategories().get(0); assertNotNull(c);
+		assertEquals("foo", c.getScheme().toString());
+		assertEquals("bar", c.getTerm());
+		assertEquals("baz", c.getLabel());
+		
+		// retrieve submitted media again, check feed size
+		response = abderaClient.get(submittedMediaCollectionUrl, authn(USER_CORA)); assertEquals(200, response.getStatus());
+		submittedMediaFeed = getFeed(response); assertNotNull(submittedMediaFeed);
+		assertEquals(submittedMedia+2, submittedMediaFeed.getEntries().size());
+		response.release();
+		
+	}
 	
 	
 	
