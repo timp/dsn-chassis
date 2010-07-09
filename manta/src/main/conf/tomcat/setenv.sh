52c6ba4d97caff0a71c0bbec3a183abf4e0c3681
Tim.Pizey <Tim.Pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-07-09 13:17:18 +0000

    Should do for cloud1

--- manta/src/main/conf/tomcat/setenv.sh
+++ manta/src/main/conf/tomcat/setenv.sh
@@ -1,4 +1,4 @@
-JAVA_OPTS="-Xms128m -Xmx512m -XX:MaxPermSize=256m -Dfile.encoding=UTF8 -Djava.awt.headless=true" 
+JAVA_OPTS="-Xms128m -Xmx1024m -XX:MaxPermSize=512m -Dfile.encoding=UTF8 -Djava.awt.headless=true" 
 
 export JAVA_OPTS
 
