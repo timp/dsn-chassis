f765897b8120283acd0c5e63aba4825058988fc7
l33h4r7 <l33h4r7@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-09-14 17:11:40 +0000

    This was a copy of the whole trunk instead of just manta at r2965.

--- branches/manta-1.1/wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/anonymizer/client/BackToStartNavigationHandler.java
+++
@@ -1,9 +0,0 @@
-package org.cggh.chassis.wwarn.ui.anonymizer.client;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface BackToStartNavigationHandler extends EventHandler {
-
-	void onNavigation(BackToStartNavigationEvent e);
-
-}
