2c7448084dab5112c548e8ac2b76493c24cc7605
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2010-09-20 12:15:55 +0000

    Allow exception to be propagated upwards

--- src/test/java/org/cggh/chassis/manta/data/TestStudyInfoFlatten.java
+++ src/test/java/org/cggh/chassis/manta/data/TestStudyInfoFlatten.java
@@ -45,22 +45,22 @@ public class TestStudyInfoFlatten extends TestCase {
 		File xmlFile = new File(xml);
 		File xsltFile = new File(xslt);
 		File preprocess = new File("src/test/resources/setupFlattenTest.xsl");
-		
+
 		// JAXP reads data using the Source interface
 		Source xmlSource = new StreamSource(xmlFile);
 		Source xsltSource = new StreamSource(xsltFile);
 		Source ppSource = new StreamSource(preprocess);
-		
+
 		// the factory pattern supports different XSLT processors
 		TransformerFactory transFact = TransformerFactory.newInstance();
-		//Preprocess the example file
+		// Preprocess the example file
 		Transformer trans = transFact.newTransformer(ppSource);
 		StringWriter intermediate = new StringWriter();
 		Result result1 = new StreamResult(intermediate);
 
 		trans.transform(xmlSource, result1);
 		intermediate.close();
-		//Now run the actual transform
+		// Now run the actual transform
 		trans = transFact.newTransformer(xsltSource);
 		StringReader inter = new StringReader(intermediate.toString());
 		Source interSource = new StreamSource(inter);
@@ -72,24 +72,19 @@ public class TestStudyInfoFlatten extends TestCase {
 		return (csv);
 	}
 
-	public void testContents() {
+	public void testContents() throws Exception {
 		String xslt = "war/xslt/flatten-ssq.xsl";
 		String file = "src/test/resources/NewFile.xml";
 		String cols = null;
 		String datavals = null;
 
 		DOMParser parser = new DOMParser();
-		try {
-			String csv = processFile(file, xslt);
-			StringTokenizer st = new StringTokenizer(csv, "\n");
-			cols = st.nextToken();
-			datavals = st.nextToken();
+		String csv = processFile(file, xslt);
+		StringTokenizer st = new StringTokenizer(csv, "\n");
+		cols = st.nextToken();
+		datavals = st.nextToken();
 
-			parser.parse(file);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		parser.parse(file);
 
 		Document doc = parser.getDocument();
 		DOMImplementation domimpl = doc.getImplementation();
@@ -105,14 +100,14 @@ public class TestStudyInfoFlatten extends TestCase {
 					nodefilter, expandreferences);
 			Node thisNode = null;
 			thisNode = walker.nextNode();
-			
+
 			String[] head = cols.split(",");
 			String[] data = datavals.split(",");
 			int dataIdx = 2;
 			int headIdx = 2;
 			boolean started = false;
 			while (thisNode != null) {
-				
+
 				if (thisNode.getNodeType() == Node.TEXT_NODE) {
 					String value = thisNode.getNodeValue().trim();
 					if (!value.isEmpty()) {
@@ -128,33 +123,36 @@ public class TestStudyInfoFlatten extends TestCase {
 							// TODO Auto-generated catch block
 							e.printStackTrace();
 						}
-						
+
 						if (started) {
 
 							boolean found = false;
 							while (dataIdx < data.length && !found) {
 								String colVal = data[dataIdx++];
-								
+
 								String headVal = head[headIdx++];
-								
-								if (colVal.equals(value) && value.length() == colVal.length()) {
+
+								if (colVal.equals(value)
+										&& value.length() == colVal.length()) {
 									found = true;
 									if (!headVal.endsWith(parent)) {
-										fail("Unexpected parent:"+headVal+" "+parent);
+										fail("Unexpected parent:" + headVal
+												+ " " + parent);
 									}
 									System.out.println(parent + ":" + headVal
 											+ ":" + colVal);
 								} else if (colVal.length() > 0) {
-									fail("Unexpected data:"+headVal+":"+colVal+"Expected:"+parent+":"+value);
+									fail("Unexpected data:" + headVal + ":"
+											+ colVal + "Expected:" + parent
+											+ ":" + value);
 								}
 							}
 						}
 					}
 				}
-				
 
-			//	System.out.println(thisNode.getNodeName() + ":"
-				//		+ thisNode.getNodeValue());
+				// System.out.println(thisNode.getNodeName() + ":"
+				// + thisNode.getNodeValue());
 				thisNode = walker.nextNode();
 			}
 
