e4ff637a8c02a417299ebbdb74f9241ec93106be
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-06-29 14:25:11 +0000

    Initial commit

---
+++ trunk/chassis-model/src/main/resources/fix-db.sql
@@ -0,0 +1,14 @@
+ALTER TABLE `chassis_studies`.`acknowledgementsinstitutiona_0` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(60) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`annualparasitincidence` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`annualparasitincidenceyear` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`thickfilmcalcofparasitemia` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`thinfilmcalcofparasitemia` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`sexualparasitemiapositivethi_0` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`asexualparasitemia` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`geneotypingtodistinguish` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(60) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`transintensityagefromunits` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`institutionwebsitesinstituti_0` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`infectioncompestloci` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`infectioncompestlociopen` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+ALTER TABLE `chassis_studies`.`infectioncompestlociother` CHANGE COLUMN `DTYPE` `DTYPE` VARCHAR(50) NULL DEFAULT NULL;
+
