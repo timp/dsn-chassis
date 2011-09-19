bf31e57a4b88de895da4853e149cc9e6a05f8c7c
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-09-19 22:29:00 +0000

    Add OS specific CLASSPATH separator

--- manta-webapp/scripts/split-studies.sh
+++ manta-webapp/scripts/split-studies.sh
@@ -1,7 +1,15 @@
+if [[ $(uname) == Cygwin ]] 
+then
+ SEPARATOR=';'
+else
+ SEPARATOR=':'
+fi
+
 #get-studies.sh
 CLASSPATH=.
 for i in `ls jars/*`
 do
-	CLASSPATH="$CLASSPATH;./$i"
+	CLASSPATH="$CLASSPATH$SEPARATOR./$i"
 done
+echo classpath ${CLASSPATH}
 java -classpath ${CLASSPATH} org.apache.xalan.xslt.Process -IN studies.xml -XSL split-feed.xsl -OUT foo.out
