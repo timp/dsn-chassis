e4cfcc489deb4cafa53bd186527bf49182d5d3be
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-09-19 22:14:13 +0000

    Match comments to actions

--- manta-webapp/scripts/get-studies.sh
+++ manta-webapp/scripts/get-studies.sh
@@ -19,10 +19,10 @@ TARGET=${HOST}/service/content/studies
 TGT=`curl -k -D - -d "username=${USERNAME}&password=${PASSWORD}" ${TICKETS} 2>&1 | grep Location | grep TGT | awk -F/ '{printf $NF}'`
 #get Service Ticket
 ST=`curl -k -d "service=${TARGET}" -q ${TICKETS}/${TGT}`
-#Get the resource
+#Get the resource ticket
 curl -k -c cookie-jar -o studies.ticket -i -H "Accept: text/xml" -X GET ${TARGET}?ticket=$ST
 
 
-#Make sure that you actually have permission to upload submitted files
+#get the resource
 curl -k -b cookie-jar -o studies.xml ${HOST}/service/content/studies
 
