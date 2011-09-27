2ead1533d60a556d87985dbd635692f1dfaf4c0c
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-09-27 15:37:53 +0000

    Initial version

---
+++ branches/manta-maven/manta-webapp/scripts/clean.sh
@@ -0,0 +1,15 @@
+rm cookie-jar
+rm curated.xml
+rm derivations.xml
+rm -r curated
+rm -r cmis-folders
+rm -r cmis-files
+rm -r cmis-entries
+rm -r studies
+rm -r files
+rm foo.out
+rm studies.ticket
+rm studies.xml
+rm submitted.xml
+rm -r submitted
+
---
+++ branches/manta-maven/manta-webapp/scripts/run.sh
@@ -0,0 +1,8 @@
+
+./clean.sh
+./get-studies.sh
+./split-studies.sh
+./alf-upload-studies.sh
+./get-files.sh
+./alf-upload-files.sh
+
