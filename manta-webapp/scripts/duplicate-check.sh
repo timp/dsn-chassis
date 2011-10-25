03873180ba008889d257d3fce36c8afd1b42c9fd
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-10-25 13:36:24 +0000

    Allow for spaces in filenames

--- manta-webapp/scripts/duplicate-check.sh
+++ manta-webapp/scripts/duplicate-check.sh
@@ -7,7 +7,6 @@ TEMP_NAMES=${TEMP_FILE}.names
 TEMP_UNIQUE_NAMES=${TEMP_NAMES}.unique
 TEMP_DUPLICATE_NAMES=${TEMP_NAMES}.dups
 
-
 for i in submitted/* curated/*
 do
 	FILE=`java -classpath ${CLASSPATH} org.apache.xalan.xslt.Process -IN ${i} -XSL filename.xsl`
@@ -16,29 +15,35 @@ done
 awk -F# '{print $2}' ${TEMP_FILE} | sort > ${TEMP_NAMES}
 sort -u ${TEMP_NAMES} > ${TEMP_UNIQUE_NAMES}
 
-diff ${TEMP_NAMES} ${TEMP_UNIQUE_NAMES} | sort -u | grep \< | awk '{print $2}' > ${TEMP_DUPLICATE_NAMES}
+diff ${TEMP_NAMES} ${TEMP_UNIQUE_NAMES} | sort -u | grep \< | sed -e 's/< //' > ${TEMP_DUPLICATE_NAMES}
 
 
+DUPLICATE_DEF=dupdef
+rm ${DUPLICATE_DEF}
 mkdir duplicates
-for i in `cat ${TEMP_DUPLICATE_NAMES}`
+
+while read i
 do
 	DATE_UPLOADED=0
 	LAST_FILE=
-	for j in `grep "$i" ${TEMP_FILE}`
+	grep "$i" ${TEMP_FILE} > ${TEMP_FILE}.2
+	while read j
 	do
 		FILE=`echo -n $j | awk -F# '{print $1}'`
-		DATE=`java -classpath ${CLASSPATH} org.apache.xalan.xslt.Process -IN ${FILE} -XSL filedate.xsl | sed -e 's/Z$//' -e 's/+01:00$//'`
+		DATE=`java -classpath ${CLASSPATH} org.apache.xalan.xslt.Process -IN "${FILE}" -XSL filedate.xsl | sed -e 's/Z$//' -e 's/+01:00$//'`
 		DATE_STAMP=`date -d $DATE +%s`
 		if [ $DATE_STAMP -gt ${DATE_UPLOADED} ]
 		then
 			if [ ${DATE_UPLOADED} -gt 0 ]
 			then
 				mv $LAST_FILE duplicates
+				echo "$LAST_FILE#$FILE" | sed -e 's#submitted/##' | sed -e 's#curated/##'  >> ${DUPLICATE_DEF}
 			fi
 			DATE_UPLOADED=$DATE_STAMP
 			LAST_FILE=${FILE}
 		else
-			mv $FILE $duplicates
+			mv $FILE duplicates
+			echo "$FILE#$LAST_FILE" | sed -e 's#submitted/##' | sed -e 's#curated/##' >> ${DUPLICATE_DEF}
 		fi
-	done
-done
+	done < ${TEMP_FILE}.2
+done < ${TEMP_DUPLICATE_NAMES}
