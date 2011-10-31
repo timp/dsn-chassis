08f374f10011ab9573c5d7f99253b25c43ffa35a
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-10-31 09:53:28 +0000

    Further checks

--- branches/manta-maven/manta-webapp/scripts/check-for-errors.sh
+++ branches/manta-maven/manta-webapp/scripts/check-for-errors.sh
@@ -1,3 +1,8 @@
+
+
+
+
+
 for i in `grep -l 'Internal Error' cmis-files/*.xml.creat`
 do
 	echo $i
@@ -5,4 +10,20 @@ do
 	egrep '(Exception|Constraint)' $i | grep -v 'Wrapped Exception' | grep -v ScriptException | grep -v ExceptionTranslatorMethodInterceptor | grep -v createStatusException 
 done
 
+#grep 'The Web Script' cmis-files/*.xml.creat | grep 404 | awk -F\< '{print $4}' | awk -F\> '{print $2}' | awk -F\/ '{print $8}' | sort -u > deletedStudies
+echo "Error in creation"
+for i in `grep -l 'Web Script Status 404' cmis-files/*.xml.creat`
+do
+	echo $i
+done
+
 egrep 'studyFileNodeRef=$' set-derivations.out
+
+echo "Files not created"
+for i in cmis-files/*.xml
+do 
+	if [ ! -f $i.creat ]
+	then 
+		echo $i
+	fi
+done
