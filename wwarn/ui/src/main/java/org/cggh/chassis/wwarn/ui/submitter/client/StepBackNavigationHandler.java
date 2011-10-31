cd5447240a6cf38400cdb00938517c9676502014
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-10-31 15:05:04 +0000

    [maven-scm] copy for tag manta-xquery-functions-0.1-beta-2

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/StepBackNavigationHandler.java
+++
@@ -1,9 +0,0 @@
-package org.cggh.chassis.wwarn.ui.submitter.client;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface StepBackNavigationHandler extends EventHandler {
-
-	void onNavigation(StepBackNavigationEvent e);
-
-}
