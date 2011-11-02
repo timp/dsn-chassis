332b3fb4f73471c2c6cccbe1a3b7d5a486afdb24
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-11-02 14:08:18 +0000

    Add read/delete tests

--- trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/StudyControllerRequesterTest.java
+++ trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/StudyControllerRequesterTest.java
@@ -28,70 +28,87 @@ public class StudyControllerRequesterTest extends TestCase {
     super.tearDown();
   }
 
+  private String url(String url) {
+    String fullUrl =  "http://localhost:8888/chassis-rest/service" + url;
+    //System.err.println(fullUrl);
+    return fullUrl;
+  }
+
+  public void testValidate() throws Exception { 
+    HttpResponse r = StudyControllerRequester.create("studies/AJYER.xml", url("/study"));
+    if (r.getBody().indexOf("errors") > 0)
+      System.err.println(r.getBody());
+    
+  }
+  
   /**
-   * Test method for {@link org.cggh.chassis.rest.util.StudyControllerRequester#post(java.lang.String, java.lang.String)}.
+   * Test method for {@link org.cggh.chassis.rest.util.StudyControllerRequester#create(java.lang.String, java.lang.String)}.
    */
   public void testPostStringString() throws Exception {
-    //StudyControllerRequester.post("TBYKQ.xml", url("/study"));
+    //StudyControllerRequester.create("TBYKQ.xml", url("/study"));
     // invalid
-    //StudyControllerRequester.post("studies/ACXFX.xml", url("/study"));
-    //StudyControllerRequester.post("studies/AJYER.xml", url("/study"));
-    //StudyControllerRequester.post("studies/AKUQT.xml", url("/study"));
-    //StudyControllerRequester.post("studies/EUKMD.xml", url("/study"));
-    
+    //StudyControllerRequester.create("studies/ACXFX.xml", url("/study"));
+    //StudyControllerRequester.create("studies/AJYER.xml", url("/study"));
+    //StudyControllerRequester.create("studies/AKUQT.xml", url("/study"));
+    //StudyControllerRequester.create("studies/EUKMD.xml", url("/study"));
+
     /*
     HttpResponse r = StudyControllerRequester.post("studies/ZZSPY.xml", url("/study"));
     assertEquals(200, r.getStatus());
     System.err.println(r.getBody());
-*/
-    
+    */
+
     String studiesDir = DATA_STUDIES;
-    HttpResponse postResponse = StudyControllerRequester.post(studiesDir + "XGQRX.xml", url("/study")); 
+    HttpResponse postResponse = StudyControllerRequester.create(studiesDir + "XGQRX.xml", url("/study"));
     // should be 201?
     assertEquals(postResponse.getBody(), 200, postResponse.getStatus());
 
-    HttpResponse deleteCollectionResponse = StudyControllerRequester.delete(url("/study/")); 
+    HttpResponse deleteCollectionResponse = StudyControllerRequester.delete(url("/study/"));
     assertEquals(deleteCollectionResponse.getBody(), 405, deleteCollectionResponse.getStatus());
-  
-    HttpResponse deleteEntryResponse = StudyControllerRequester.delete(url("/study/XGQRX")); 
-    assertEquals(deleteEntryResponse.getBody(), 200, deleteEntryResponse.getStatus());
-    
-  }
-   private String url(String url) { 
-     return "http://localhost:8888/chassis-rest/service" + url;
-   }
 
-  public void testPostAllStudies() throws Exception { 
+    HttpResponse deleteEntryResponse = StudyControllerRequester.delete(url("/study/XGQRX"));
+    assertEquals(deleteEntryResponse.getBody(), 200, deleteEntryResponse.getStatus());
+
+  }
+
+  public void testPostAllStudies() throws Exception {
     File studiesDir = new File(DATA_STUDIES);
     //File studiesDir = new File("studies");
     @SuppressWarnings("unchecked")
-    Iterator<File> it = FileUtils.iterateFiles(studiesDir, new String[] {"xml"}, false);
+    Iterator<File> it = FileUtils.iterateFiles(studiesDir, new String[] { "xml" }, false);
     int fileCount = 0;
-    while(it.hasNext()){
+    while (it.hasNext()) {
       fileCount++;
       File f = it.next();
-      String studyFileName = DATA_STUDIES  + f.getName(); 
+      String studyFileName = DATA_STUDIES + f.getName();
       //String studyFileName = "studies/"  + it.next().getName();
-      HttpResponse r = StudyControllerRequester.post(studyFileName, url("/study"));
+      HttpResponse r = StudyControllerRequester.create(studyFileName, url("/study"));
       System.out.print(studyFileName);
       System.out.print(" - ");
       System.out.println(r.getBody());
-      if (r.getStatus() == 500) { 
+      if (r.getStatus() == 500) {
         System.out.print(studyFileName);
         System.out.print(" - ");
         System.out.println(r.getBody());
-      } else { 
-        // FIXME should be <errors><error>
-        if (r.getBody().indexOf("error") > 0)
+      } else {
+        if (r.getBody().indexOf("errors") > 0)
           System.err.println(r.getBody());
       }
       // Think you need to delete to rollback
       StudyControllerRequester.delete(url("/study/" + f.getName())).getStatus();
     }
-    
+
     System.out.println("Files: " + fileCount);
     assertEquals(2, fileCount);
   }
 
+  public void testRead() throws Exception {
+    HttpResponse response = StudyControllerRequester.read(url("/study/"));
+    assertEquals(405, response.getStatus());
+    
+    response = StudyControllerRequester.read(url("/studies/"));   
+    assertEquals(200, response.getStatus());
+    System.out.println(response.getBody());
+  }
 
 }
