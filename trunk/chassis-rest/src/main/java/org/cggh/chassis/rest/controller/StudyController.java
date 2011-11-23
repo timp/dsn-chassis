a6195cef5002551ef76ddce8c89e2d6004af0516
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-11-23 16:34:30 +0000

    Extract repeated failure code into a method, eclipse formatting

--- trunk/chassis-rest/src/main/java/org/cggh/chassis/rest/controller/StudyController.java
+++ trunk/chassis-rest/src/main/java/org/cggh/chassis/rest/controller/StudyController.java
@@ -27,27 +27,28 @@ import org.springframework.web.servlet.ModelAndView;
 import org.w3._2005.atom.Entry;
 import org.w3._2005.atom.Feed;
 import org.xml.sax.SAXException;
+
 @Controller
 public class StudyController {
 
   @Autowired
   Jaxb2Marshaller validatingMarshaller;
-  
+
   @Autowired
   StudyDAO studyDAO;
-  
+
   // if the request accept header is xml, or */* then the xml bean is used
   // if the request accept header is empty or text/html then .jsp is used
-  
+
   private static final String STUDY_OBJECT_VIEW_NAME = "study";
   private static final String STUDY_COLLECTION_VIEW_NAME = "studies";
   private static final String ERROR_LIST_VIEW_NAME = "errors";
-    
+
   @RequestMapping(method = RequestMethod.GET, value = "/study/{id}")
-  public ModelAndView getStudy(@PathVariable String id, HttpServletResponse response)  {
+  public ModelAndView getStudy(@PathVariable String id, HttpServletResponse response) {
     Entry entry = studyDAO.getEntry(id);
-    if (entry == null) { 
-      return notFound(id, response);          
+    if (entry == null) {
+      return notFound(id, response);
     } else {
       return new ModelAndView(STUDY_OBJECT_VIEW_NAME, "entry", entry);
     }
@@ -59,87 +60,83 @@ public class StudyController {
     mav.addObject("id", id);
     return mav;
   }
-  
-  @RequestMapping(method=RequestMethod.PUT, value="/study/{id}")
+
+  @RequestMapping(method = RequestMethod.PUT, value = "/study/{id}")
   public ModelAndView updateStudy(@RequestBody String body, @PathVariable String id, HttpServletResponse response) throws JAXBException, SAXException {
     Source source = new StreamSource(new StringReader(body));
-      UnmarshalledEntry unmarshalledResult = EntryUtil.validate(validatingMarshaller, source);
-      //s = m_studyDAO.unmarshal(source);
-      //s= (Entry) jaxb2Mashaller.unmarshal(source);
-      if (unmarshalledResult.getErrors().isEmpty()) {
-        try { 
-          studyDAO.updateEntry(id, unmarshalledResult.getEntry());
-        } catch (Exception e) { 
-          response.setStatus(HttpStatus.SC_BAD_REQUEST);
-          ModelAndView mav = new ModelAndView(ERROR_LIST_VIEW_NAME, "errors", unmarshalledResult.getErrors());
-          mav.addObject("id", id);
-
-          return mav.addObject("exception", e.getMessage());          
-        }
-        response.setStatus(HttpStatus.SC_CREATED);
-        return new ModelAndView(STUDY_OBJECT_VIEW_NAME, "entry", unmarshalledResult.getEntry());
-      } else {
-        //unmarshalledResult.setEntry(null);
-        return new ModelAndView(ERROR_LIST_VIEW_NAME, "errors", unmarshalledResult.getErrors());
-      }
-  }
-  
-  @RequestMapping(method=RequestMethod.POST, value="/study")
-  public ModelAndView addStudy(@RequestBody String body, HttpServletResponse response) 
-        throws JAXBException, SAXException {
-    Source source = new StreamSource(new StringReader(body));
     UnmarshalledEntry unmarshalledResult = EntryUtil.validate(validatingMarshaller, source);
-      
+    // s = m_studyDAO.unmarshal(source);
+    // s= (Entry) jaxb2Mashaller.unmarshal(source);
     if (unmarshalledResult.getErrors().isEmpty()) {
-      try { 
-        studyDAO.saveEntry(unmarshalledResult.getEntry());
-      } catch (Exception e) { 
-          response.setStatus(HttpStatus.SC_BAD_REQUEST);
-          Entry entry = unmarshalledResult.getEntry();
-          ModelAndView mav = new ModelAndView(ERROR_LIST_VIEW_NAME, "errors", unmarshalledResult.getErrors());
-          String studyId = entry == null ? "dummy" : entry.getStudyID();
-          mav.addObject("id", studyId);
-          return mav.addObject("exception", e.getMessage());          
-        }
-        response.setStatus(HttpStatus.SC_CREATED);        
-        return new ModelAndView(STUDY_OBJECT_VIEW_NAME, "entry", unmarshalledResult.getEntry());
-      } else {
-        response.setStatus(HttpStatus.SC_BAD_REQUEST);
-        Entry entry = unmarshalledResult.getEntry();
-        ModelAndView mav = new ModelAndView(ERROR_LIST_VIEW_NAME, "errors", unmarshalledResult.getErrors());
-        String studyId = entry == null ? "dummy" : entry.getStudyID();
-        mav.addObject("id", studyId);
-
+      try {
+        studyDAO.updateEntry(id, unmarshalledResult.getEntry());
+      } catch (Exception e) {
+        ModelAndView mav = marshallingError(response, unmarshalledResult);
+        mav.addObject("exception", e.getMessage());
         return mav;
       }
+      response.setStatus(HttpStatus.SC_CREATED);
+      return new ModelAndView(STUDY_OBJECT_VIEW_NAME, "entry", unmarshalledResult.getEntry());
+    } else {
+      return new ModelAndView(ERROR_LIST_VIEW_NAME, "errors", unmarshalledResult.getErrors());
+    }
   }
 
-  @RequestMapping(method=RequestMethod.POST, value="/studies")
+  @RequestMapping(method = RequestMethod.POST, value = "/study")
+  public ModelAndView addStudy(@RequestBody String body, HttpServletResponse response)
+          throws JAXBException, SAXException {
+    Source source = new StreamSource(new StringReader(body));
+    UnmarshalledEntry unmarshalledResult = EntryUtil.validate(validatingMarshaller, source);
+
+    if (unmarshalledResult.getErrors().isEmpty()) {
+      try {
+        studyDAO.saveEntry(unmarshalledResult.getEntry());
+      } catch (Exception e) {
+        ModelAndView mav = marshallingError(response, unmarshalledResult);
+        mav.addObject("exception", e.getMessage());
+        return mav;
+      }
+      response.setStatus(HttpStatus.SC_CREATED);
+      return new ModelAndView(STUDY_OBJECT_VIEW_NAME, "entry", unmarshalledResult.getEntry());
+    } else {
+      return marshallingError(response, unmarshalledResult);
+    }
+  }
+
+  private ModelAndView marshallingError(HttpServletResponse response, UnmarshalledEntry unmarshalledResult) {
+    response.setStatus(HttpStatus.SC_BAD_REQUEST);
+    Entry entry = unmarshalledResult.getEntry();
+    ModelAndView mav = new ModelAndView(ERROR_LIST_VIEW_NAME, "errors", unmarshalledResult.getErrors());
+    String studyId = entry == null ? "dummy2" : entry.getStudyID();
+    mav.addObject("id", studyId);
+
+    return mav;
+  }
+
+  @RequestMapping(method = RequestMethod.POST, value = "/studies")
   public ModelAndView addStudies(@RequestBody String body, HttpServletResponse response) throws JAXBException, SAXException {
     // Source source = new StreamSource(new StringReader(body));
-    // FIXME not working yet 
+    // FIXME not working yet
     // UnmarshalledEntry unmarshalledResult = EntryUtil.validate(validatingMarshaller, source);
     Feed list = new Feed();
     list.setEntry((List<Entry>) studyDAO.getAll());
     return new ModelAndView(STUDY_COLLECTION_VIEW_NAME, "studies", list);
   }
 
-  
-  @RequestMapping(method=RequestMethod.DELETE, value="/study/{id}")
+  @RequestMapping(method = RequestMethod.DELETE, value = "/study/{id}")
   public ModelAndView removeStudy(@PathVariable String id, HttpServletResponse response) throws NotFoundException {
     if (!studyDAO.remove(id)) {
-      return notFound(id, response); 
+      return notFound(id, response);
     }
     return getStudies();
   }
-  
-  @RequestMapping(method=RequestMethod.GET, value="/studies")
+
+  @RequestMapping(method = RequestMethod.GET, value = "/studies")
   public ModelAndView getStudies() {
     Feed list = new Feed();
     // Wouldn't it be nice it this was setEntrys or similar
     list.setEntry((List<Entry>) studyDAO.getAll());
     return new ModelAndView(STUDY_COLLECTION_VIEW_NAME, "studies", list);
   }
-  
-  
+
 }
