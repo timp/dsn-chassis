ba7d1b4261ba9c4ffec172f2af0b9dfe3cebaaf4
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-11-30 19:41:49 +0000

    add evict method

--- chassis-rest/src/main/java/org/cggh/chassis/rest/dao/StudyDAO.java
+++ chassis-rest/src/main/java/org/cggh/chassis/rest/dao/StudyDAO.java
@@ -1,5 +1,9 @@
 package org.cggh.chassis.rest.dao;
 
+import java.lang.IllegalArgumentException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Collection;
 
 import javax.persistence.EntityManager;
@@ -7,37 +11,49 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.Query;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.w3._2005.atom.Entry;
+import org.apache.commons.dbcp.BasicDataSource;
+import org.cggh.chassis.rest.bean.Options;
+import org.eclipse.persistence.jpa.JpaEntityManager;
 
 public class StudyDAO {
 	// http://static.springsource.org/spring/docs/3.0.5.RELEASE/reference/orm.html#orm-jpa
 
 	private EntityManagerFactory emf;
+	private EntityManager em;
+
+  @Autowired
+  BasicDataSource dataSource;
+  @Autowired
+  Options options;
+  private String databaseName;
+  @Autowired
+  LocalContainerEntityManagerFactoryBean emfBean;
+	
 
 	@PersistenceUnit
 	public void setEntityManagerFactory(EntityManagerFactory emf) {
 		this.emf = emf;
+    this.em = this.emf.createEntityManager();
 	}
 
   public Entry getEntry(String id) {
-    final EntityManager em = emf.createEntityManager();
     final Entry beta = em.find(Entry.class, id);
-    em.close();
     return beta;
   }
 
   @SuppressWarnings("unchecked")
   public Collection<Entry> getAll() {
-    final EntityManager em = emf.createEntityManager();
     Query query = em.createQuery("SELECT e FROM Entry e");
     Collection<Entry> resultsList = (Collection<Entry>)query.getResultList();
-    em.close();
     return resultsList;
   }
 
 	public void saveEntry(Entry entry) {
-		final EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
+    if (!em.getTransaction().isActive())
+      em.getTransaction().begin();
 		try {
       em.persist(entry);
       em.getTransaction().commit();
@@ -45,14 +61,12 @@ public class StudyDAO {
       if (em.isOpen() &&  em.getTransaction().isActive())
         em.getTransaction().rollback();
       throw e;
-		} finally { 
-		  em.close();
 		}
 	}
 
 	public void updateEntry(String id, Entry entry) {
-		final EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
+    if (!em.getTransaction().isActive())
+      em.getTransaction().begin();
     try {	
 		  em.merge(entry);
 	    em.getTransaction().commit();
@@ -60,16 +74,14 @@ public class StudyDAO {
       if (em.isOpen() &&  em.getTransaction().isActive())
         em.getTransaction().rollback();
       throw e;
-    } finally { 
-      em.close();
     }
 	}
 
 	public boolean remove(String id)  {
-		final EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
 		Entry entry = em.find(Entry.class, id);
 		boolean found = false;
+    if (!em.getTransaction().isActive())
+      em.getTransaction().begin();
 		if (entry != null) {
 		  found = true;
       try {
@@ -79,12 +91,34 @@ public class StudyDAO {
         if (em.isOpen() &&  em.getTransaction().isActive())
           em.getTransaction().rollback();
         throw e;
-      } finally { 
-        em.close();
       }
-		} else { 
-      em.close();		  
 		}
 		return found;
 	}
+
+  public void evict() {
+    ((JpaEntityManager)em.getDelegate()).getServerSession().getIdentityMapAccessor().invalidateAll();
+    try {
+      if (options.get("databaseName") == null)
+        throw new IllegalArgumentException("Option databaseName not set");
+      databaseName = options.get("databaseName");
+      Connection c = dataSource.getConnection();
+      System.err.println("Db name: " + databaseName);
+      System.err.println("Connection: " + c);
+      Statement s = c.createStatement();
+      s.execute("drop database " + databaseName);
+      s.execute("create database " + databaseName);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }    
+    em.close();
+    emf.close();
+    emf = null;
+    
+    // @see http://thejavablog.wordpress.com/2008/09/06/how-to-create-a-toplink-entitymanagerfactory-with-spring-outside-a-j2ee-container/
+    emfBean.afterPropertiesSet();
+    emf =  emfBean.getObject();
+    em = emf.createEntityManager();
+    ((JpaEntityManager)em.getDelegate()).getServerSession().getIdentityMapAccessor().initializeAllIdentityMaps();
+  }
 }
