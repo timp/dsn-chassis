2981c241d2235e83d47fcf6178b9f79e5ffee861
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-12-01 14:14:26 +0000

    Refactoring into unit tests and runner

--- trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/DownloadLiveCreateDBTest.java
+++ trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/DownloadLiveCreateDBTest.java
@@ -4,29 +4,27 @@ import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.util.Date;
+import java.util.Iterator;
 
+import org.apache.commons.io.FileUtils;
 import org.cggh.casutils.CasProtectedResourceDownloader;
 import org.cggh.casutils.CasProtectedResourceDownloaderFactory;
 import org.cggh.casutils.NotFoundException;
 
-import junit.framework.TestCase;
 
 /**
  * @author timp
  * @since 2011-11-16
  */
-public class AAAGetStudyFeedTest extends TestCase {
+public class DownloadLiveCreateDBTest extends AbstractUtilSpec {
 
-  protected static ChassisRestConfig config;
-  
-  
-  private static final String LOCAL_STUDIES_FEED_FILENAME = "downloaded/studies_feed.xml";
+  protected static final String LOCAL_STUDIES_FEED_FILENAME = "downloaded/studies_feed.xml";
   private static String wwarnLivePassord;
   
-  public AAAGetStudyFeedTest() { 
+  public DownloadLiveCreateDBTest() { 
     super();
   }
-  public AAAGetStudyFeedTest(String name) { 
+  public DownloadLiveCreateDBTest(String name) { 
     super(name);
   }
   
@@ -34,7 +32,6 @@ public class AAAGetStudyFeedTest extends TestCase {
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    config = new ChassisRestConfig();
     wwarnLivePassord = config.getConfiguration().get("wwarn-live-password");
   }
 
@@ -42,11 +39,60 @@ public class AAAGetStudyFeedTest extends TestCase {
   public void testGetWwarnStudyFeed() throws Exception {
     if (wwarnLivePassord != null) {
       runIt();
+      testPostsFromDirectory(DATA_STUDIES);      
     } else { 
       System.err.println("No password set in " + config.getConfiguration().getFileName());
     }
       
   }
+
+  private void testPostsFromDirectory(String directory) throws Exception { 
+    HttpResponse response = StudyControllerRequester.uncache(url("/uncache"));
+    System.out.println(response.getBody());
+    assertEquals(200, response.getStatus());
+    response = StudyControllerRequester.read(url("/studies/"));   
+    assertEquals(200, response.getStatus());
+    
+    
+    //assertTrue(response.getBody(), response.getBody().indexOf('\n') == -1); // empty feed
+
+    
+    File studiesDir = new File(directory);
+    @SuppressWarnings("unchecked")
+    Iterator<File> it = FileUtils.iterateFiles(studiesDir, new String[] { "xml" }, false);
+    int fileCount = 0;
+    int failCount = 0;
+    while (it.hasNext()) {
+      fileCount++;
+      File f = it.next();
+      String studyFileName = directory + f.getName();
+      String entryUrl = url("/study/" + f.getName());
+      if (StudyControllerRequester.read(url("/study/" + f.getName())).getStatus() == 200) {
+        int deleteStatus = StudyControllerRequester.delete(entryUrl).getStatus();
+        System.err.println("Deleted existing " + entryUrl + " : " + deleteStatus);
+      }
+      HttpResponse r = StudyControllerRequester.create(studyFileName, url("/study"));
+      //System.out.println(studyFileName);
+      //System.out.print(" - ");
+      //System.out.println(r.getBody());
+      if (r.getStatus() != 201) {
+        System.out.print(studyFileName);
+        System.out.print(" - ");
+        System.out.println(r.getBody());
+        if (r.getBody().indexOf("errors") > 0) { 
+          System.err.println(r.getBody());
+        }
+        failCount ++;
+      } else {
+        //StudyControllerRequester.delete(url(entryUrl)).getStatus();
+      }
+    }
+  
+    System.out.println("Files: " + fileCount + " fail count: " + failCount);
+    
+  }
+  
+  
 
   private void runIt() throws NotFoundException, IOException {
     Date start = new Date();
