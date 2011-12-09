a7a5cf820f3c776b03745c812aabaef7535f6b0a
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-12-09 17:29:09 +0000

    Initial

---
+++ trunk/chassis-hyperjaxb3-naming/src/main/java/org/cggh/chassis/hyperjaxb3/ejb/strategy/naming/impl/CustomNaming.java
@@ -0,0 +1,84 @@
+package org.cggh.chassis.hyperjaxb3.ejb.strategy.naming.impl;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.Validate;
+import org.jvnet.hyperjaxb3.ejb.strategy.naming.impl.DefaultNaming;
+
+import com.sun.tools.xjc.model.nav.NType;
+import com.sun.tools.xjc.outline.Outline;
+
+public class CustomNaming extends DefaultNaming {
+  private Map<String, String> nameKeyMap = new TreeMap<String, String>();
+
+  private Map<String, String> keyNameMap = new TreeMap<String, String>();
+
+  
+  @Override
+  public void afterPropertiesSet() throws Exception {
+
+    final Set<Entry<Object, Object>> entries = getReservedNames()
+        .entrySet();
+    for (final Entry<Object, Object> entry : entries) {
+      final Object entryKey = entry.getKey();
+      if (entryKey != null) {
+        final String key = entryKey.toString();
+        final Object entryValue = entry.getValue();
+        final String value = entryValue == null
+            || "".equals(entryValue.toString().trim()) ? key + "_"
+            : entryValue.toString();
+        nameKeyMap.put(key, value);
+        keyNameMap.put(value, key);
+      }
+    }
+  }
+
+  
+  @Override
+  public String getName(final String draftName) {
+
+    Validate.notNull(draftName, "Name must not be null.");
+    final String name = draftName.replace('$', '_');
+    if (nameKeyMap.containsKey(name)) {
+      return (String) nameKeyMap.get(name);
+    } else if (name.length() >= getMaxIdentifierLength()) {
+      for (int i = 0;; i++) {
+        final String suffix = Integer.toString(i);
+        final String prefix = name.substring(0,
+            getMaxIdentifierLength() - suffix.length() - 1);
+        final String identifier = prefix + "_" + suffix;
+        if (!keyNameMap.containsKey(identifier)) {
+          nameKeyMap.put(name, identifier);
+          keyNameMap.put(identifier, name);
+          return identifier;
+        }
+      }
+    } else if (getReservedNames().contains(name)) {
+      return name + "_";
+    } else {
+      return name;
+    }
+
+  }
+
+  @Override
+  public String getTableName(String qualifiedName) {
+    System.err.println("qualifiedName:" + qualifiedName);    
+    return super.getTableName(qualifiedName);
+  }
+
+  @Override
+  public String getEntityName(Outline outline, NType type) {
+    System.err.println("getEntityName:" + super.getEntityName(outline, type));    
+    return super.getEntityName(outline, type);
+  }
+
+  @Override
+  public int getMaxIdentifierLength() {
+    return 60;
+  }
+  
+}
