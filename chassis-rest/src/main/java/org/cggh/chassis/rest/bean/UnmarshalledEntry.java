3eb32efa2d93a7c132b8202b66d34ff5aab33e88
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-12-13 17:58:16 +0000

    Refactor to allow marshalling of feeds as well as entries

--- chassis-rest/src/main/java/org/cggh/chassis/rest/bean/UnmarshalledEntry.java
+++ chassis-rest/src/main/java/org/cggh/chassis/rest/bean/UnmarshalledEntry.java
@@ -1,57 +1,26 @@
 package org.cggh.chassis.rest.bean;
 
-import java.util.List;
-import java.util.Vector;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElementWrapper;
-import javax.xml.bind.annotation.XmlRootElement;
-
 import org.w3._2005.atom.Entry;
 
-@XmlRootElement(name="results")
-@XmlAccessorType(XmlAccessType.FIELD)
-
-public class UnmarshalledEntry {
+public class UnmarshalledEntry extends UnmarshalledObject {
 	
-  private String id; 
-  private String exceptionMessage; 
+  protected Entry entry;
   
-	private Entry entry;
-	@XmlElementWrapper(name="errors")
-	@XmlElement(name="error")
-	private List<ValidationError> errors = new Vector<ValidationError>();
-	
-	public Entry getEntry() {
+  public Entry getEntry() {
 		return entry;
 	}
 
 	public void setEntry(Entry entry) {
 		this.entry = entry;
 	}
-	public List<ValidationError> getErrors() {
-		return errors;
-	}
-	
-	public void addError(ValidationError e) { 
-	  errors.add(e);
-	}
 
-  public String getId() {
-    return id;
+  @Override
+  public void setIt(Object it) {
+    setEntry((Entry)it);
   }
 
-  public void setId(String id) {
-    this.id = id;
-  }
-
-  public String getExceptionMessage() {
-    return exceptionMessage;
-  }
-
-  public void setException(String exceptionMessage) {
-    this.exceptionMessage = exceptionMessage;
+  @Override
+  public Object getIt() {
+    return getEntry();
   }
 }
