ed8e1a47e750edeb04ed6983637dc712c42385cc
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2011-12-20 15:42:20 +0000

    Initial sql reports

---
+++ chassis-rest/src/main/sql/TransmissionIntensityByModule.sql
@@ -0,0 +1,25 @@
+use chassisDb;
+select Entry.StudyId, Entry.PublishedItem, Study.Modules, Study.StudyTitle,
+   StudyInfo.START_, StudyInfo.END_, POPULATION,
+   TransmissionIntensity.TransmissionIntensityAgeFrom,
+   TransmissionIntensity.EntomologicalInoculationRateYear,
+   TransmissionIntensity.TransmissionIntensityAgeToUnits,
+   TransmissionIntensity.AnnualParasitologicalIncidence,
+   TransmissionIntensity.AnnualParasitologicalIncidence,
+   TransmissionIntensity.EntomologicalInoculationRate,
+   TransmissionIntensity.SeasonalTransmission,
+   TransmissionIntensity.TransmissionIntensityAgeTo,
+   TransmissionIntensity.ParaSitePrevalenceYear,
+   TransmissionIntensity.ParaSitePrevalence,
+   TransmissionIntensity.TransmissionIntensityAgeFromUnits,
+   TransmissionIntensity.TransmissionMonths
+ from Entry, Content, Study, StudyInfo, Sites, Site, TransmissionIntensity
+ where
+ Entry.Content_Entry_Hjid = Content.Hjid and
+ Content.Study_Content_Hjid = Study.Hjid and
+ Study.StudyInfo_Study_Hjid = StudyInfo.Hjid and
+ StudyInfo.Sites_StudyInfo_Hjid = Sites.Hjid and
+ Sites.Hjid  = Site.Site_Sites_Hjid and
+ Site.TransmissionIntensity_Site_Hjid = TransmissionIntensity.Hjid
+ order by Study.Modules
+
