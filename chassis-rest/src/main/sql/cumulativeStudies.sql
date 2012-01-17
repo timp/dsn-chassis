10f89653726c810fd463c9c9a56e45368aad181e
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-01-17 15:45:03 +0000

    [maven-release-plugin]  copy for tag chassis-hyperjaxb3-naming-1.0-3

--- chassis-rest/src/main/sql/cumulativeStudies.sql
+++
@@ -1,5 +0,0 @@
-SELECT e1.PublishedItem, COUNT(e2.PublishedItem) AS TotalStudies
-  FROM Entry e1, Entry e2 
-  WHERE e1.PublishedItem >= e2.PublishedItem
-  GROUP BY e1.PublishedItem
-  ORDER BY e1.PublishedItem 
