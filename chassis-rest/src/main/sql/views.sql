bce9258ee4f35d01d9dd11a8bd3fd43547aeba0d
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-01-18 23:48:35 +0000

    Case of table names, add drops

--- chassis-rest/src/main/sql/views.sql
+++ chassis-rest/src/main/sql/views.sql
@@ -1,7 +1,7 @@
                                                                      
                                              
-
-CREATE VIEW `v_studydetails` AS 
+DROP VIEW IF EXISTS `Studies`;
+CREATE VIEW `Studies` AS 
 
 	select `e`.`StudyID` AS `StudyID`
 			,`s`.`StudyTitle` AS `studyTitle`
@@ -9,71 +9,78 @@ CREATE VIEW `v_studydetails` AS
 			,`s`.`CuratorNotes` AS `curatorNotes`
 			,`s`.`StudyIsPublished` AS `studyIsPublished`
 			,`s`.`ExplorerDisplay` AS `explorerDisplay`
-			,`s`.`Modules` AS `modules` from `entry` `e` left join `content` `c` on`e`.`Content_Entry_Hjid` = `c`.`Hjid`
-		 left join `study` `s` on`c`.`Study_Content_Hjid` = `s`.`Hjid`;
-;
+			,`s`.`Modules` AS `modules` 
+        from `Entry` `e` 
+		left join `Content` `c` on `e`.`Content_Entry_Hjid` = `c`.`Hjid`
+		left join `Study` `s` on `c`.`Study_Content_Hjid` = `s`.`Hjid`;
 
-CREATE VIEW `v_sites` AS 
+
+
+DROP VIEW IF EXISTS `StudySites`;
+CREATE VIEW `StudySites` AS 
 	select `e`.`StudyID` AS `StudyID`
-			,`site`.`Hjid` AS `Hjid`
-			,`site`.`DTYPE` AS `DTYPE`
-			,`site`.`Region` AS `Region`
-			,`site`.`LookupAddress` AS `LookupAddress`
-			,`site`.`TestingDelay` AS `TestingDelay`
-			,`site`.`SiteCode` AS `SiteCode`
-			,`site`.`Country` AS `Country`
-			,`site`.`Anticoagulant` AS `Anticoagulant`
-			,`site`.`Locality` AS `Locality`
-			,`site`.`Longitude` AS `Longitude`
-			,`site`.`Latitude` AS `Latitude`
-			,`site`.`TransportAndStorageTemperature` AS `TransportAndStorageTemperature`
-			,`site`.`District` AS `District`
-			,`site`.`Population` AS `Population`
-			,`site`.`TransmissionIntensity_Site_Hjid` AS `TransmissionIntensity_Site_Hjid`
-			,`site`.`Site_Sites_Hjid` AS `Site_Sites_Hjid` from `site` left join `sites` on`sites`.`Hjid` = `site`.`Site_Sites_Hjid`
-		 left join `studyinfo` `si` on`si`.`Sites_StudyInfo_Hjid` = `sites`.`Hjid`
-		 left join `study` `s` on`s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
-		 left join `content` `c` on`c`.`Study_Content_Hjid` = `s`.`Hjid`
-		 left join `entry` `e` on`e`.`Content_Entry_Hjid` = `c`.`Hjid`;
+			,`Site`.`Hjid` AS `Hjid`
+			,`Site`.`DTYPE` AS `DTYPE`
+			,`Site`.`Region` AS `Region`
+			,`Site`.`LookupAddress` AS `LookupAddress`
+			,`Site`.`TestingDelay` AS `TestingDelay`
+			,`Site`.`SiteCode` AS `SiteCode`
+			,`Site`.`Country` AS `Country`
+			,`Site`.`Anticoagulant` AS `Anticoagulant`
+			,`Site`.`Locality` AS `Locality`
+			,`Site`.`Longitude` AS `Longitude`
+			,`Site`.`Latitude` AS `Latitude`
+			,`Site`.`TransportAndStorageTemperature` AS `TransportAndStorageTemperature`
+			,`Site`.`District` AS `District`
+			,`Site`.`Population` AS `Population`
+			,`Site`.`TransmissionIntensity_Site_Hjid` AS `TransmissionIntensity_Site_Hjid`
+			,`Site`.`Site_Sites_Hjid` AS `Site_Sites_Hjid` 
+from `Site` left join `Sites` on`Sites`.`Hjid` = `Site`.`Site_Sites_Hjid`
+		 left join `StudyInfo` `si` on`si`.`Sites_StudyInfo_Hjid` = `Sites`.`Hjid`
+		 left join `Study` `s` on`s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
+		 left join `Content` `c` on`c`.`Study_Content_Hjid` = `s`.`Hjid`
+		 left join `Entry` `e` on`e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 ;
 
-CREATE VIEW `v_clinicaldrugs` AS 
+DROP VIEW IF EXISTS `ClinicalDrugs`;
+CREATE VIEW `ClinicalDrugs` AS 
 	select `e`.`StudyID` AS `StudyID`
-			,`drug`.`Hjid` AS `Hjid`
-			,`drug`.`DTYPE` AS `DTYPE`
-			,`drug`.`FeedingOther` AS `FeedingOther`
-			,`drug`.`DrugName` AS `DrugName`
-			,`drug`.`Feeding` AS `Feeding`
-			,`drug`.`TradeName` AS `TradeName`
-			,`drug`.`DrugDosingDeterminant` AS `DrugDosingDeterminant`
-			,`drug`.`ManufacturerOther` AS `ManufacturerOther`
-			,`drug`.`TradeNameOther` AS `TradeNameOther`
-			,`drug`.`Manufacturer` AS `Manufacturer`
-			,`drug`.`FatPerMeal` AS `FatPerMeal`
-			,`drug`.`DrugStorage` AS `DrugStorage`
-			,`drug`.`DrugNameOther` AS `DrugNameOther`
-			,`drug`.`Comments` AS `Comments`
-			,`drug`.`AdministrationRoute` AS `AdministrationRoute`
-			,`drug`.`ReadministeredOnVomitting` AS `ReadministeredOnVomitting`
-			,`drug`.`ActiveIngredients_Drug_Hjid` AS `ActiveIngredients_Drug_Hjid`
-			,`drug`.`WeightGroupDosing_Drug_Hjid` AS `WeightGroupDosing_Drug_Hjid`
-			,`drug`.`AgeDosing_Drug_Hjid` AS `AgeDosing_Drug_Hjid`
-			,`drug`.`Batches_Drug_Hjid` AS `Batches_Drug_Hjid`
-			,`drug`.`WeightDosing_Drug_Hjid` AS `WeightDosing_Drug_Hjid`
-			,`drug`.`Drug_Drugs_Hjid` AS `Drug_Drugs_Hjid` from `drug` left join `drugs` on`drug`.`Drug_Drugs_Hjid` = `drugs`.`Hjid`
-		 left join `regimen` `r` on`r`.`Drugs_Regimen_Hjid` = `drugs`.`Hjid`
-		 left join `regimens` `rs` on`r`.`Regimen_Regimens_Hjid` = `rs`.`Hjid`
-		 left join `treatment` `t` on`t`.`Regimens_Treatment_Hjid` = `rs`.`Hjid`
-		 left join `clinical` on`clinical`.`Treatment_Clinical_Hjid` = `t`.`Hjid`
-		 left join `studyinfo` `si` on`si`.`Clinical_StudyInfo_Hjid` = `clinical`.`Hjid`
-		 left join `study` `s` on`s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
-		 left join `content` `c` on`c`.`Study_Content_Hjid` = `s`.`Hjid`
-		 left join `entry` `e` on`e`.`Content_Entry_Hjid` = `c`.`Hjid`;
+			,`Drug`.`Hjid` AS `Hjid`
+			,`Drug`.`DTYPE` AS `DTYPE`
+			,`Drug`.`FeedingOther` AS `FeedingOther`
+			,`Drug`.`DrugName` AS `DrugName`
+			,`Drug`.`Feeding` AS `Feeding`
+			,`Drug`.`TradeName` AS `TradeName`
+			,`Drug`.`DrugDosingDeterminant` AS `DrugDosingDeterminant`
+			,`Drug`.`ManufacturerOther` AS `ManufacturerOther`
+			,`Drug`.`TradeNameOther` AS `TradeNameOther`
+			,`Drug`.`Manufacturer` AS `Manufacturer`
+			,`Drug`.`FatPerMeal` AS `FatPerMeal`
+			,`Drug`.`DrugStorage` AS `DrugStorage`
+			,`Drug`.`DrugNameOther` AS `DrugNameOther`
+			,`Drug`.`Comments` AS `Comments`
+			,`Drug`.`AdministrationRoute` AS `AdministrationRoute`
+			,`Drug`.`ReadministeredOnVomitting` AS `ReadministeredOnVomitting`
+			,`Drug`.`ActiveIngredients_Drug_Hjid` AS `ActiveIngredients_Drug_Hjid`
+			,`Drug`.`WeightGroupDosing_Drug_Hjid` AS `WeightGroupDosing_Drug_Hjid`
+			,`Drug`.`AgeDosing_Drug_Hjid` AS `AgeDosing_Drug_Hjid`
+			,`Drug`.`Batches_Drug_Hjid` AS `Batches_Drug_Hjid`
+			,`Drug`.`WeightDosing_Drug_Hjid` AS `WeightDosing_Drug_Hjid`
+			,`Drug`.`Drug_Drugs_Hjid` AS `Drug_Drugs_Hjid` 
+from `Drug` left join `Drugs` on `Drug`.`Drug_Drugs_Hjid` = `Drugs`.`Hjid`
+		 left join `Regimen` `r` on`r`.`Drugs_Regimen_Hjid` = `Drugs`.`Hjid`
+		 left join `Regimens` `rs` on`r`.`Regimen_Regimens_Hjid` = `rs`.`Hjid`
+		 left join `Treatment` `t` on`t`.`Regimens_Treatment_Hjid` = `rs`.`Hjid`
+		 left join `Clinical` on`Clinical`.`Treatment_Clinical_Hjid` = `t`.`Hjid`
+		 left join `StudyInfo` `si` on`si`.`Clinical_StudyInfo_Hjid` = `Clinical`.`Hjid`
+		 left join `Study` `s` on`s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
+		 left join `Content` `c` on`c`.`Study_Content_Hjid` = `s`.`Hjid`
+		 left join `Entry` `e` on`e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 ;
 
 
-
-CREATE VIEW v_pkAnalytes AS
+DROP VIEW IF EXISTS `pkAnalytes`;
+CREATE VIEW `pkAnalytes` AS
 
 SELECT `e`.`StudyID` AS `StudyID`
      , `a`.`Hjid` AS `Hjid`
@@ -86,32 +93,34 @@ SELECT `e`.`StudyID` AS `StudyID`
      , `a`.`UnitsOfMeasure` AS `UnitsOfMeasure`
      , `a`.`Analyte_Analytes_Hjid` AS `Analyte_Analytes_Hjid`
 FROM
-  `analyte` `a`
-LEFT JOIN `analytes` `ans` ON `a`.`Analyte_Analytes_Hjid` = `ans`.`Hjid`
-LEFT JOIN `pharmacology` `p` ON `p`.`Analytes_Pharmacology_Hjid` = `ans`.`Hjid`
-LEFT JOIN `studyinfo` `si` ON `si`.`Pharmacology_StudyInfo_Hjid` = `p`.`Hjid`
-LEFT JOIN `study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
-LEFT JOIN `content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
-LEFT JOIN `entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
+  `Analyte` `a`
+LEFT JOIN `Analytes`  ON `a`.`Analyte_Analytes_Hjid` = Analytes.`Hjid`
+LEFT JOIN `Pharmacology` `p` ON `p`.`Analytes_Pharmacology_Hjid` = Analytes.`Hjid`
+LEFT JOIN `StudyInfo` `si` ON `si`.`Pharmacology_StudyInfo_Hjid` = `p`.`Hjid`
+LEFT JOIN `Study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
+LEFT JOIN `Content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
+LEFT JOIN `Entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 
-;
 
-CREATE VIEW v_pkSamples AS
+
+DROP VIEW IF EXISTS `pkSamples`;
+CREATE VIEW `pkSamples` AS
 
 SELECT `e`.`StudyID` AS `StudyID`
-     ,sam.*
+     ,Sample.*
 FROM
-  `sample` `sam`
-LEFT JOIN `samples` `sams` ON `sam`.`Sample_Samples_Hjid` = `sams`.`Hjid`
-LEFT JOIN `pharmacology` `p` ON `p`.`Samples_Pharmacology_Hjid` = `sams`.`Hjid`
-LEFT JOIN `studyinfo` `si` ON `si`.`Pharmacology_StudyInfo_Hjid` = `p`.`Hjid`
-LEFT JOIN `study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
-LEFT JOIN `content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
-LEFT JOIN `entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
+  `Sample` 
+LEFT JOIN `Samples`  ON `Sample`.`Sample_Samples_Hjid` = `Samples`.`Hjid`
+LEFT JOIN `Pharmacology` `p` ON `p`.`Samples_Pharmacology_Hjid` = `Samples`.`Hjid`
+LEFT JOIN `StudyInfo` `si` ON `si`.`Pharmacology_StudyInfo_Hjid` = `p`.`Hjid`
+LEFT JOIN `Study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
+LEFT JOIN `Content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
+LEFT JOIN `Entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 
-;
 
-CREATE VIEW v_pkdetails AS
+
+DROP VIEW IF EXISTS `pkdetails`;
+CREATE VIEW `pkdetails` AS
 
 SELECT `e`.`StudyID` AS `StudyID`
      , `p`.`Hjid` AS `Hjid`
@@ -123,10 +132,9 @@ SELECT `e`.`StudyID` AS `StudyID`
      , `p`.`Analytes_Pharmacology_Hjid` AS `Analytes_Pharmacology_Hjid`
      , `p`.`AssayReferences_Pharmacology_Hjid` AS `AssayReferences_Pharmacology_Hjid`
 FROM
-  `pharmacology` `p`
-LEFT JOIN `studyinfo` `si` ON `si`.`Pharmacology_StudyInfo_Hjid` = `p`.`Hjid`
-LEFT JOIN `study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
-LEFT JOIN `content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
-LEFT JOIN `entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
+  `Pharmacology` `p`
+LEFT JOIN `StudyInfo` `si` ON `si`.`Pharmacology_StudyInfo_Hjid` = `p`.`Hjid`
+LEFT JOIN `Study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
+LEFT JOIN `Content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
+LEFT JOIN `Entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 
-;
