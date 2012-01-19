9a987a653fc181e7ce3e4d16575b3a90e7882532
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-01-19 18:04:42 +0000

    Fix security, case, tabs

--- chassis-rest/src/main/sql/views.sql
+++ chassis-rest/src/main/sql/views.sql
@@ -1,86 +1,79 @@
                                                                      
-                                             
-DROP VIEW IF EXISTS `Studies`;
-CREATE VIEW `Studies` AS 
-
-	select `e`.`StudyID` AS `StudyID`
-			,`s`.`StudyTitle` AS `studyTitle`
-			,`s`.`StudyStatus` AS `studyStatus`
-			,`s`.`CuratorNotes` AS `curatorNotes`
-			,`s`.`StudyIsPublished` AS `studyIsPublished`
-			,`s`.`ExplorerDisplay` AS `explorerDisplay`
-			,`s`.`Modules` AS `modules` 
-        from `Entry` `e` 
-		left join `Content` `c` on `e`.`Content_Entry_Hjid` = `c`.`Hjid`
-		left join `Study` `s` on `c`.`Study_Content_Hjid` = `s`.`Hjid`;
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW  `Studies` AS 
+  SELECT `e`.`StudyID` AS `StudyID`
+      ,`s`.`StudyTitle` AS `studyTitle`
+      ,`s`.`StudyStatus` AS `studyStatus`
+      ,`s`.`CuratorNotes` AS `curatorNotes`
+      ,`s`.`StudyIsPublished` AS `studyIsPublished`
+      ,`s`.`ExplorerDisplay` AS `explorerDisplay`
+      ,`s`.`Modules` AS `modules` 
+        FROM `Entry` `e` 
+    LEFT JOIN `Content` `c` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`
+    LEFT JOIN `Study` `s` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`;
 
 
 
-DROP VIEW IF EXISTS `StudySites`;
-CREATE VIEW `StudySites` AS 
-	select `e`.`StudyID` AS `StudyID`
-			,`Site`.`Hjid` AS `Hjid`
-			,`Site`.`DTYPE` AS `DTYPE`
-			,`Site`.`Region` AS `Region`
-			,`Site`.`LookupAddress` AS `LookupAddress`
-			,`Site`.`TestingDelay` AS `TestingDelay`
-			,`Site`.`SiteCode` AS `SiteCode`
-			,`Site`.`Country` AS `Country`
-			,`Site`.`Anticoagulant` AS `Anticoagulant`
-			,`Site`.`Locality` AS `Locality`
-			,`Site`.`Longitude` AS `Longitude`
-			,`Site`.`Latitude` AS `Latitude`
-			,`Site`.`TransportAndStorageTemperature` AS `TransportAndStorageTemperature`
-			,`Site`.`District` AS `District`
-			,`Site`.`Population` AS `Population`
-			,`Site`.`TransmissionIntensity_Site_Hjid` AS `TransmissionIntensity_Site_Hjid`
-			,`Site`.`Site_Sites_Hjid` AS `Site_Sites_Hjid` 
-from `Site` left join `Sites` on`Sites`.`Hjid` = `Site`.`Site_Sites_Hjid`
-		 left join `StudyInfo` `si` on`si`.`Sites_StudyInfo_Hjid` = `Sites`.`Hjid`
-		 left join `Study` `s` on`s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
-		 left join `Content` `c` on`c`.`Study_Content_Hjid` = `s`.`Hjid`
-		 left join `Entry` `e` on`e`.`Content_Entry_Hjid` = `c`.`Hjid`;
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW  `StudySites` AS 
+  SELECT `e`.`StudyID` AS `StudyID`
+      ,`Site`.`Hjid` AS `Hjid`
+      ,`Site`.`DTYPE` AS `DTYPE`
+      ,`Site`.`Region` AS `Region`
+      ,`Site`.`LookupAddress` AS `LookupAddress`
+      ,`Site`.`TestingDelay` AS `TestingDelay`
+      ,`Site`.`SiteCode` AS `SiteCode`
+      ,`Site`.`Country` AS `Country`
+      ,`Site`.`Anticoagulant` AS `Anticoagulant`
+      ,`Site`.`Locality` AS `Locality`
+      ,`Site`.`Longitude` AS `Longitude`
+      ,`Site`.`Latitude` AS `Latitude`
+      ,`Site`.`TransportAndStorageTemperature` AS `TransportAndStorageTemperature`
+      ,`Site`.`District` AS `District`
+      ,`Site`.`Population` AS `Population`
+      ,`Site`.`TransmissionIntensity_Site_Hjid` AS `TransmissionIntensity_Site_Hjid`
+      ,`Site`.`Site_Sites_Hjid` AS `Site_Sites_Hjid` 
+from `Site` LEFT JOIN `Sites` ON `Sites`.`Hjid` = `Site`.`Site_Sites_Hjid`
+     LEFT JOIN `StudyInfo` `si` ON `si`.`Sites_StudyInfo_Hjid` = `Sites`.`Hjid`
+     LEFT JOIN `Study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
+     LEFT JOIN `Content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
+     LEFT JOIN `Entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 ;
 
-DROP VIEW IF EXISTS `ClinicalDrugs`;
-CREATE VIEW `ClinicalDrugs` AS 
-	select `e`.`StudyID` AS `StudyID`
-			,`Drug`.`Hjid` AS `Hjid`
-			,`Drug`.`DTYPE` AS `DTYPE`
-			,`Drug`.`FeedingOther` AS `FeedingOther`
-			,`Drug`.`DrugName` AS `DrugName`
-			,`Drug`.`Feeding` AS `Feeding`
-			,`Drug`.`TradeName` AS `TradeName`
-			,`Drug`.`DrugDosingDeterminant` AS `DrugDosingDeterminant`
-			,`Drug`.`ManufacturerOther` AS `ManufacturerOther`
-			,`Drug`.`TradeNameOther` AS `TradeNameOther`
-			,`Drug`.`Manufacturer` AS `Manufacturer`
-			,`Drug`.`FatPerMeal` AS `FatPerMeal`
-			,`Drug`.`DrugStorage` AS `DrugStorage`
-			,`Drug`.`DrugNameOther` AS `DrugNameOther`
-			,`Drug`.`Comments` AS `Comments`
-			,`Drug`.`AdministrationRoute` AS `AdministrationRoute`
-			,`Drug`.`ReadministeredOnVomitting` AS `ReadministeredOnVomitting`
-			,`Drug`.`ActiveIngredients_Drug_Hjid` AS `ActiveIngredients_Drug_Hjid`
-			,`Drug`.`WeightGroupDosing_Drug_Hjid` AS `WeightGroupDosing_Drug_Hjid`
-			,`Drug`.`AgeDosing_Drug_Hjid` AS `AgeDosing_Drug_Hjid`
-			,`Drug`.`Batches_Drug_Hjid` AS `Batches_Drug_Hjid`
-			,`Drug`.`WeightDosing_Drug_Hjid` AS `WeightDosing_Drug_Hjid`
-			,`Drug`.`Drug_Drugs_Hjid` AS `Drug_Drugs_Hjid` 
-from `Drug` left join `Drugs` on `Drug`.`Drug_Drugs_Hjid` = `Drugs`.`Hjid`
-		 left join `Regimen` `r` on`r`.`Drugs_Regimen_Hjid` = `Drugs`.`Hjid`
-		 left join `Regimens` `rs` on`r`.`Regimen_Regimens_Hjid` = `rs`.`Hjid`
-		 left join `Treatment` `t` on`t`.`Regimens_Treatment_Hjid` = `rs`.`Hjid`
-		 left join `Clinical` on`Clinical`.`Treatment_Clinical_Hjid` = `t`.`Hjid`
-		 left join `StudyInfo` `si` on`si`.`Clinical_StudyInfo_Hjid` = `Clinical`.`Hjid`
-		 left join `Study` `s` on`s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
-		 left join `Content` `c` on`c`.`Study_Content_Hjid` = `s`.`Hjid`
-		 left join `Entry` `e` on`e`.`Content_Entry_Hjid` = `c`.`Hjid`;
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW  `ClinicalDrugs` AS 
+  SELECT `e`.`StudyID` AS `StudyID`
+      ,`Drug`.`Hjid` AS `Hjid`
+      ,`Drug`.`DTYPE` AS `DTYPE`
+      ,`Drug`.`FeedingOther` AS `FeedingOther`
+      ,`Drug`.`DrugName` AS `DrugName`
+      ,`Drug`.`Feeding` AS `Feeding`
+      ,`Drug`.`TradeName` AS `TradeName`
+      ,`Drug`.`DrugDosingDeterminant` AS `DrugDosingDeterminant`
+      ,`Drug`.`ManufacturerOther` AS `ManufacturerOther`
+      ,`Drug`.`TradeNameOther` AS `TradeNameOther`
+      ,`Drug`.`Manufacturer` AS `Manufacturer`
+      ,`Drug`.`FatPerMeal` AS `FatPerMeal`
+      ,`Drug`.`DrugStorage` AS `DrugStorage`
+      ,`Drug`.`DrugNameOther` AS `DrugNameOther`
+      ,`Drug`.`Comments` AS `Comments`
+      ,`Drug`.`AdministrationRoute` AS `AdministrationRoute`
+      ,`Drug`.`ReadministeredOnVomitting` AS `ReadministeredOnVomitting`
+      ,`Drug`.`ActiveIngredients_Drug_Hjid` AS `ActiveIngredients_Drug_Hjid`
+      ,`Drug`.`WeightGroupDosing_Drug_Hjid` AS `WeightGroupDosing_Drug_Hjid`
+      ,`Drug`.`AgeDosing_Drug_Hjid` AS `AgeDosing_Drug_Hjid`
+      ,`Drug`.`Batches_Drug_Hjid` AS `Batches_Drug_Hjid`
+      ,`Drug`.`WeightDosing_Drug_Hjid` AS `WeightDosing_Drug_Hjid`
+      ,`Drug`.`Drug_Drugs_Hjid` AS `Drug_Drugs_Hjid` 
+from `Drug` LEFT JOIN `Drugs` ON `Drug`.`Drug_Drugs_Hjid` = `Drugs`.`Hjid`
+     LEFT JOIN `Regimen` `r` ON `r`.`Drugs_Regimen_Hjid` = `Drugs`.`Hjid`
+     LEFT JOIN `Regimens` `rs` ON `r`.`Regimen_Regimens_Hjid` = `rs`.`Hjid`
+     LEFT JOIN `Treatment` `t` ON `t`.`Regimens_Treatment_Hjid` = `rs`.`Hjid`
+     LEFT JOIN `Clinical` ON `Clinical`.`Treatment_Clinical_Hjid` = `t`.`Hjid`
+     LEFT JOIN `StudyInfo` `si` ON `si`.`Clinical_StudyInfo_Hjid` = `Clinical`.`Hjid`
+     LEFT JOIN `Study` `s` ON `s`.`StudyInfo_Study_Hjid` = `si`.`Hjid`
+     LEFT JOIN `Content` `c` ON `c`.`Study_Content_Hjid` = `s`.`Hjid`
+     LEFT JOIN `Entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 ;
 
-
-DROP VIEW IF EXISTS `pkAnalytes`;
-CREATE VIEW `pkAnalytes` AS
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW  `pkAnalytes` AS
 
 SELECT `e`.`StudyID` AS `StudyID`
      , `a`.`Hjid` AS `Hjid`
@@ -103,8 +96,7 @@ LEFT JOIN `Entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 
 
 
-DROP VIEW IF EXISTS `pkSamples`;
-CREATE VIEW `pkSamples` AS
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW  `pkSamples` AS
 
 SELECT `e`.`StudyID` AS `StudyID`
      ,Sample.*
@@ -119,8 +111,7 @@ LEFT JOIN `Entry` `e` ON `e`.`Content_Entry_Hjid` = `c`.`Hjid`;
 
 
 
-DROP VIEW IF EXISTS `pkdetails`;
-CREATE VIEW `pkdetails` AS
+CREATE OR REPLACE SQL SECURITY INVOKER VIEW  `pkdetails` AS
 
 SELECT `e`.`StudyID` AS `StudyID`
      , `p`.`Hjid` AS `Hjid`
