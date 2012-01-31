7b933cf37d5b447ca1ba14160e5766155fa597c3
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-01-31 17:05:29 +0000

    Now also handles links

--- trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/DownloadLiveCreateDBTest.java
+++ trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/DownloadLiveCreateDBTest.java
@@ -3,17 +3,10 @@ package org.cggh.chassis.rest.util;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
-import java.util.Date;
 import java.util.Iterator;
 
-import javax.xml.parsers.DocumentBuilderFactory;
-
 import org.apache.commons.io.FileUtils;
-import org.cggh.casutils.CasProtectedResourceDownloader;
-import org.cggh.casutils.CasProtectedResourceDownloaderFactory;
 import org.cggh.casutils.NotFoundException;
-import org.w3c.dom.Document;
-import org.w3c.dom.NodeList;
 
 /**
  * @author timp
@@ -21,9 +14,11 @@ import org.w3c.dom.NodeList;
  */
 public class DownloadLiveCreateDBTest extends AbstractUtilSpec {
 
-  private static String PRUNED_FEED_FILE_PATH;
-  private static String wwarnLivePassword;
-  private static String wwarnLiveUser;
+  private static final String STUDY_FEED_URL = "https://www.wwarn.org/repository/service/content/studies";
+  private static final String LINK_FEED_URL = "https://www.wwarn.org/repository/service/content/link";
+  // private static String PRUNED_STUDY_FEED_FILE_PATH;
+  static String wwarnLivePassword;
+  static String wwarnLiveUser;
   
   public DownloadLiveCreateDBTest() { 
     super();
@@ -36,29 +31,32 @@ public class DownloadLiveCreateDBTest extends AbstractUtilSpec {
   @Override
   protected void setUp() throws Exception {
     super.setUp();
-    wwarnLivePassword = config.getConfiguration().get("wwarn-live-password");
     wwarnLiveUser = config.getConfiguration().get("wwarn-live-user");
-    PRUNED_FEED_FILE_PATH = config.getConfiguration().get("DATA_DIR_NAME") + "/chassis-rest-studies.xml";;
+    if (wwarnLiveUser == null)
+      throw new NullPointerException("wwarn-live-user not set in configuration file.");
+    wwarnLivePassword = config.getConfiguration().get("wwarn-live-password");
+    
+    //PRUNED_STUDY_FEED_FILE_PATH = config.getConfiguration().get("DATA_DIR_NAME") + "/chassis-rest-studies.xml";;
   }
 
   
   public void testGetWwarnChassis() throws Exception {
       setupXmlFiles();
       testPostsFromDirectory(DATA_STUDIES);
+      //postFeeds();
       HttpResponse response = StudyControllerRequester.readAcceptingHtml(url("/studyCount"));
       assertEquals(url("/studyCount"), 200, response.getStatus());
-      System.err.println(response.getBody());
-      if (response.getBody().indexOf("" + countEntries(FEED_FILE_PATH, "atom:entry")) > -1)
-        System.err.println("Looks like there no are validation errors");
+      if (response.getBody().indexOf("" + (countEntries(STUDY_FEED_FILE_PATH, "atom:entry") 
+              + countEntries(STUDY_FEED_FILE_PATH, "atom:entry"))) > -1)
+        System.out.println("Looks like there no are validation errors");
       else
-        System.err.println("Looks like there are validation errors");
+        System.out.println("Looks like there are validation errors");
         
   }
-
+  
   private void testPostsFromDirectory(String directory) throws Exception { 
     String url = url("/uncache");
     HttpResponse response = StudyControllerRequester.uncache(url);
-    System.out.println(response.getBody());
     assertEquals(url, 200, response.getStatus());
     
     url = url("/studies/");
@@ -88,45 +86,66 @@ public class DownloadLiveCreateDBTest extends AbstractUtilSpec {
       //System.out.print(" - ");
       //System.out.println(r.getBody());
       if (r.getStatus() != 201) {
-        System.out.print(studyFileName);
+        System.out.println(studyFileName);
         System.out.print(" - ");
-        System.out.println(r.getBody());
         if (r.getBody().indexOf("errors") > 0) { 
-          System.err.println(r.getBody());
+          System.err.println(r.getPrettyBody());
         }
         failCount ++;
       } else {
-        //StudyControllerRequester.delete(url(entryUrl)).getStatus();
+        StudyControllerRequester.delete(url(entryUrl)).getStatus();
       }
     }
   
     System.out.println("Files: " + fileCount + " fail count: " + failCount);
+
     
   }
+  private void postFeeds() throws Exception { 
+    String url = url("/uncache");
+    HttpResponse response = StudyControllerRequester.uncache(url);
+    assertEquals(url, 200, response.getStatus());
+    url = url("/studies");
+    response = StudyControllerRequester.create(STUDY_FEED_FILE_PATH, url );
+    // We know there are currently failing entries
+    assertEquals(response.getBody(), 400, response.getStatus());
+    url = url("/links");
+    response = StudyControllerRequester.create(LINK_FEED_FILE_PATH, url("/links"));
+    assertEquals(url, 201, response.getStatus());
+  }
+
   
   public void testPrune() { 
-  //  XsltTransformer.transform(FEED_FILE_PATH, "prune.xsl", PRUNED_FEED_FILE_PATH, true);    
+  //  XsltTransformer.transform(STUDY_FEED_FILE_PATH, "prune.xsl", PRUNED_STUDY_FEED_FILE_PATH, true);    
   }
 
   private void setupXmlFiles() throws NotFoundException, IOException {
-    if (wwarnLivePassword != null && ! (new File(FEED_FILE_PATH).exists())) { 
-      System.err.println("Downloading feed: " + FEED_FILE_PATH);
-      downloadFeed();
+    if (wwarnLivePassword != null) {
+      if (! (new File(STUDY_FEED_FILE_PATH).exists())) { 
+        System.err.println("Downloading feed: " + STUDY_FEED_URL + " to " + STUDY_FEED_FILE_PATH);
+        downloadFeed(STUDY_FEED_FILE_PATH, STUDY_FEED_URL, wwarnLiveUser, wwarnLivePassword);
+      }
     } else 
       System.err.println("Using existing feed");
     System.err.println("Config:" + config.getConfiguration());
+    if (! (new File(LINK_FEED_FILE_PATH).exists())) { 
+      System.err.println("Downloading feed: " + LINK_FEED_URL + " to " + LINK_FEED_FILE_PATH);
+      downloadFeed(LINK_FEED_FILE_PATH, LINK_FEED_URL, wwarnLiveUser, wwarnLivePassword);
+    } else 
+      System.err.println("Using existing link feed");
+    System.err.println(LINK_FEED_FILE_PATH + " to " + url("/links"));
     
     deleteExistingFiles();
     
-    //XsltTransformer.transform(FEED_FILE_PATH, "prune.xsl", PRUNED_FEED_FILE_PATH, true);
+    //XsltTransformer.transform(STUDY_FEED_FILE_PATH, "prune.xsl", PRUNED_STUDY_FEED_FILE_PATH, true);
     
     String studyFileName = config.getConfiguration().get("STUDIES_DIR_NAME") 
             + "/" + config.getConfiguration().get("STUDY_ID") + ".xml";
     File studyEntry = new File(studyFileName );
 
     assertFalse("Study file " + studyFileName + "created", studyEntry.exists());
-    // StudyFeedSplitter.split(PRUNED_FEED_FILE_PATH);
-    StudyFeedSplitter.split(FEED_FILE_PATH);
+    // StudyFeedSplitter.split(PRUNED_STUDY_FEED_FILE_PATH);
+    StudyFeedSplitter.split(STUDY_FEED_FILE_PATH);
     assertTrue("Study file " + studyFileName + " not created", studyEntry.exists());
   }
 
@@ -142,45 +161,5 @@ public class DownloadLiveCreateDBTest extends AbstractUtilSpec {
       if (!child.delete()) 
         throw new RuntimeException("Could not delete " + child.getCanonicalPath());
     }
-  }
-  private void downloadFeed() throws NotFoundException, IOException {
-    Date start = new Date();
-    System.err.println("Start:" + start);
-    CasProtectedResourceDownloaderFactory.downloaders.put(
-            CasProtectedResourceDownloaderFactory.keyFromUrl("https://www.wwarn.org/"),
-            new CasProtectedResourceDownloader("https://", "www.wwarn.org:443",
-                    wwarnLiveUser, wwarnLivePassword, System.getProperty("java.io.tmpdir")));
-
-    File studyOut = new File(FEED_FILE_PATH);
-    studyOut.delete();
-    String studyFeedUrl = "https://www.wwarn.org/repository/service/content/studies";
-
-    CasProtectedResourceDownloader downloader =
-            CasProtectedResourceDownloaderFactory.getCasProtectedResourceDownloader(studyFeedUrl);
-
-    downloader.downloadUrlToFile(studyFeedUrl, studyOut);
-    assertTrue(new File(FEED_FILE_PATH).exists());
-
-    Date end = new Date();
-    System.err.println("End:" + end);
-    long diff = end.getTime() - start.getTime();
-    System.err.println("Elapsed:" + diff / 1000);
-  }
-
-  private int countEntries(String xmlFile, String elementName) throws Exception { 
-    File file = new File(xmlFile);
-    if (!file.exists())
-      throw new IllegalArgumentException("File (" + file.getCanonicalPath() + ") not found");
-    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-     // Create the builder and parse the file
-    Document doc;
-    try {
-      doc = factory.newDocumentBuilder().parse(xmlFile);
-    } catch (Exception e) {
-      throw new RuntimeException(e);
-    }
-    NodeList nodes = doc.getElementsByTagName(elementName);
-    System.out.println("Document contains " + nodes.getLength() + " " + elementName + " elements.");
-    return nodes.getLength();
   }
 }
