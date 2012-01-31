8f19dbd859a16e6e752bc1d37c46be35a6816507
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-01-31 17:05:05 +0000

    Move shared items up

--- trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/AbstractUtilSpec.java
+++ trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/AbstractUtilSpec.java
@@ -1,5 +1,17 @@
 package org.cggh.chassis.rest.util;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.Date;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+
+import org.cggh.casutils.CasProtectedResourceDownloader;
+import org.cggh.casutils.CasProtectedResourceDownloaderFactory;
+import org.cggh.casutils.NotFoundException;
+import org.w3c.dom.Document;
+import org.w3c.dom.NodeList;
+
 import junit.framework.TestCase;
 
 /**
@@ -8,8 +20,9 @@ import junit.framework.TestCase;
  */
 public abstract class AbstractUtilSpec extends TestCase {
   public String DATA_STUDIES;
-  public String FEED_FILE_PATH;
+  public String STUDY_FEED_FILE_PATH;
   public String SERVICE_PROTOCOL_HOST_PORT;
+  public String LINK_FEED_FILE_PATH;
   protected static ChassisRestConfig config;
   
   public AbstractUtilSpec() {
@@ -23,7 +36,8 @@ public abstract class AbstractUtilSpec extends TestCase {
     super.setUp();
     config =  new ChassisRestConfig();
     DATA_STUDIES = config.getConfiguration().get("STUDIES_DIR_NAME") + "/";
-    FEED_FILE_PATH = config.getConfiguration().get("FEED_FILE_PATH");
+    STUDY_FEED_FILE_PATH = config.getConfiguration().get("STUDY_FEED_FILE_PATH");
+    LINK_FEED_FILE_PATH = config.getConfiguration().get("DATA_DIR_NAME") + "/link_feed.xml";
 
     SERVICE_PROTOCOL_HOST_PORT = config.getConfiguration().get("SERVICE_PROTOCOL_HOST_PORT");
   }
@@ -34,6 +48,45 @@ public abstract class AbstractUtilSpec extends TestCase {
   protected String url(String url) {
     return SERVICE_PROTOCOL_HOST_PORT + "/chassis-rest/service" + url;
   }
+  protected void downloadFeed(String outputFileName, String feedUrl, String user, String password) throws NotFoundException, IOException {
+    Date start = new Date();
+    System.err.println("Start:" + start);
+    CasProtectedResourceDownloaderFactory.downloaders.put(
+            CasProtectedResourceDownloaderFactory.keyFromUrl("https://www.wwarn.org/"),
+            new CasProtectedResourceDownloader("https://", "www.wwarn.org:443",
+                    user, password, System.getProperty("java.io.tmpdir")));
   
+    File fileOut = new File(outputFileName);
+    fileOut.delete();
+  
+    CasProtectedResourceDownloader downloader =
+            CasProtectedResourceDownloaderFactory.getCasProtectedResourceDownloader(feedUrl);
+  
+    downloader.downloadUrlToFile(feedUrl, fileOut);
+    assertTrue(new File(outputFileName).exists());
+  
+    Date end = new Date();
+    System.err.println("End:" + end);
+    long diff = end.getTime() - start.getTime();
+    System.err.println("Elapsed:" + diff / 1000);
+  }
+  
+  public static int countEntries(String xmlFile, String elementName) throws Exception { 
+    File file = new File(xmlFile);
+    if (!file.exists())
+      throw new IllegalArgumentException("File (" + file.getCanonicalPath() + ") not found");
+    DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+     // Create the builder and parse the file
+    Document doc;
+    try {
+      doc = factory.newDocumentBuilder().parse(xmlFile);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+    NodeList nodes = doc.getElementsByTagName(elementName);
+    return nodes.getLength();
+  }
   
 }
+
+
