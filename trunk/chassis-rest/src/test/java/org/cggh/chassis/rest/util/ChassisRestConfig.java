a165574138692f3c571d2461fe268fdbf4d8196d
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-02-03 22:52:52 +0000

    fidddle about

--- trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/ChassisRestConfig.java
+++ trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/util/ChassisRestConfig.java
@@ -5,13 +5,13 @@ import java.util.Properties;
 import org.cggh.chassis.rest.configuration.Configuration;
 
 public class ChassisRestConfig {
-  private Configuration c;
+  private Configuration configuraton;
   
   public ChassisRestConfig() { 
     Properties defaults = new Properties();
     defaults.put("truth", "Beauty");
-    String DATA_DIR_NAME = "downloaded";
-    String STUDY_STUDY_FEED_FILENAME = "/studies_feed.xml";
+    String CHASSIS_DOWNLOAD_DIR = "data";
+    String STUDY_STUDY_FEED_FILENAME = "/studies.xml";
     String STUDY_FEED_FILE_PATH;
     String STUDY_ID = "XGQRX";
     String STUDIES_DIR_NAME;
@@ -24,14 +24,14 @@ public class ChassisRestConfig {
     // This is just a dummy run to see if file exists
     // Not happy at duplication but we cannot check 
     // the properties file until we have set up the defaults
-    c = new Configuration("chassis-rest", defaults);
-    if (!c.configFileExists()) { 
-      DATA_DIR_NAME = "data";
+    configuraton = new Configuration("chassis-rest", defaults);
+    if (!configuraton.configFileExists()) { 
+      CHASSIS_DOWNLOAD_DIR = "data";
       INVALID_STUDY_ID = "AJYER";      
       System.err.println("Using dummy data");
     }    
-    STUDY_FEED_FILE_PATH = DATA_DIR_NAME + STUDY_STUDY_FEED_FILENAME; 
-    STUDIES_DIR_NAME = DATA_DIR_NAME + "/studies";
+    STUDY_FEED_FILE_PATH = CHASSIS_DOWNLOAD_DIR + STUDY_STUDY_FEED_FILENAME; 
+    STUDIES_DIR_NAME = CHASSIS_DOWNLOAD_DIR + "/studies";
     
     defaults.put("DATA_DIR_NAME", "data");
     defaults.put("STUDY_FEED_FILE_PATH", STUDY_FEED_FILE_PATH); 
@@ -41,10 +41,10 @@ public class ChassisRestConfig {
     defaults.put("SERVICE_PROTOCOL_HOST_PORT", SERVICE_PROTOCOL_HOST_PORT);
     
     // Create it again with new defaults
-    c = new Configuration("chassis-rest", defaults);
+    configuraton = new Configuration("chassis-rest", defaults);
   }
 
   public Configuration getConfiguration() { 
-    return c;
+    return configuraton;
   }
 }
