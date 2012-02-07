cd260e4c56675b70264961345636d4c1ddb37351
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-02-07 15:25:12 +0000

    Refactor

--- trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/configuration/ConfigurationTest.java
+++ trunk/chassis-rest/src/test/java/org/cggh/chassis/rest/configuration/ConfigurationTest.java
@@ -7,17 +7,19 @@ import junit.framework.TestCase;
 public class ConfigurationTest extends TestCase {
 
   public void testConfigurationString() {
-    Configuration c = new Configuration("notFound");
-    assertNull(c.get("nothing"));
-    assertNull(c.put("key", "value"));
-    assertEquals("value", c.put("key", "value"));
-    assertEquals("value", c.get("key"));
+    try { 
+      new Configuration("notFound");
+      fail("Should have bombed");
+    } catch (RuntimeException e) { 
+      assertTrue(e.getCause().getClass().getName().equals("java.io.FileNotFoundException"));
+    }
+      
   }
 
   public void testConfigurationStringProperties() {
    Properties defaults = new Properties();
    defaults.put("truth", "Beauty");
-   Configuration c = new Configuration("notFound", defaults);
+   Configuration c = new Configuration("notFound", "notFound", defaults);
    assertNull(c.get("nothing"));
    assertNull(c.put("key", "value"));
    assertEquals("value", c.put("key", "value"));
