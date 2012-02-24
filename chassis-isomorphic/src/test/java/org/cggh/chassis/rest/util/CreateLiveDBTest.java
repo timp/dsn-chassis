0606d32f0c9106345fced327d5f3c579fc69e117
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-02-24 23:23:57 +0000

    Refactored

---
+++ chassis-isomorphic/src/test/java/org/cggh/chassis/rest/util/CreateLiveDBTest.java
@@ -0,0 +1,35 @@
+package org.cggh.chassis.rest.util;
+
+
+/**
+ * @author timp
+ * @since 2011-11-16
+ */
+public class CreateLiveDBTest extends AbstractUtilSpec {
+
+  
+  public CreateLiveDBTest() { 
+    super();
+  }
+  public CreateLiveDBTest(String name) { 
+    super(name);    
+  }
+  
+  
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    String downloadedDir = config.getConfiguration().get("DATA_DIR_NAME") + "/www.wwarn.org/";
+    STUDY_FEED_FILE_PATH = downloadedDir + "studies.xml";
+    STUDY_ENTRY_DIR_NAME = downloadedDir + "studies";
+    LINK_FEED_FILE_PATH = downloadedDir + "links.xml";
+
+    PRUNED_STUDY_FEED_FILE_PATH = downloadedDir + "pruned-studies.xml";
+  }
+
+  public void testPostStudies() throws Exception { 
+    if(!postStudies())
+      fail("There are validation errors");
+  }
+  
+}
