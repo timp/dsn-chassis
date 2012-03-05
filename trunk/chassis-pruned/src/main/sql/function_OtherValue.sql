27bd191ed07053e707934bad19140daea36d8056
tim.pizey <tim.pizey@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-03-05 09:54:37 +0000

    Redefine sql function so as to exclude semicolons

--- trunk/chassis-pruned/src/main/sql/function_OtherValue.sql
+++ trunk/chassis-pruned/src/main/sql/function_OtherValue.sql
@@ -1,16 +1,16 @@
+-- Beware, we are splitting on semicolons in jdbc executuion, so don't introduce them
 
 DROP FUNCTION IF EXISTS `otherValue`;
 
-CREATE FUNCTION `otherValue`( col varchar(255), otherCol varchar(255)) RETURNS varchar(255) CHARSET utf8
-    DETERMINISTIC
-BEGIN
-    DECLARE ret varchar(255);
-    select CASE col
-        WHEN  'other' THEN  otherCol
+CREATE FUNCTION `otherValue`( col varchar(255), otherCol varchar(255)) 
+    RETURNS varchar(255) CHARSET utf8 DETERMINISTIC
+  RETURN (select CASE col
+        WHEN  'other' THEN  
+          otherCol
         ELSE  
-         col 
-        END INTO ret;
-        RETURN ret;
-        
-END
+          col 
+        END
+        )
 ;
+
+
