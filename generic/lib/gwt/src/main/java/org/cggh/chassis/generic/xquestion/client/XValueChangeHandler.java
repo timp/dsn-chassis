a0edc4ba540dd9e7a5699f46f15ef2ad0f389b8c
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-03-26 12:49:39 +0000

    [maven-release-plugin]  copy for tag chassis-rest-0.0.5

--- generic/lib/gwt/src/main/java/org/cggh/chassis/generic/xquestion/client/XValueChangeHandler.java
+++
@@ -1,9 +0,0 @@
-package org.cggh.chassis.generic.xquestion.client;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface XValueChangeHandler extends EventHandler {
-
-	void onChange(XValueChangeEvent e);
-
-}
