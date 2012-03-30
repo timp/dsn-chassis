560d1b8c9d62d1f907881bd6832910b6c670659e
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-03-30 09:39:27 +0000

    [maven-release-plugin]  copy for tag chassisViewsCore-0.0.1

--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/SubmitDataNavigationHandler.java
+++
@@ -1,9 +0,0 @@
-package org.cggh.chassis.wwarn.ui.submitter.client;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface SubmitDataNavigationHandler extends EventHandler {
-
-	void onNavigation(SubmitDataNavigationEvent e);
-
-}
