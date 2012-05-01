1ea35d20c102b1dd1df5fc537fd08eef42812961
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-05-01 14:24:00 +0000

    [maven-release-plugin]  copy for tag chassisViewsCore-0.0.2

--- chassis-pruned/src/main/sql/ClinicalArmsPerStudy.sql
+++
@@ -1,9 +0,0 @@
--- arms per study
-
-
-select ac.armCount, count(ac.armCount) from 
-( select StudyId, count(StudyId) as armCount 
-  from v_ClinicalCompleteStudyRegimens group by StudyId  ) as ac
-group by armCount
-
-;
