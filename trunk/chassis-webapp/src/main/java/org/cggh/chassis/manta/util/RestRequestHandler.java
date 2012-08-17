7ebf865d480174f9c4c9f285d257b28ffacbc4c7
ian.wright@well.ox.ac.uk <ian.wright@well.ox.ac.uk@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-08-17 13:04:21 +0000

    Handle empty messages

--- trunk/chassis-webapp/src/main/java/org/cggh/chassis/manta/util/RestRequestHandler.java
+++ trunk/chassis-webapp/src/main/java/org/cggh/chassis/manta/util/RestRequestHandler.java
@@ -3,12 +3,12 @@ package org.cggh.chassis.manta.util;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 import javax.servlet.http.HttpServletRequest;
 
 import org.apache.log4j.Logger;
-import org.cggh.chassis.manta.security.ProxyServlet;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.web.client.RequestCallback;
@@ -48,10 +48,16 @@ public class RestRequestHandler implements RequestCallback {
 		String in;
 		HttpMethod methodType = arg0.getMethod();
 		if (methodType != HttpMethod.GET) {
-			in = new Scanner(inputStream).useDelimiter("\\A").next();
-			arg0.getBody().write(in.getBytes());
-			if (logger.isDebugEnabled()) {
-				logger.debug(in);
+			try {
+				in = new Scanner(inputStream).useDelimiter("\\A").next();
+				arg0.getBody().write(in.getBytes());
+				if (logger.isDebugEnabled()) {
+					logger.debug(in);
+				}
+			} catch (NoSuchElementException e) {
+				if (logger.isDebugEnabled()) {
+					logger.debug("Empty content passed to " + methodType + " " + arg0.getURI());
+				}
 			}
 		}
 	}
