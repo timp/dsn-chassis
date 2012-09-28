28cb593b05e6dd794c0949b35d2e4c96031b4955
rmcooksey@gmail.com <rmcooksey@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-09-28 15:29:04 +0000

    [maven-release-plugin]  copy for tag chassis-webapp-3.0-alpha-5

--- chassis-pruned/src/main/sql/ClinicalStudyDuration.sql
+++
@@ -1,6 +0,0 @@
--- Not there yet
-
-select sid, count(lastday) as mode from (
-SELECT sid, lastday FROM `curated_files`.`clinical_WWARNSet_imported` order by sid, lastday
-) basis group by sid, lastDay 
-
--- wwarn/ui/src/main/java/org/cggh/chassis/wwarn/ui/submitter/client/ProceedActionHandler.java
+++
@@ -1,9 +0,0 @@
-package org.cggh.chassis.wwarn.ui.submitter.client;
-
-import com.google.gwt.event.shared.EventHandler;
-
-public interface ProceedActionHandler extends EventHandler {
-
-	void onAction(ProceedActionEvent e);
-
-}
