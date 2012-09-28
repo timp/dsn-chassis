3b8ac0a8c287596d0dc66f0b96e0ca227afa24cf
rmcooksey@gmail.com <rmcooksey@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-09-28 10:56:05 +0000

    [maven-release-plugin]  copy for tag chassis-webapp-3.0-alpha-2

--- chassis-pruned/src/main/sql/ClinicalPatientsPerStudy.sql
+++
@@ -1,8 +0,0 @@
--- It is assumed that sid/psid is unique
--- Add cunique constraint to table to import to
--- ALTER TABLE `curated_files`.`clinical_WWARNSet_imported` 
--- ADD UNIQUE INDEX `studyPatient_u` (`sid` ASC, `psid` ASC) ;
-
-SELECT count(psid) as pcount FROM `curated_files`.`clinical_WWARNSet_imported` group by sid order by pcount;
-
-
