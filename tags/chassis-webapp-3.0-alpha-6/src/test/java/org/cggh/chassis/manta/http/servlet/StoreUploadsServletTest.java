9fe9e89b09103bd70d039be5d669ef0a5de9d082
rmcooksey@gmail.com <rmcooksey@gmail.com@bc3d9232-6646-11de-8b62-cffe7e141a8d>
2012-10-01 09:27:18 +0000

    [maven-release-plugin]  copy for tag chassis-webapp-3.0-alpha-6

---
+++ tags/chassis-webapp-3.0-alpha-6/src/test/java/org/cggh/chassis/manta/http/servlet/StoreUploadsServletTest.java
@@ -0,0 +1,25 @@
+package org.cggh.chassis.manta.http.servlet;
+
+import junit.framework.TestCase;
+import org.springframework.mock.web.MockHttpServletRequest;
+
+import java.net.MalformedURLException;
+
+/**
+ * Not so much a test as documentation.
+ */
+public class StoreUploadsServletTest extends TestCase {
+
+	    public void testURL() throws MalformedURLException {
+            String testURL = "https://demo.wwarn.org:443/repo/service/content/media/curated/MNZMZ";
+            MockHttpServletRequest mockReq = new MockHttpServletRequest();
+
+            StoreUploadsServlet.setRequestURL(testURL, mockReq, null);
+            String uri = StoreUploadsServlet.requestToURL(mockReq);
+
+            assertEquals(testURL,uri);
+
+        }
+	
+	
+}
